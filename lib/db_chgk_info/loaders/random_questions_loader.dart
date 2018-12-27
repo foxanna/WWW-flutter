4f8c0dd017acdfeb8c22ef2e3ea9ae656699f601
Anna Domashych <doan@ciklum.com>
2018-12-27 20:31:31 +0200

    Load random questions

---
+++ lib/db_chgk_info/loaders/random_questions_loader.dart
@@ -0,0 +1,17 @@
+import 'package:what_when_where/db_chgk_info/http_client.dart';
+import 'package:what_when_where/db_chgk_info/models/question.dart';
+import 'package:what_when_where/db_chgk_info/models/random_questions.dart';
+
+class RandomQuestionsLoader {
+  static final _instance = RandomQuestionsLoader._internal();
+
+  RandomQuestionsLoader._internal();
+
+  factory RandomQuestionsLoader() => _instance;
+
+  Future<Iterable<Question>> get() async {
+    var map = await HttpClient().get("/xml/random");
+    var randomQuestions = RandomQuestions.fromJson(map);
+    return randomQuestions?.search ?? Iterable<Question>.empty();
+  }
+}
