20344e53aa1690617a408dcebc74b7fe9f6f47af
Anna Domashych <doan@ciklum.com>
2018-12-27 18:31:40 +0200

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Container(),
+      ),
+    );
+  }
+}
