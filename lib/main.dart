f88ca7da56539cb69f5ae64108c0d3d01089d79f
Anna Domashych <doan@ciklum.com>
2018-12-27 18:38:52 +0200

    Set app name with Strings value

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:what_when_where/resources/strings.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,8 +7,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: Strings.appName,
       home: Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          title: Text(Strings.appName),
+        ),
         body: Container(),
       ),
     );
