3e4f9a234a38e4a18d914f00b1313116900814e7
Anna Domashych <doan@ciklum.com>
2018-12-27 19:42:45 +0200

    Create custom logger

---
+++ lib/utils/logger.dart
@@ -0,0 +1,14 @@
+import 'dart:math';
+
+const _maxLogSize = 512;
+
+void log(Object object) {
+  var data = _split(object.toString(), _maxLogSize);
+  data.forEach(print);
+}
+
+Iterable<String> _split(String string, int size) sync* {
+  for (var i = 0; i < string.length; i += size) {
+    yield string.substring(i, min(i + size, string.length));
+  }
+}
