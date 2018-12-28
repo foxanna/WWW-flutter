f550fe7952fe2672cfb5ec0130a33a79ec56a6c6
Anna Domashych <doan@ciklum.com>
2018-12-28 14:01:22 +0200

    Load latest tournaments

---
+++ lib/db_chgk_info/loaders/latest_tournaments_loader.dart
@@ -0,0 +1,29 @@
+import 'package:html/parser.dart';
+import 'package:what_when_where/db_chgk_info/http_client.dart';
+import 'package:what_when_where/db_chgk_info/models/tournament.dart';
+import 'package:what_when_where/utils/extentions.dart';
+
+class LatestTournamentsLoader {
+  static final _instance = LatestTournamentsLoader._internal();
+
+  factory LatestTournamentsLoader() => _instance;
+
+  LatestTournamentsLoader._internal();
+
+  Future<Iterable<Tournament>> get({page = 0}) async {
+    var html = await HttpClient()
+        .getRaw(Uri(path: '/last', queryParameters: {'page': page.toString()}));
+    var tournaments = parseHtml(html);
+    return tournaments;
+  }
+
+  Iterable<Tournament> parseHtml(String html) {
+    var table = parse(html).getElementsByClassName('last_packages');
+
+    var rows = IterableExtensions.merge(
+            table.first.getElementsByClassName('odd'),
+            table.first.getElementsByClassName('even'))
+        .toList();
+    return rows.map((r) => Tournament.fromHtml(r)).toList();
+  }
+}
