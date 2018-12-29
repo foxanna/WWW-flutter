05494e45f29e4082444979e2939a7140981646f2
Anna Domashych <doan@ciklum.com>
2018-12-29 18:51:57 +0200

    More default dimentions

--- lib/resources/dimentions.dart
+++ lib/resources/dimentions.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
 class Dimensions {
-  static const defaultPadding = const EdgeInsets.all(8.0);
+  static const defaultPadding = const EdgeInsets.all(defaultSidePadding);
+  static const defaultSidePadding = 8.0;
 }
