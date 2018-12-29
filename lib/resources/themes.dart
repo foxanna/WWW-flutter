fc29b062bcae4d2a575c39ddbe9c8e53dfb07b65
Anna Domashych <doan@ciklum.com>
2018-12-29 21:09:46 +0200

    Modify theme

--- lib/resources/themes.dart
+++ lib/resources/themes.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
 class Themes {
-  static final ThemeData appTheme = ThemeData(
-    brightness: Brightness.light,
-    primaryColor: Colors.lightBlue[800],
-    accentColor: Colors.cyan[600],
-    fontFamily: 'Montserrat',
-    textTheme: TextTheme(
-      headline: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-      title: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-      body1: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
-    ),
-  );
+  static ThemeData get appTheme {
+    final ThemeData base = ThemeData.light();
+
+    return base.copyWith(
+      primaryColor: Colors.lightBlue[800],
+      accentColor: Colors.cyan[600],
+      textTheme: base.textTheme.copyWith(
+          body2: base.textTheme.body1.copyWith(
+              color: base.textTheme.caption.color,
+              fontWeight: FontWeight.w400)),
+    );
+  }
 }
