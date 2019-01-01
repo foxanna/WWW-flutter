8f9b629e21e014bbc36b033e733f2fb067547214
Anna Domashych <doan@ciklum.com>
2019-01-01 20:42:24 +0200

    Display TournamentDetailsPage when tournament item tapped

--- lib/ui/tournament_list_tile.dart
+++ lib/ui/tournament_list_tile.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:what_when_where/db_chgk_info/models/tournament.dart';
 import 'package:what_when_where/resources/dimentions.dart';
 import 'package:what_when_where/resources/strings.dart';
+import 'package:what_when_where/ui/tournament_details_page.dart';
 
 class TournamentListTile extends StatelessWidget {
   final Tournament tournament;
@@ -16,41 +17,50 @@ class TournamentListTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var textTheme = Theme.of(context).textTheme;
-    return Padding(
-        padding: EdgeInsets.symmetric(
-            vertical: Dimensions.defaultSidePadding,
-            horizontal: Dimensions.defaultSidePadding * 2),
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Text(
-                tournament.title,
-                style: textTheme.subhead,
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-              ),
-              SizedBox.fromSize(
-                size: Size(0, Dimensions.defaultSidePadding / 2),
-              ),
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Text('${Strings.addedAt} ${tournament.createdAt}',
-                        textAlign: TextAlign.left,
-                        overflow: TextOverflow.ellipsis,
-                        style: textTheme.body2),
-                  ),
-                  SizedBox.fromSize(
-                    size: Size(Dimensions.defaultSidePadding, 0),
-                  ),
-                  Expanded(
-                    child: Text('${Strings.playedAt} ${tournament.playedAt}',
-                        textAlign: TextAlign.right,
-                        overflow: TextOverflow.ellipsis,
-                        style: textTheme.body2),
-                  )
-                ],
-              ),
-            ]));
+    return InkWell(
+      child: Padding(
+          padding: EdgeInsets.symmetric(
+              vertical: Dimensions.defaultSidePadding,
+              horizontal: Dimensions.defaultSidePadding * 2),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Text(
+                  tournament.title,
+                  style: textTheme.subhead,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                SizedBox.fromSize(
+                  size: Size(0, Dimensions.defaultSidePadding / 2),
+                ),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Text('${Strings.addedAt} ${tournament.createdAt}',
+                          textAlign: TextAlign.left,
+                          overflow: TextOverflow.ellipsis,
+                          style: textTheme.body2),
+                    ),
+                    SizedBox.fromSize(
+                      size: Size(Dimensions.defaultSidePadding, 0),
+                    ),
+                    Expanded(
+                      child: Text('${Strings.playedAt} ${tournament.playedAt}',
+                          textAlign: TextAlign.right,
+                          overflow: TextOverflow.ellipsis,
+                          style: textTheme.body2),
+                    )
+                  ],
+                ),
+              ])),
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) =>
+                    TournamentDetailsPage(tournament: tournament)));
+      },
+    );
   }
 }
