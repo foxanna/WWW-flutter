bf32b06243ef40a03d557842458c0b04d55dbfad
Anna Domashych <doan@ciklum.com>
2019-01-02 14:39:40 +0200

    Create spacing widgets

--- lib/resources/dimentions.dart
+++ lib/resources/dimentions.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class Dimensions {
   static const defaultPadding = const EdgeInsets.all(defaultSidePadding);
   static const defaultSidePadding = 8.0;
+  static const defaultSpacing = 8.0;
 }
---
+++ lib/ui/common/spacing.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:what_when_where/resources/dimentions.dart';
+
+class VerticalSpace extends StatelessWidget {
+  final double height;
+
+  const VerticalSpace({Key key, this.height = Dimensions.defaultSpacing})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => SizedBox(width: 0, height: height);
+}
+
+class HorizontalSpace extends StatelessWidget {
+  final double width;
+
+  const HorizontalSpace({Key key, this.width = Dimensions.defaultSpacing})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => SizedBox(width: width, height: 0);
+}
