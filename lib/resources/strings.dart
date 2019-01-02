26a31e6d4d4f55e9534773fd44cbcdddee07cfd0
Anna Domashych <doan@ciklum.com>
2019-01-02 16:33:15 +0200

    More string resources

--- lib/resources/strings.dart
+++ lib/resources/strings.dart
@@ -2,4 +2,6 @@ class Strings {
   static const String appName = "What? When? Where?";
   static const String addedAt = "добавлено";
   static const String playedAt = "сыграно";
+  static const String tours = "туров";
+  static const String questions = "вопросов";
 }
