644b35f0bed1c4785d40ab6d238e13bca0674f9d
Anna Domashych <doan@ciklum.com>
2019-01-03 13:20:41 +0200

    Refactor HomePage

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -6,7 +6,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
-          title: Text(Strings.appName),
+          title: const Text(Strings.appName),
         ),
         body: LatestTournamentsListView(),
       );
