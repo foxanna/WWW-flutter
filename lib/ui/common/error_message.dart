d7adcccafae8f630dcb6ab1bcb53ca9ccb2a1e8b
Anna Domashych <doan@ciklum.com>
2019-01-05 17:04:12 +0200

    Improve ErrorMessage widget

--- lib/ui/common/error_message.dart
+++ lib/ui/common/error_message.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:what_when_where/resources/dimensions.dart';
+import 'package:what_when_where/resources/strings.dart';
 
 class ErrorMessage extends StatelessWidget {
+  final Function _retryFunction;
+
+  const ErrorMessage({Key key, Function retryFunction})
+      : this._retryFunction = retryFunction,
+        super(key: key);
+
   @override
-  Widget build(BuildContext context) => Padding(
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
         padding: Dimensions.defaultPadding,
-        child: Center(child: Text("error :(")),
-      );
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(Strings.genericError),
+            _retryFunction != null
+                ? RaisedButton(
+                    child: Text(Strings.retry),
+                    onPressed: () {
+                      _retryFunction();
+                    },
+                  )
+                : Container()
+          ],
+        ),
+      ),
+    );
+  }
 }
