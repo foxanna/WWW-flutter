e04cb8b692a92908f15367e09bd9866c8d301de3
Anna Domashych <doan@ciklum.com>
2019-01-06 17:17:54 +0200

    Refactor TournamentDetailsHeader to be short and display details in dialog

--- lib/ui/tournament_details/tournament_details_header.dart
+++ lib/ui/tournament_details/tournament_details_header.dart
@@ -2,46 +2,114 @@ import 'package:flutter/material.dart';
 import 'package:what_when_where/db_chgk_info/models/tournament.dart';
 import 'package:what_when_where/resources/dimensions.dart';
 import 'package:what_when_where/resources/strings.dart';
-import 'package:what_when_where/ui/common/spacing.dart';
 
 class TournamentDetailsHeader extends StatelessWidget {
-  final Tournament _tournament;
+  final String _title;
+  final String _detailsText;
 
-  const TournamentDetailsHeader({Key key, @required Tournament tournament})
-      : this._tournament = tournament,
-        super(key: key);
+  TournamentDetailsHeader({Key key, @required Tournament tournament})
+      : this._title = tournament.title,
+        this._detailsText = _buildDetailsText(tournament),
+        super(key: key) {
+    assert(tournament != null);
+  }
 
   @override
-  Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
-    return Padding(
-      padding: Dimensions.defaultPadding * 2,
-      child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              _tournament.title,
-              style: textTheme.title,
-            ),
-            VerticalSpace(height: Dimensions.defaultSpacing / 2),
-            (_tournament.editors != null)
-                ? Text(_tournament.editors, style: textTheme.body2)
-                : Container(),
-            Text('${Strings.playedAt} ${_tournament.playedAt}',
-                textAlign: TextAlign.left,
-                overflow: TextOverflow.ellipsis,
-                style: textTheme.body2),
-            (_tournament.description != null)
-                ? Column(children: <Widget>[
-                    VerticalSpace(height: Dimensions.defaultSpacing),
-                    Text(_tournament.description, style: textTheme.body2)
-                  ])
-                : Container(),
-            VerticalSpace(height: Dimensions.defaultSpacing),
-            Text(
-                '${Strings.tours}: ${_tournament.tours.length}, ${Strings.questions}: ${_tournament.questionsCount}',
-                style: textTheme.body2),
-          ]),
-    );
+  Widget build(BuildContext context) => Padding(
+        padding: const EdgeInsets.only(
+            left: kToolbarHeight - Dimensions.defaultSidePadding,
+            right: kToolbarHeight,
+            bottom: Dimensions.defaultSidePadding * 2),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding:
+                    const EdgeInsets.only(left: Dimensions.defaultSidePadding),
+                child: Text(
+                  _title,
+                  style: Theme.of(context).accentTextTheme.title,
+                ),
+              ),
+              IconButton(
+                  padding: Dimensions.defaultPadding,
+                  icon: Icon(
+                    Icons.more_horiz,
+                    color: Theme.of(context).accentIconTheme.color,
+                  ),
+                  onPressed: () => _showDialog(context)),
+            ]),
+      );
+
+  void _showDialog(BuildContext context) => showDialog(
+        context: context,
+        builder: (BuildContext context) => _createDialog(context),
+      );
+
+  AlertDialog _createDialog(BuildContext context) => AlertDialog(
+        content: Text(
+          _detailsText,
+          style: Theme.of(context).textTheme.body2,
+        ),
+        actions: [
+          FlatButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Text(Strings.close),
+          ),
+        ],
+      );
+
+  static String _buildDetailsText(Tournament tournament) {
+    var result = StringBuffer();
+
+    if (tournament.editors != null) {
+      result.writeln(tournament.editors);
+    }
+
+    if (tournament.description != null) {
+      if (result.isNotEmpty) {
+        result.writeln();
+      }
+
+      result.writeln(tournament.description);
+    }
+
+    var toursAndQuestions = _buildToursAndQuestionsText(tournament);
+
+    if (toursAndQuestions.isNotEmpty) {
+      if (result.isNotEmpty) {
+        result.writeln();
+      }
+
+      result.writeln(toursAndQuestions);
+    }
+
+    if (tournament.playedAt != null) {
+      if (result.isNotEmpty) {
+        result.writeln();
+      }
+
+      result.writeln('${Strings.playedAt} ${tournament.playedAt}');
+    }
+
+    return result.toString();
+  }
+
+  static String _buildToursAndQuestionsText(Tournament tournament) {
+    var toursAndQuestions = StringBuffer();
+
+    if (tournament.tours != null && tournament.tours.isNotEmpty) {
+      toursAndQuestions.write('${Strings.tours}: ${tournament.tours.length}');
+    }
+
+    if (tournament.questionsCount != null && tournament.questionsCount != '0') {
+      if (toursAndQuestions.isNotEmpty) {
+        toursAndQuestions.write(', ');
+      }
+
+      toursAndQuestions
+          .write('${Strings.questions}: ${tournament.questionsCount}');
+    }
+    return toursAndQuestions.toString();
   }
 }
