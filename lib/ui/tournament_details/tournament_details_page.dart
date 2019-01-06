4ee423aa2d526100c49bcc8bee83f4b44901c176
Anna Domashych <doan@ciklum.com>
2019-01-06 17:31:34 +0200

    Display nice header on tournament details page

--- lib/ui/tournament_details/tournament_details_page.dart
+++ lib/ui/tournament_details/tournament_details_page.dart
@@ -4,6 +4,7 @@ import 'package:what_when_where/ui/common/error_message.dart';
 import 'package:what_when_where/ui/common/progress_indicator.dart';
 import 'package:what_when_where/ui/tournament_details/tournament_details_bloc.dart';
 import 'package:what_when_where/ui/tournament_details/tournament_details_bloc_state.dart';
+import 'package:what_when_where/ui/tournament_details/tournament_details_header.dart';
 import 'package:what_when_where/ui/tournament_details/tournament_details_tours_list.dart';
 
 class TournamentDetailsPage extends StatefulWidget {
@@ -21,11 +22,11 @@ class TournamentDetailsPage extends StatefulWidget {
 }
 
 class _TournamentDetailsPageState extends State<TournamentDetailsPage> {
-  final String _title;
+  final Tournament _tournament;
   final TournamentDetailsBloc _bloc;
 
   _TournamentDetailsPageState({@required Tournament tournament})
-      : this._title = tournament.title,
+      : this._tournament = tournament,
         this._bloc = TournamentDetailsBloc(tournament.textId) {
     _bloc.load();
   }
@@ -33,15 +34,31 @@ class _TournamentDetailsPageState extends State<TournamentDetailsPage> {
   @override
   Widget build(BuildContext context) => Scaffold(
       appBar: AppBar(
-        title: Text(_title),
       ),
-      body: StreamBuilder<TournamentDetailsBlocState>(
+      body: _buildStreamBuilder());
+
+  StreamBuilder<TournamentDetailsBlocState> _buildStreamBuilder() =>
+      StreamBuilder<TournamentDetailsBlocState>(
           stream: _bloc.stateStream,
           builder: (BuildContext context,
                   AsyncSnapshot<TournamentDetailsBlocState> snapshot) =>
-              _createBody(snapshot)));
+              Column(
+                children: <Widget>[
+                  _buildHeader(context, snapshot.data?.data),
+                  Expanded(child: _buildBody(snapshot)),
+                ],
+              ));
 
-  StatelessWidget _createBody(
+  Widget _buildHeader(BuildContext context, Tournament tournament) =>
+      PhysicalModel(
+        color: Theme.of(context).primaryColor,
+        elevation: 4.0,
+        child: TournamentDetailsHeader(
+          tournament: tournament ?? _tournament,
+        ),
+      );
+
+  StatelessWidget _buildBody(
       AsyncSnapshot<TournamentDetailsBlocState> snapshot) {
     if (snapshot.hasData) {
       var state = snapshot.data;
