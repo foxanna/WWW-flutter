26cf402ae6b2ae573fa91f356aaf4038957202ec
Anna Domashych <doan@ciklum.com>
2019-01-08 19:55:32 +0200

    Implement removing multiple spaces

--- lib/utils/texts.dart
+++ lib/utils/texts.dart
@@ -2,8 +2,14 @@ class TextUtils {
   static String normalize(String text) {
     if (text == null) return null;
 
-    return _removeParagraphs(text);
+    text = _removeParagraphs(text);
+    text = _removeMultipleSpaces(text);
+
+    return text;
   }
 
   static String _removeParagraphs(String text) => text.replaceAll('\\n', ' ');
+
+  static String _removeMultipleSpaces(String text) =>
+      text.replaceAll(new RegExp(r'\s{2,}'), ' ');
 }
