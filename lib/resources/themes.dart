635c531a52e62181292477ae22f9fea1226e8a22
Anna Domashych <doan@ciklum.com>
2019-01-10 22:24:48 +0200

    Change theming

--- lib/resources/themes.dart
+++ lib/resources/themes.dart
@@ -4,11 +4,7 @@ class Themes {
   static ThemeData get appTheme {
     final ThemeData base = ThemeData(
       primaryColor: Colors.teal[900],
-      accentColor: Colors.teal[500],
-//      primarySwatch: Colors.green,
-//      primaryColor: Colors.green[900],
-//      highlightColor: Colors.green[100],
-//      splashColor: Colors.green[200],
+      accentColor: Colors.teal[400],
     );
 
     return base.copyWith(
