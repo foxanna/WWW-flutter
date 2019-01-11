66ca23c214b6e6455ac402ea73785c188248781c
Anna Domashych <doan@ciklum.com>
2019-01-11 18:18:08 +0200

    Implement UrlLauncher

---
+++ lib/services/url_launcher.dart
@@ -0,0 +1,16 @@
+import 'package:url_launcher/url_launcher.dart';
+
+class UrlLauncher {
+  UrlLauncher._();
+
+  static Future launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    }
+  }
+
+  static Future sendEmail(String to, String subject) async {
+    var url = 'mailto:$to?subject=$subject';
+    await launchURL(url);
+  }
+}
