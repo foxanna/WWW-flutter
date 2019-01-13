93b6f5abba875b98a6f00ba535bee0893ecdd068
Anna Domashych <doan@ciklum.com>
2019-01-13 15:20:28 +0200

    Remove animations and display svg image

--- lib/ui/about/about_page.dart
+++ lib/ui/about/about_page.dart
@@ -1,45 +1,12 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:what_when_where/constants.dart';
 import 'package:what_when_where/resources/dimensions.dart';
 import 'package:what_when_where/resources/strings.dart';
 import 'package:what_when_where/services/url_launcher.dart';
 
-class AboutPage extends StatefulWidget {
-  @override
-  createState() => _AboutPageState();
-}
-
-class _AboutPageState extends State<AboutPage> with TickerProviderStateMixin {
-  AnimationController _controller;
-  Animation _fadeAnimation;
-  Animation _slideAnimation;
-
-  _AboutPageState() {
-    _controller =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 600));
-
-    _fadeAnimation =
-        CurvedAnimation(parent: _controller, curve: Curves.easeOut);
-
-    _slideAnimation = Tween(begin: Offset(0, 0.1), end: Offset.zero)
-        .animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    _controller.forward();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-
-    super.dispose();
-  }
-
+class AboutPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: _buildAppBar(context),
@@ -52,24 +19,18 @@ class _AboutPageState extends State<AboutPage> with TickerProviderStateMixin {
         elevation: 0.0,
       );
 
-  Widget _buildBody(BuildContext context) => _buildTransitions(context);
-
-  Widget _buildTransitions(BuildContext context) => FadeTransition(
-        opacity: _fadeAnimation,
-        child: SlideTransition(
-          position: _slideAnimation,
-          child: _buildContent(context),
-        ),
-      );
-
-  ListView _buildContent(BuildContext context) => ListView(
+  Widget _buildBody(BuildContext context) => ListView(
         padding: const EdgeInsets.symmetric(
             vertical: Dimensions.defaultSidePadding * 5,
             horizontal: Dimensions.defaultSidePadding * 3),
         children: [
-          Image.asset(
-            'assets/owl.jpg',
-            height: 300,
+          Hero(
+            tag: 'icon',
+            child: SvgPicture.asset(
+              'assets/owl.svg',
+              height: 300,
+              color: Theme.of(context).accentColor,
+            ),
           ),
           SizedBox(
             height: Dimensions.defaultSpacing * 5,
