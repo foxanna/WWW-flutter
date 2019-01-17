14e9d85e797108a3ee9c2b5b1e1da9aac902e1e7
Anna Domashych <doan@ciklum.com>
2019-01-17 19:47:08 +0200

    Temporary add a link to PlayMarket

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,5 +2,7 @@ class Constants {
   Constants._();
 
   static const String databaseUrl = 'https://db.chgk.info';
+  static const String marketLink =
+      'https://play.google.com/store/apps/details?id=chgk.droid';
   static const String developersEmail = 'que.ou.quand@gmail.com';
 }
