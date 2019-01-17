ca3dc9002964df51c7313fbb03d086be55b1af2f
Anna Domashych <doan@ciklum.com>
2019-01-17 19:48:21 +0200

    Implement a service for sharing tournament, tour or question

---
+++ lib/services/sharing.dart
@@ -0,0 +1,35 @@
+import 'package:share/share.dart';
+import 'package:what_when_where/constants.dart';
+import 'package:what_when_where/db_chgk_info/models/question.dart';
+import 'package:what_when_where/db_chgk_info/models/tour.dart';
+import 'package:what_when_where/db_chgk_info/models/tournament.dart';
+import 'package:what_when_where/resources/strings.dart';
+import 'package:what_when_where/services/analytics.dart';
+import 'package:what_when_where/services/question_parser/question_parser.dart';
+
+class Sharing {
+  static final _instance = Sharing._();
+
+  factory Sharing() => _instance;
+
+  Sharing._();
+
+  void shareTournament(Tournament tournament) {
+    Analytics().logEvent(name: 'share_tournament');
+    Share.share('${tournament.title}\n${tournament.url}${_createAppendix()}');
+  }
+
+  void shareTour(Tour tour) {
+    Analytics().logEvent(name: 'share_tour');
+    Share.share('${tour.title}\n${tour.url}${_createAppendix()}');
+  }
+
+  void shareQuestion(Question question) {
+    Analytics().logEvent(name: 'share_question');
+    Share.share(
+        '${QuestionParser.trim(question.question)}\n${question.url}${_createAppendix()}');
+  }
+
+  String _createAppendix() =>
+      '\n\n${Strings.sharedVia} ${Strings.appName}\n${Constants.marketLink}';
+}
