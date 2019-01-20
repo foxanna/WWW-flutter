c9cddd3d80a8ca81867aec21e58f78faeca6880d
Anna Domashych <doan@ciklum.com>
2019-01-20 20:56:24 +0200

    Apply Store and StoreProvider to WWWApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
+import 'package:what_when_where/redux/app/middleware.dart';
+import 'package:what_when_where/redux/app/reducer.dart';
+import 'package:what_when_where/redux/app/state.dart';
 import 'package:what_when_where/resources/strings.dart';
 import 'package:what_when_where/resources/themes.dart';
 import 'package:what_when_where/services/analytics.dart';
@@ -12,13 +17,21 @@ void main() {
 }
 
 class WWWApp extends StatelessWidget {
+  final store = Store<AppState>(
+    AppReducer.reducer,
+    initialState: AppState.initial(),
+    middleware: AppMiddleware.middleware,
+  );
+
   @override
-  Widget build(BuildContext context) => MaterialApp(
+  Widget build(BuildContext context) => StoreProvider<AppState>(
+      store: store,
+      child: MaterialApp(
         title: Strings.appName,
         theme: Themes.appTheme,
         navigatorObservers: <NavigatorObserver>[
           AnalyticsService().observer(home: LatestTournamentsPage.routeName)
         ],
         home: LatestTournamentsPage(),
-      );
+      ));
 }
