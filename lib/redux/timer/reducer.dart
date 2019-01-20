12b0ecd4c4820fd85aaf7af45344da5f4dc424dd
Anna Domashych <doan@ciklum.com>
2019-01-20 20:50:56 +0200

    Create TimerReducer

---
+++ lib/redux/timer/reducer.dart
@@ -0,0 +1,18 @@
+import 'package:redux/redux.dart';
+import 'package:what_when_where/redux/timer/actions.dart';
+import 'package:what_when_where/redux/timer/state.dart';
+
+class TimerReducer {
+  static final Reducer<TimerState> _reducer = combineReducers<TimerState>([
+    TypedReducer<TimerState, UpdateTimeValue>(_updateTimeValue),
+    TypedReducer<TimerState, UpdateIsRunningValue>(_updateIsRunningValue),
+  ]);
+
+  static TimerState reduce(state, action) => _reducer(state, action);
+
+  static TimerState _updateTimeValue(state, action) =>
+      TimerState(isRunning: state.isRunning, time: action.newValue);
+
+  static TimerState _updateIsRunningValue(state, action) =>
+      TimerState(time: state.time, isRunning: action.newValue);
+}
