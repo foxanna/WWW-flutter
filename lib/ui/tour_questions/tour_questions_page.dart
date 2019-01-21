5c065eaeeac984d36aaff8c2a2d870611652054c
Anna Domashych <doan@ciklum.com>
2019-01-21 10:50:38 +0200

    Use new widgets for timer

--- lib/ui/tour_questions/tour_questions_page.dart
+++ lib/ui/tour_questions/tour_questions_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:what_when_where/db_chgk_info/models/tour.dart';
-import 'package:what_when_where/resources/dimensions.dart';
-import 'package:what_when_where/resources/strings.dart';
 import 'package:what_when_where/ui/tour_questions/question_card.dart';
 import 'package:what_when_where/ui/tour_questions/timer_bloc.dart';
+import 'package:what_when_where/ui/tour_questions/timer_button.dart';
+import 'package:what_when_where/ui/tour_questions/timer_text.dart';
 import 'package:what_when_where/ui/tour_questions/tour_questions_page_menu.dart';
 
 class TourQuestionsPage extends StatefulWidget {
@@ -34,7 +34,7 @@ class _TourQuestionsPageState extends State<TourQuestionsPage> {
   @override
   Widget build(BuildContext context) => Scaffold(
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        floatingActionButton: _buildTimerButton(),
+        floatingActionButton: TimerButton(),
         appBar: AppBar(
           backgroundColor: Theme.of(context).canvasColor,
           iconTheme: Theme.of(context).iconTheme,
@@ -47,7 +47,7 @@ class _TourQuestionsPageState extends State<TourQuestionsPage> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  _buildTimerWidget(context),
+                  TimerText(),
                   _menu.createAppBarMenuAction(context),
                 ],
               ),
@@ -64,30 +64,6 @@ class _TourQuestionsPageState extends State<TourQuestionsPage> {
           onPageChanged: (index) => _timerBloc.actions.add(TimerActions.reset),
         ),
       );
-
-  Widget _buildTimerButton() => StreamBuilder<bool>(
-      stream: _timerBloc.isRunning,
-      initialData: false,
-      builder: (context, snapshot) => FloatingActionButton(
-            child: Icon(
-              snapshot.data ? Icons.timer_off : Icons.timer,
-            ),
-            tooltip: snapshot.data ? Strings.pauseTimer : Strings.startTimer,
-            onPressed: () => _timerBloc.actions
-                .add((snapshot.data) ? TimerActions.pause : TimerActions.run),
-          ));
-
-  Widget _buildTimerWidget(BuildContext context) => StreamBuilder<String>(
-      stream: _timerBloc.time,
-      initialData: '',
-      builder: (context, snapshot) => Padding(
-            padding: const EdgeInsets.symmetric(
-                horizontal: Dimensions.defaultSidePadding * 2),
-            child: Text(
-              snapshot.data,
-              style: Theme.of(context).primaryTextTheme.title,
-            ),
-          ));
 
   @override
   void dispose() {
