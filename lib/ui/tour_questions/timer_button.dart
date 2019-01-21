014af4a58c026bcbb5155270b60aedcf4938c875
Anna Domashych <doan@ciklum.com>
2019-01-21 12:59:31 +0200

    Optimize UI redraw with distinct = true

--- lib/ui/tour_questions/timer_button.dart
+++ lib/ui/tour_questions/timer_button.dart
@@ -1,31 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
-import 'package:redux/redux.dart';
 import 'package:tuple/tuple.dart';
 import 'package:what_when_where/redux/app/state.dart';
 import 'package:what_when_where/redux/timer/actions.dart';
-import 'package:what_when_where/redux/timer/state.dart';
 import 'package:what_when_where/resources/strings.dart';
+import 'package:what_when_where/utils/function_holder.dart';
 
 class TimerButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) =>
-      StoreConnector<AppState, Tuple2<Store, TimerState>>(
-        converter: (store) =>
-            Tuple2<Store, TimerState>(store, store.state.timerState),
+      StoreConnector<AppState, Tuple2<FunctionHolder, bool>>(
+        distinct: true,
+        converter: (store) {
+          var isRunning = store.state.timerState.isRunning;
+          return Tuple2<FunctionHolder, bool>(
+              FunctionHolder(() => store.dispatch(
+                    isRunning ? StopTimer() : StartTimer(),
+                  )),
+              isRunning);
+        },
         builder: (context, data) {
-          final timerState = data.item2;
-          final store = data.item1;
+          final holder = data.item1;
+          final isRunning = data.item2;
           return FloatingActionButton(
               child: Icon(
-                timerState.isRunning ? Icons.timer_off : Icons.timer,
+                isRunning ? Icons.timer_off : Icons.timer,
               ),
-              tooltip: timerState.isRunning
-                  ? Strings.pauseTimer
-                  : Strings.startTimer,
-              onPressed: () => store.dispatch(
-                    timerState.isRunning ? StopTimer() : StartTimer(),
-                  ));
+              tooltip: isRunning ? Strings.pauseTimer : Strings.startTimer,
+              onPressed: () => holder.function());
         },
       );
 }
--- lib/ui/tour_questions/timer_text.dart
+++ lib/ui/tour_questions/timer_text.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:what_when_where/redux/app/state.dart';
-import 'package:what_when_where/redux/timer/state.dart';
 import 'package:what_when_where/resources/dimensions.dart';
 
 class TimerText extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => StoreConnector<AppState, TimerState>(
-      converter: (store) => store.state.timerState,
-      builder: (context, data) => Padding(
+  Widget build(BuildContext context) => StoreConnector<AppState, Duration>(
+      distinct: true,
+      converter: (store) => store.state.timerState.time,
+      builder: (context, time) => Padding(
             padding: const EdgeInsets.symmetric(
                 horizontal: Dimensions.defaultSidePadding * 2),
             child: Text(
-              _formatDuration(data.time),
+              _formatDuration(time),
               style: Theme.of(context).primaryTextTheme.title,
             ),
           ));
