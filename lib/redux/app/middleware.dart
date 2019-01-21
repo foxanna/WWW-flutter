bc683dfe202faf6606dbee8ec0297bce00f955cc
Anna Domashych <doan@ciklum.com>
2019-01-21 09:24:48 +0200

    Update AppMiddleware with LogsMiddleware

--- lib/redux/app/middleware.dart
+++ lib/redux/app/middleware.dart
@@ -1,8 +1,10 @@
 import 'package:redux/redux.dart';
 import 'package:what_when_where/redux/app/state.dart';
+import 'package:what_when_where/redux/logs/middleware.dart';
 import 'package:what_when_where/redux/timer/middleware.dart';
 
 class AppMiddleware {
   static final middleware = List<Middleware<AppState>>()
-    ..addAll(TimerMiddleware.middleware);
+    ..addAll(TimerMiddleware.middleware)
+    ..addAll(LogsMiddleware.middleware);
 }
