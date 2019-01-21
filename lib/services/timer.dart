d6d6380a08934703ea45b0ab11290542a6a99fe9
Anna Domashych <doan@ciklum.com>
2019-01-21 09:32:43 +0200

    Create WWWTimer

---
+++ lib/services/timer.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+
+class WWWTimer {
+  void Function(Duration duration) _callback;
+
+  final int updateFrequency;
+  final _stopwatch = Stopwatch();
+  Timer _timer;
+
+  WWWTimer({this.updateFrequency = 1000})
+      : assert(updateFrequency != null),
+        assert(updateFrequency > 0);
+
+  bool get isRunning => _stopwatch.isRunning;
+
+  void start({void Function(Duration duration) callback}) {
+    _callback = callback;
+
+    if (isRunning) return;
+
+    _timer = Timer.periodic(
+        Duration(milliseconds: updateFrequency), _onTimerTimeout);
+    _stopwatch.start();
+  }
+
+  void pause() {
+    if (!isRunning) return;
+
+    _stopwatch.stop();
+    _timer.cancel();
+  }
+
+  void reset() {
+    pause();
+
+    _stopwatch.reset();
+    _updateTime();
+    _callback = null;
+  }
+
+  void _onTimerTimeout(Timer timer) {
+    if (_stopwatch.isRunning) {
+      _updateTime();
+    }
+  }
+
+  void _updateTime() {
+    if (_callback != null) _callback(_stopwatch.elapsed);
+  }
+}
