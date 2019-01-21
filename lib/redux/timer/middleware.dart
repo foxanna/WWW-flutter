85fd28a4ddcb98dcc618d31638a7246121055443
Anna Domashych <doan@ciklum.com>
2019-01-21 09:33:16 +0200

    Use WWWTimer in TimerMiddleware

--- lib/redux/timer/middleware.dart
+++ lib/redux/timer/middleware.dart
@@ -1,6 +1,7 @@
 import 'package:redux/redux.dart';
 import 'package:what_when_where/redux/app/state.dart';
 import 'package:what_when_where/redux/timer/actions.dart';
+import 'package:what_when_where/services/timer.dart';
 
 class TimerMiddleware {
   static final List<Middleware<AppState>> middleware = [
@@ -8,9 +9,26 @@ class TimerMiddleware {
     TypedMiddleware<AppState, StopTimer>(_stopTimer),
   ];
 
-  static void _startTimer(store, action, next) =>
-      next(UpdateIsRunningValue(true));
+  static const int _timerFrequency = 200;
+  static final _timer = WWWTimer(updateFrequency: _timerFrequency);
 
-  static void _stopTimer(store, action, next) =>
-      next(UpdateIsRunningValue(false));
+  static void _startTimer(
+      Store<AppState> store, TimerAction action, NextDispatcher next) {
+    next(action);
+
+    _timer.start(callback: (duration) => _updateTime(store, duration));
+    store.dispatch(UpdateIsRunningValue(_timer.isRunning));
+  }
+
+  static void _stopTimer(
+      Store<AppState> store, TimerAction action, NextDispatcher next) {
+    next(action);
+
+    _timer.pause();
+    store.dispatch(UpdateIsRunningValue(_timer.isRunning));
+  }
+
+  static void _updateTime(Store<AppState> store, duration) {
+    store.dispatch(UpdateTimeValue(duration));
+  }
 }
