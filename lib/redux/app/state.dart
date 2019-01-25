879d25c335a9ecbc0c21a50273c4f858c6d381ff
Anna Domashych <doan@ciklum.com>
2019-01-25 12:54:57 +0200

    Update AppState with QuestionsState

--- lib/redux/app/state.dart
+++ lib/redux/app/state.dart
@@ -1,11 +1,16 @@
 import 'package:meta/meta.dart';
+import 'package:what_when_where/redux/questions/state.dart';
 import 'package:what_when_where/redux/timer/state.dart';
 
 @immutable
 class AppState {
   final TimerState timerState;
+  final QuestionsState questionsState;
 
-  AppState({this.timerState});
+  AppState({this.timerState, this.questionsState});
 
-  AppState.initial() : this(timerState: TimerState.initial());
+  AppState.initial()
+      : this(
+            timerState: TimerState.initial(),
+            questionsState: QuestionsState.initial());
 }
