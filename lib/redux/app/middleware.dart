8da9b75b466db67b01e6b74056b25616b913dfe0
Anna Domashych <doan@ciklum.com>
2019-01-25 14:28:21 +0200

    Include ShareMiddleware in global list

--- lib/redux/app/middleware.dart
+++ lib/redux/app/middleware.dart
@@ -1,10 +1,12 @@
 import 'package:redux/redux.dart';
 import 'package:what_when_where/redux/app/state.dart';
 import 'package:what_when_where/redux/logs/middleware.dart';
+import 'package:what_when_where/redux/sharing/middleware.dart';
 import 'package:what_when_where/redux/timer/middleware.dart';
 
 class AppMiddleware {
   static final middleware = List<Middleware<AppState>>()
     ..addAll(TimerMiddleware.middleware)
-    ..addAll(LogsMiddleware.middleware);
+    ..addAll(LogsMiddleware.middleware)
+    ..addAll(ShareMiddleware.middleware);
 }
