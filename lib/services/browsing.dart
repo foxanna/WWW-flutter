1c3cd3190a99da1c1f2c0bd8dee0892ff5136c73
Anna Domashych <doan@ciklum.com>
2019-01-25 14:59:47 +0200

    Improve BrowsingService

--- lib/services/browsing.dart
+++ lib/services/browsing.dart
@@ -1,7 +1,6 @@
 import 'package:what_when_where/db_chgk_info/models/question.dart';
 import 'package:what_when_where/db_chgk_info/models/tour.dart';
 import 'package:what_when_where/db_chgk_info/models/tournament.dart';
-import 'package:what_when_where/services/analytics.dart';
 import 'package:what_when_where/services/url_launcher.dart';
 
 class BrowsingService {
@@ -11,18 +10,10 @@ class BrowsingService {
 
   BrowsingService._();
 
-  void browseTournament(Tournament tournament) {
-    AnalyticsService().logEvent(name: 'browse_tournament');
-    UrlLauncher.launchURL(tournament.url);
-  }
+  void browseTournament(Tournament tournament) =>
+      UrlLauncher.launchURL(tournament.url);
 
-  void browseTour(Tour tour) {
-    AnalyticsService().logEvent(name: 'browse_tour');
-    UrlLauncher.launchURL(tour.url);
-  }
+  void browseTour(Tour tour) => UrlLauncher.launchURL(tour.url);
 
-  void browseQuestion(Question question) {
-    AnalyticsService().logEvent(name: 'browse_question');
-    UrlLauncher.launchURL(question.url);
-  }
+  void browseQuestion(Question question) => UrlLauncher.launchURL(question.url);
 }
