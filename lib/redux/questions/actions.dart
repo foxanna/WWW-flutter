4932227834329a1b3cc9f8485ec17642121f3698
Anna Domashych <doan@ciklum.com>
2019-01-25 12:54:12 +0200

    Create actions for questions module

---
+++ lib/redux/questions/actions.dart
@@ -0,0 +1,33 @@
+import 'package:meta/meta.dart';
+import 'package:what_when_where/db_chgk_info/models/question.dart';
+
+abstract class QuestionAction {}
+
+@immutable
+class SetQuestions extends QuestionAction {
+  final Iterable<Question> questions;
+  final int selectedQuestionIndex;
+
+  SetQuestions({this.questions, this.selectedQuestionIndex});
+}
+
+@immutable
+class SelectQuestion extends QuestionAction {
+  final int questionIndex;
+
+  SelectQuestion(this.questionIndex);
+}
+
+@immutable
+class ShowAnswer extends QuestionAction {
+  final int questionIndex;
+
+  ShowAnswer(this.questionIndex);
+}
+
+@immutable
+class HideAnswer extends QuestionAction {
+  final int questionIndex;
+
+  HideAnswer(this.questionIndex);
+}
