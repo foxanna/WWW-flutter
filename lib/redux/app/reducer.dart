d0e3e4436dfe7a5905912ec97d2eabf2863b9400
Anna Domashych <doan@ciklum.com>
2019-01-25 12:55:23 +0200

    Update AppReducer with QuestionsReducer

--- lib/redux/app/reducer.dart
+++ lib/redux/app/reducer.dart
@@ -1,12 +1,14 @@
 import 'package:redux/redux.dart';
 import 'package:what_when_where/redux/app/state.dart';
+import 'package:what_when_where/redux/questions/reducer.dart';
 import 'package:what_when_where/redux/timer/reducer.dart';
 
 class AppReducer {
   static final Reducer<AppState> reducer =
       TypedReducer<AppState, dynamic>(_reduce);
 
-  static AppState _reduce(state, action) => AppState(
+  static AppState _reduce(AppState state, action) => AppState(
         timerState: TimerReducer.reduce(state.timerState, action),
+        questionsState: QuestionsReducer.reduce(state.questionsState, action),
       );
 }
