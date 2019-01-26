7c32890e999816d0ec1838baebb76a88933cacc0
Anna Domashych <doan@ciklum.com>
2019-01-26 19:34:00 +0200

    Use TourCache in TourDetailsLoader

--- lib/db_chgk_info/loaders/tour_details_loader.dart
+++ lib/db_chgk_info/loaders/tour_details_loader.dart
@@ -1,3 +1,4 @@
+import 'package:what_when_where/db_chgk_info/cache/tour_cache.dart';
 import 'package:what_when_where/db_chgk_info/http_client.dart';
 import 'package:what_when_where/db_chgk_info/models/tour.dart';
 
@@ -8,9 +9,14 @@ class TourDetailsLoader {
 
   TourDetailsLoader._internal();
 
+  final _cache = TourCache();
+
   Future<Tour> get(String id) async {
+    if (_cache.contains(id)) return _cache.get(id);
+
     var map = await HttpClient().get(Uri(path: '/tour/$id/xml'));
     var tour = Tour.fromJson(map['tournament']);
+    _cache.save(tour);
     return tour;
   }
 }
