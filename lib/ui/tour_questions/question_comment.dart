def060bb5010f66c9421279e9c9f1e2724a7c50b
Anna Domashych <doan@ciklum.com>
2019-01-26 09:56:26 +0200

    Create QuestionComment widget with TextSections inside

---
+++ lib/ui/tour_questions/question_comment.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:what_when_where/services/question_parser/question_parser.dart';
+import 'package:what_when_where/ui/common/text_sections.dart';
+
+class QuestionComment extends StatelessWidget {
+  final List<dynamic> _sections;
+
+  QuestionComment({Key key, String text})
+      : _sections = QuestionParser.split(text).toList(),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var textTheme = Theme.of(context).textTheme;
+
+    return TextSections(
+        sections: _sections,
+        speakersNoteStyle: textTheme.body1.copyWith(
+          fontStyle: FontStyle.italic,
+          color: textTheme.body2.color,
+        ),
+        textStyle: textTheme.body1);
+  }
+}
