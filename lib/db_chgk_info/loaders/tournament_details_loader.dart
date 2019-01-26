7b24d1df1ffef59b1693f630b03c74d63638297f
Anna Domashych <doan@ciklum.com>
2019-01-26 19:34:44 +0200

    Use TournamentCache in TournamentDetailsLoader

--- lib/db_chgk_info/loaders/tournament_details_loader.dart
+++ lib/db_chgk_info/loaders/tournament_details_loader.dart
@@ -1,3 +1,4 @@
+import 'package:what_when_where/db_chgk_info/cache/tournament_cache.dart';
 import 'package:what_when_where/db_chgk_info/http_client.dart';
 import 'package:what_when_where/db_chgk_info/models/tournament.dart';
 
@@ -9,9 +10,14 @@ class TournamentDetailsLoader {
 
   TournamentDetailsLoader._internal();
 
+  final _cache = TournamentCache();
+
   Future<Tournament> get(String id) async {
+    if (_cache.contains(id)) return _cache.get(id);
+
     var map = await HttpClient().get(Uri(path: '/tour/$id/xml'));
     var tournament = Tournament.fromJson(map['tournament']);
+    _cache.save(tournament);
     return tournament;
   }
 }
