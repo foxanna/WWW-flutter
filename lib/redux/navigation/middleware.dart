7f16b7653667558dc97dd67d9c60dd68d5d00413
Anna Domashych <doan@ciklum.com>
2019-01-26 10:56:01 +0200

    Handle OpenTourInfo action

--- lib/redux/navigation/middleware.dart
+++ lib/redux/navigation/middleware.dart
@@ -3,10 +3,12 @@ import 'package:redux/redux.dart';
 import 'package:what_when_where/redux/app/state.dart';
 import 'package:what_when_where/redux/navigation/actions.dart';
 import 'package:what_when_where/ui/image/image_page.dart';
+import 'package:what_when_where/ui/tour_questions/tour_details_about_dialog.dart';
 
 class NavigationMiddleware {
   static final List<Middleware<AppState>> middleware = [
     TypedMiddleware<AppState, OpenImage>(_openImage),
+    TypedMiddleware<AppState, OpenTourInfo>(_openTourInfo),
   ];
 
   static void _openImage(
@@ -17,6 +19,13 @@ class NavigationMiddleware {
         context: action.context,
         routeName: ImagePage.routeName,
         builder: (context) => ImagePage(url: action.imageUrl));
+  }
+
+  static void _openTourInfo(
+      Store<AppState> store, OpenTourInfo action, NextDispatcher next) {
+    next(action);
+
+    TourDetailsAboutDialog(tour: action.tour).show(action.context);
   }
 
   static void _navigateTo(
