5911d3d6c76b82bc1dc3cd84fe94ab01ad3e8429
Anna Domashych <doan@ciklum.com>
2019-01-29 16:43:03 +0200

    Add ToursState to AppState

--- lib/redux/app/state.dart
+++ lib/redux/app/state.dart
@@ -1,16 +1,20 @@
 import 'package:meta/meta.dart';
 import 'package:what_when_where/redux/questions/state.dart';
 import 'package:what_when_where/redux/timer/state.dart';
+import 'package:what_when_where/redux/tours/state.dart';
 
 @immutable
 class AppState {
   final TimerState timerState;
   final QuestionsState questionsState;
+  final ToursState toursState;
 
-  AppState({this.timerState, this.questionsState});
+  AppState({this.timerState, this.questionsState, this.toursState});
 
   AppState.initial()
       : this(
-            timerState: TimerState.initial(),
-            questionsState: QuestionsState.initial());
+          timerState: TimerState.initial(),
+          toursState: ToursState.initial(),
+          questionsState: QuestionsState.initial(),
+        );
 }
