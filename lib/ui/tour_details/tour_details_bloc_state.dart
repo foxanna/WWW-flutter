bbef4a076854db7c552a3018687279cf0455488a
Anna Domashych <doan@ciklum.com>
2019-01-29 17:30:06 +0200

    Remove unused code

--- lib/ui/tour_details/tour_details_bloc_state.dart
+++
@@ -1,8 +0,0 @@
-import 'package:what_when_where/db_chgk_info/models/tour.dart';
-import 'package:what_when_where/ui/common/loading_state.dart';
-
-class TourDetailsBlocState extends LoadingState<Tour> {
-  TourDetailsBlocState.withError(Exception error) : super.withError(error);
-  TourDetailsBlocState.withLoading() : super.withLoading();
-  TourDetailsBlocState.withResult(Tour tour) : super.withResult(tour);
-}
