4b19d70dcfa05c745c994ad97dfdf501670c03c7
Anna Domashych <doan@ciklum.com>
2019-01-29 17:29:50 +0200

    Update ToursState when tour details arrive

--- lib/redux/tours/reducer.dart
+++ lib/redux/tours/reducer.dart
@@ -1,12 +1,16 @@
 import 'package:redux/redux.dart';
 import 'package:what_when_where/redux/tours/actions.dart';
 import 'package:what_when_where/redux/tours/state.dart';
+import 'package:what_when_where/utils/extensions.dart';
 
 class ToursReducer {
   static final Reducer<ToursState> _reducer = combineReducers<ToursState>([
     TypedReducer<ToursState, SetTours>(_setTours),
     TypedReducer<ToursState, VoidTours>(_voidTours),
     TypedReducer<ToursState, SelectTour>(_selectTour),
+    TypedReducer<ToursState, TourIsLoading>(_updateTourIsLoading),
+    TypedReducer<ToursState, TourLoaded>(_updateTourLoaded),
+    TypedReducer<ToursState, TourFailedLoading>(_updateTourFailed),
   ]);
 
   static ToursState reduce(ToursState state, action) => _reducer(state, action);
@@ -19,4 +23,46 @@ class ToursReducer {
 
   static ToursState _selectTour(ToursState state, SelectTour action) =>
       state.copyWith(currentTourIndex: action.tourIndex);
+
+  static ToursState _updateTourIsLoading(
+          ToursState state, TourIsLoading action) =>
+      _replaceTour(
+          state,
+          action.tourId,
+          (tourState) => tourState.copyWith(
+                isLoading: true,
+                error: null,
+              ));
+
+  static ToursState _updateTourLoaded(ToursState state, TourLoaded action) =>
+      _replaceTour(
+          state,
+          action.tour.id,
+          (tourState) => tourState.copyWith(
+                tour: action.tour,
+                isLoading: false,
+                error: null,
+              ));
+
+  static ToursState _updateTourFailed(
+          ToursState state, TourFailedLoading action) =>
+      _replaceTour(
+          state,
+          action.tourId,
+          (tourState) => tourState.copyWith(
+                isLoading: false,
+                error: action.exception,
+              ));
+
+  static ToursState _replaceTour(
+      ToursState state, String tourId, TourState Function(TourState) function) {
+    var tourIndex =
+        state.tours.indexWhere((tourState) => tourState.tour.id == tourId);
+    if (tourIndex == -1) return state;
+
+    var newTourState = function(state.tours[tourIndex]);
+    return state.copyWith(
+        tours:
+            IterableExtensions.replaceAt(state.tours, tourIndex, newTourState));
+  }
 }
