ebeca45d39c30c8e6ddd1a6ad01e7cde8def65fb
Anna Domashych <doan@ciklum.com>
2019-01-30 21:28:59 +0200

    Add TournamentState to global state

--- lib/redux/app/state.dart
+++ lib/redux/app/state.dart
@@ -1,6 +1,7 @@
 import 'package:meta/meta.dart';
 import 'package:what_when_where/redux/questions/state.dart';
 import 'package:what_when_where/redux/timer/state.dart';
+import 'package:what_when_where/redux/tornament/state.dart';
 import 'package:what_when_where/redux/tours/state.dart';
 
 @immutable
@@ -8,12 +9,19 @@ class AppState {
   final TimerState timerState;
   final QuestionsState questionsState;
   final ToursState toursState;
+  final TournamentState tournamentState;
 
-  AppState({this.timerState, this.questionsState, this.toursState});
+  AppState({
+    this.timerState,
+    this.tournamentState,
+    this.toursState,
+    this.questionsState,
+  });
 
   AppState.initial()
       : this(
           timerState: TimerState.initial(),
+          tournamentState: TournamentState.initial(),
           toursState: ToursState.initial(),
           questionsState: QuestionsState.initial(),
         );
