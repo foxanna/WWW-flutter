3f40bfa5c3f1c63505651e4771db893993978afc
Anna Domashych <doan@ciklum.com>
2019-02-01 21:56:16 +0200

    Use Strings. value for default tour name

--- lib/db_chgk_info/loaders/tournament_details_loader.dart
+++ lib/db_chgk_info/loaders/tournament_details_loader.dart
@@ -3,6 +3,7 @@ import 'package:what_when_where/db_chgk_info/cache/tournament_cache.dart';
 import 'package:what_when_where/db_chgk_info/http_client.dart';
 import 'package:what_when_where/db_chgk_info/models/tour.dart';
 import 'package:what_when_where/db_chgk_info/models/tournament.dart';
+import 'package:what_when_where/resources/strings.dart';
 
 class TournamentDetailsLoader {
   static final TournamentDetailsLoader _instance =
@@ -31,7 +32,7 @@ class TournamentDetailsLoader {
   void _handleTourlessTournament(Map<String, dynamic> map) {
     if (!map.containsKey('tour')) {
       var tourMap = Map<String, dynamic>.from(map);
-      tourMap['Title'] = '1 тур';
+      tourMap['Title'] = Strings.firstTour;
       tourMap['ParentId'] = map['Id'];
 
       var tour = Tour.fromJson(tourMap);
