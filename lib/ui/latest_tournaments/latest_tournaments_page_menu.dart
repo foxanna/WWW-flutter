9c6a223fe846abd9c76cbfc9cdd2c555711aa442
Anna Domashych <doan@ciklum.com>
2019-02-04 14:34:07 +0200

    Create LatestTournamentsMoreIconButton

--- lib/ui/latest_tournaments/latest_tournaments_page_menu.dart
+++ lib/ui/latest_tournaments/latest_tournaments_page_menu.dart
@@ -4,13 +4,12 @@ import 'package:what_when_where/redux/app/state.dart';
 import 'package:what_when_where/redux/navigation/actions.dart';
 import 'package:what_when_where/resources/strings.dart';
 
-class LatestTournamentsPageMenu {
-  List<Widget> createMenuActions(BuildContext context) => <Widget>[
-        IconButton(
-          icon: const Icon(Icons.more_vert),
-          onPressed: () => _showMenu(context),
-        ),
-      ];
+class LatestTournamentsMoreIconButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => IconButton(
+        icon: const Icon(Icons.more_vert),
+        onPressed: () => _showMenu(context),
+      );
 
   void _showMenu(BuildContext context) => showModalBottomSheet<Object>(
       context: context,
