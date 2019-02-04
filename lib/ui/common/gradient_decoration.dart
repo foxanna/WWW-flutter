e38552aade5be99020886179266a6b657146a163
Anna Domashych <doan@ciklum.com>
2019-02-04 17:49:48 +0200

    Extract GradientDecoration

---
+++ lib/ui/common/gradient_decoration.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class GradientDecoration extends BoxDecoration {
+  factory GradientDecoration({@required Color color}) {
+    final gradientEnd = color;
+    final gradientStart = color.withAlpha(0);
+    const gradientSize = 0.05;
+    final gradient = LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [gradientEnd, gradientStart, gradientStart, gradientEnd],
+        stops: const [0, gradientSize, 1 - gradientSize, 1]);
+    return GradientDecoration._(gradient: gradient);
+  }
+
+  const GradientDecoration._({Gradient gradient}) : super(gradient: gradient);
+}
