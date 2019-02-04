5098174c9e4f865a7b7343c291cdc937cc88a3a9
Anna Domashych <doan@ciklum.com>
2019-02-04 14:34:24 +0200

    Extract LatestTournamentsAppBar

--- lib/ui/latest_tournaments/latest_tournaments_page.dart
+++ lib/ui/latest_tournaments/latest_tournaments_page.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:what_when_where/resources/dimensions.dart';
 import 'package:what_when_where/ui/common/error_message.dart';
 import 'package:what_when_where/ui/common/progress_indicator.dart';
 import 'package:what_when_where/ui/latest_tournaments/latest_tournaments_bloc.dart';
 import 'package:what_when_where/ui/latest_tournaments/latest_tournaments_bloc_state.dart';
 import 'package:what_when_where/ui/latest_tournaments/latest_tournaments_grid.dart';
-import 'package:what_when_where/ui/latest_tournaments/latest_tournaments_page_menu.dart';
+import 'package:what_when_where/ui/latest_tournaments/latest_tournaments_page_appbar.dart';
 
 class LatestTournamentsPage extends StatefulWidget {
   static const String routeName = 'latest_tournaments';
@@ -19,9 +18,6 @@ class _LatestTournamentsPageState extends State<LatestTournamentsPage> {
   final _bloc = LatestTournamentsBloc();
   final _refreshIndicatorKey = GlobalKey<RefreshIndicatorState>();
   final _scrollController = ScrollController();
-  final _menu = LatestTournamentsPageMenu();
-
-  static const _appBarHeight = 140.0;
 
   @override
   Widget build(BuildContext context) => Scaffold(
@@ -39,7 +35,7 @@ class _LatestTournamentsPageState extends State<LatestTournamentsPage> {
       StreamBuilder<LatestTournamentsBlocState>(
         stream: _bloc.stateStream,
         builder: (context, snapshot) {
-          final children = [_buildSliverAppBar(context)];
+          final children = <Widget>[LatestTournamentsAppBar()];
 
           if (snapshot.hasData) {
             final state = snapshot.data;
@@ -57,14 +53,16 @@ class _LatestTournamentsPageState extends State<LatestTournamentsPage> {
               if (state.isLoading) {
                 children.add(const SliverToBoxAdapter(
                     child: Padding(
-                  padding: EdgeInsets.only(top: _appBarHeight),
+                  padding: EdgeInsets.only(
+                      top: LatestTournamentsAppBar.appBarHeight),
                   child: WWWProgressIndicator(),
                 )));
               }
               if (state.hasError) {
                 children.add(SliverToBoxAdapter(
                     child: Padding(
-                  padding: const EdgeInsets.only(top: _appBarHeight),
+                  padding: const EdgeInsets.only(
+                      top: LatestTournamentsAppBar.appBarHeight),
                   child: ErrorMessage(
                     retryFunction: _loadMore,
                     color: Theme.of(context).primaryIconTheme.color,
@@ -79,31 +77,6 @@ class _LatestTournamentsPageState extends State<LatestTournamentsPage> {
             slivers: children,
           );
         },
-      );
-
-  Widget _buildSliverAppBar(BuildContext context) => SliverAppBar(
-        floating: true,
-        snap: true,
-        expandedHeight: _appBarHeight,
-        flexibleSpace: FlexibleSpaceBar(
-          title: Hero(
-            tag: 'owl',
-            child: SvgPicture.asset(
-              'assets/owl.svg',
-              height: 60,
-              color: Theme.of(context).primaryIconTheme.color,
-            ),
-          ),
-        ),
-        bottom: PreferredSize(
-            child: IconTheme.merge(
-              data: Theme.of(context).primaryIconTheme,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: _menu.createMenuActions(context),
-              ),
-            ),
-            preferredSize: const Size.fromHeight(kToolbarHeight)),
       );
 
   @override
