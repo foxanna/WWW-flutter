5da860003a59bfa327dd63492f34873afddda969
Anna Domashych <doan@ciklum.com>
2019-02-04 14:09:54 +0200

    Refactor

--- lib/ui/latest_tournaments/latest_tournaments_page_menu.dart
+++ lib/ui/latest_tournaments/latest_tournaments_page_menu.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:what_when_where/redux/app/state.dart';
+import 'package:what_when_where/redux/navigation/actions.dart';
 import 'package:what_when_where/resources/strings.dart';
-import 'package:what_when_where/ui/about/about_page.dart';
 
 class LatestTournamentsPageMenu {
-  List<Widget> createAppBarMenuActions(BuildContext context) => <Widget>[
+  List<Widget> createMenuActions(BuildContext context) => <Widget>[
         IconButton(
-          icon: Icon(Icons.more_vert),
+          icon: const Icon(Icons.more_vert),
           onPressed: () => _showMenu(context),
         ),
       ];
@@ -14,21 +16,19 @@ class LatestTournamentsPageMenu {
       context: context,
       builder: (context) => Column(
             mainAxisSize: MainAxisSize.min,
-            children: [
-              ListTile(
-                leading: const Icon(Icons.info_outline),
-                title: const Text(Strings.aboutApplication),
-                onTap: () => _openAboutPage(context),
-              )
-            ],
+            children: [_AboutBottomSheetItem()],
           ));
+}
 
-  static void _openAboutPage(BuildContext context) {
-    Navigator.pop(context);
-    Navigator.push<Object>(
-        context,
-        MaterialPageRoute(
-            settings: const RouteSettings(name: AboutPage.routeName),
-            builder: (context) => AboutPage()));
-  }
+class _AboutBottomSheetItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => ListTile(
+        leading: const Icon(Icons.info_outline),
+        title: const Text(Strings.aboutApplication),
+        onTap: () {
+          Navigator.pop(context);
+
+          StoreProvider.of<AppState>(context).dispatch(OpenAboutPage(context));
+        },
+      );
 }
