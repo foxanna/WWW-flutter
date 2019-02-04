0ad1046ca5a756a9a85b852378cc45fcd948d1ed
Anna Domashych <doan@ciklum.com>
2019-02-04 16:51:28 +0200

    Create Misc actions

---
+++ lib/redux/misc/actions.dart
@@ -0,0 +1,13 @@
+import 'package:meta/meta.dart';
+
+@immutable
+class EmailDevelopers {
+  @override
+  String toString() => '$EmailDevelopers';
+}
+
+@immutable
+class BrowseDatabase {
+  @override
+  String toString() => '$BrowseDatabase';
+}
