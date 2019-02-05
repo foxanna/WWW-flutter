0ddff400517189d98c8285e54c7c188514740b66
Anna Domashych <doan@ciklum.com>
2019-02-05 20:07:41 +0200

    Fix RefreshIndicator

--- lib/ui/latest_tournaments/latest_tournaments_page.dart
+++ lib/ui/latest_tournaments/latest_tournaments_page.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:tuple/tuple.dart';
 import 'package:what_when_where/redux/app/state.dart';
 import 'package:what_when_where/redux/latest/actions.dart';
 import 'package:what_when_where/redux/latest/state.dart';
@@ -8,6 +11,7 @@ import 'package:what_when_where/ui/common/error_message.dart';
 import 'package:what_when_where/ui/common/progress_indicator.dart';
 import 'package:what_when_where/ui/latest_tournaments/latest_tournaments_grid.dart';
 import 'package:what_when_where/ui/latest_tournaments/latest_tournaments_page_appbar.dart';
+import 'package:what_when_where/utils/function_holder.dart';
 
 class LatestTournamentsPage extends StatefulWidget {
   static const String routeName = 'latest_tournaments';
@@ -20,23 +24,90 @@ class _LatestTournamentsPageState extends State<LatestTournamentsPage> {
   final _refreshIndicatorKey = GlobalKey<RefreshIndicatorState>();
   final _scrollController = ScrollController();
 
+  Completer _completer = Completer<dynamic>();
+
   @override
   Widget build(BuildContext context) => Scaffold(
         backgroundColor: Theme.of(context).primaryColor,
         body: SafeArea(
-          child: RefreshIndicator(
-            key: _refreshIndicatorKey,
-            onRefresh: _refresh,
-            child: _build(context),
-          ),
+          child: _buildRefreshIndicator(),
         ),
       );
 
-  Widget _build(BuildContext context) =>
-      StoreConnector<AppState, LatestTournamentsState>(
+  Widget _buildRefreshIndicator() =>
+      StoreConnector<AppState, Tuple2<bool, FunctionHolder>>(
         distinct: true,
-        converter: (store) => store.state.latestTournamentsState,
-        builder: (context, state) {
+        converter: (store) => Tuple2(
+            store.state.latestTournamentsState.isRefreshing,
+            FunctionHolder(() => store.dispatch(RefreshLatestTournaments()))),
+        builder: (context, data) {
+          final isRefreshing = data.item1;
+          final refreshFunctionHolder = data.item2;
+
+          if (!isRefreshing) {
+            _completer.complete();
+            _completer = Completer<dynamic>();
+          }
+
+          return RefreshIndicator(
+            key: _refreshIndicatorKey,
+            onRefresh: () async {
+              refreshFunctionHolder.function();
+              await _completer.future;
+              _loadMoreIfRequested();
+            },
+            child: _LatestTournamentsPageBody(
+              scrollController: _scrollController,
+            ),
+          );
+        },
+        onInit: (store) => _loadMore(),
+      );
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_scrollListener);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_scrollListener);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _scrollListener() => _loadMoreIfRequested();
+
+  void _loadMoreIfRequested() {
+    if (_scrollController.position.extentAfter < 500) {
+      _loadMore();
+    }
+  }
+
+  void _loadMore() {
+    final store = StoreProvider.of<AppState>(context);
+    store.dispatch(LoadMoreLatestTournaments());
+  }
+}
+
+class _LatestTournamentsPageBody extends StatelessWidget {
+  final ScrollController _scrollController;
+
+  const _LatestTournamentsPageBody({Key key, ScrollController scrollController})
+      : this._scrollController = scrollController,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) =>
+      StoreConnector<AppState, Tuple2<LatestTournamentsState, FunctionHolder>>(
+        distinct: true,
+        converter: (store) => Tuple2(store.state.latestTournamentsState,
+            FunctionHolder(() => store.dispatch(LoadMoreLatestTournaments()))),
+        builder: (context, data) {
+          final state = data.item1;
+          final retryFunctionHolder = data.item2;
+
           final children = <Widget>[LatestTournamentsAppBar()];
 
           if (state.data.isNotEmpty) {
@@ -63,7 +134,7 @@ class _LatestTournamentsPageState extends State<LatestTournamentsPage> {
                 padding: const EdgeInsets.only(
                     top: LatestTournamentsAppBar.appBarHeight),
                 child: ErrorMessage(
-                  retryFunction: _loadMore,
+                  retryFunction: () => retryFunctionHolder.function(),
                   color: Theme.of(context).primaryIconTheme.color,
                 ),
               )));
@@ -75,44 +146,4 @@ class _LatestTournamentsPageState extends State<LatestTournamentsPage> {
           );
         },
       );
-
-  @override
-  void initState() {
-    super.initState();
-    _scrollController.addListener(_scrollListener);
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-
-    _loadMore();
-  }
-
-  @override
-  void dispose() {
-    _scrollController.removeListener(_scrollListener);
-    _scrollController.dispose();
-    super.dispose();
-  }
-
-  void _scrollListener() => _loadMoreIfRequested();
-
-  void _loadMoreIfRequested() {
-    if (_scrollController.position.extentAfter < 500) {
-      _loadMore();
-    }
-  }
-
-  void _loadMore() {
-    final store = StoreProvider.of<AppState>(context);
-    store.dispatch(LoadMoreLatestTournaments());
-  }
-
-  Future _refresh() async {
-    final store = StoreProvider.of<AppState>(context);
-    store.dispatch(RefreshLatestTournaments());
-
-    _loadMoreIfRequested();
-  }
 }
