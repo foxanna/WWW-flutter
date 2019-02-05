ea57976417526e7dd91815f4d8da2fe08666f082
Anna Domashych <doan@ciklum.com>
2019-02-05 10:30:06 +0200

    Remove BackIconButton

--- lib/ui/tournament_details/back_icon_button.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class BackIconButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) => IconButton(
-        icon: const Icon(Icons.arrow_back),
-        onPressed: () => Navigator.pop(context),
-      );
-}
