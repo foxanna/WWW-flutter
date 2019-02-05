952edb58f5f9d02a135ed89eafcc9755f3b7b010
Anna Domashych <doan@ciklum.com>
2019-02-05 17:05:22 +0200

    Add LatestTournamentsState to global state

--- lib/redux/app/state.dart
+++ lib/redux/app/state.dart
@@ -1,4 +1,5 @@
 import 'package:meta/meta.dart';
+import 'package:what_when_where/redux/latest/state.dart';
 import 'package:what_when_where/redux/questions/state.dart';
 import 'package:what_when_where/redux/timer/state.dart';
 import 'package:what_when_where/redux/tornament/state.dart';
@@ -10,9 +11,11 @@ class AppState {
   final QuestionsState questionsState;
   final ToursState toursState;
   final TournamentState tournamentState;
+  final LatestTournamentsState latestTournamentsState;
 
   const AppState({
     this.timerState,
+    this.latestTournamentsState,
     this.tournamentState,
     this.toursState,
     this.questionsState,
@@ -21,6 +24,7 @@ class AppState {
   AppState.initial()
       : this(
           timerState: const TimerState.initial(),
+          latestTournamentsState: LatestTournamentsState.initial(),
           tournamentState: const TournamentState.initial(),
           toursState: ToursState.initial(),
           questionsState: QuestionsState.initial(),
