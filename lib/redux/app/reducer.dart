415f9255b78807f5b18f00cd732044257ea33f65
Anna Domashych <doan@ciklum.com>
2019-02-05 17:05:43 +0200

    Use LatestTournamentsReducer

--- lib/redux/app/reducer.dart
+++ lib/redux/app/reducer.dart
@@ -1,5 +1,6 @@
 import 'package:redux/redux.dart';
 import 'package:what_when_where/redux/app/state.dart';
+import 'package:what_when_where/redux/latest/reducer.dart';
 import 'package:what_when_where/redux/questions/reducer.dart';
 import 'package:what_when_where/redux/timer/reducer.dart';
 import 'package:what_when_where/redux/tornament/reducer.dart';
@@ -11,6 +12,8 @@ class AppReducer {
 
   static AppState _reduce(AppState state, dynamic action) => AppState(
         timerState: TimerReducer.reduce(state.timerState, action),
+        latestTournamentsState: LatestTournamentsReducer.reduce(
+            state.latestTournamentsState, action),
         tournamentState:
             TournamentReducer.reduce(state.tournamentState, action),
         toursState: ToursReducer.reduce(state.toursState, action),
