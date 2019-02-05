cda789a26987b6c0fc5275e0e91345aeb9af193d
Anna Domashych <doan@ciklum.com>
2019-02-05 17:05:34 +0200

    Create LatestTournamentsReducer

---
+++ lib/redux/latest/reducer.dart
@@ -0,0 +1,60 @@
+import 'package:redux/redux.dart';
+import 'package:what_when_where/db_chgk_info/models/tournament.dart';
+import 'package:what_when_where/redux/latest/actions.dart';
+import 'package:what_when_where/redux/latest/state.dart';
+
+class LatestTournamentsReducer {
+  static final Reducer<LatestTournamentsState> _reducer =
+      combineReducers<LatestTournamentsState>([
+    TypedReducer<LatestTournamentsState, ClearLatestTournaments>(_onClear),
+    TypedReducer<LatestTournamentsState, LatestTournamentsIsRefreshing>(
+        _onRefreshing),
+    TypedReducer<LatestTournamentsState, LatestTournamentsIsLoadingMore>(
+        _onLoadingMore),
+    TypedReducer<LatestTournamentsState, MoreLatestTournamentsLoaded>(
+        _onLoaded),
+    TypedReducer<LatestTournamentsState, LatestTournamentsLoadFailed>(
+        _onLoadFailed),
+  ]);
+
+  static LatestTournamentsState reduce(
+          LatestTournamentsState state, dynamic action) =>
+      _reducer(state, action);
+
+  static LatestTournamentsState _onClear(
+          LatestTournamentsState state, ClearLatestTournaments action) =>
+      state.copyWith(data: <Tournament>[]);
+
+  static LatestTournamentsState _onRefreshing(
+          LatestTournamentsState state, LatestTournamentsIsRefreshing action) =>
+      state.copyWith(
+        isRefreshing: true,
+        isLoadingMore: false,
+        error: null,
+      );
+
+  static LatestTournamentsState _onLoadingMore(LatestTournamentsState state,
+          LatestTournamentsIsLoadingMore action) =>
+      state.copyWith(
+        isRefreshing: false,
+        isLoadingMore: true,
+        error: null,
+      );
+
+  static LatestTournamentsState _onLoaded(
+          LatestTournamentsState state, MoreLatestTournamentsLoaded action) =>
+      state.copyWith(
+        data: <Tournament>[]..addAll(state.data)..addAll(action.data),
+        isRefreshing: false,
+        isLoadingMore: false,
+        error: null,
+      );
+
+  static LatestTournamentsState _onLoadFailed(
+          LatestTournamentsState state, LatestTournamentsLoadFailed action) =>
+      state.copyWith(
+        isRefreshing: false,
+        isLoadingMore: false,
+        error: action.exception,
+      );
+}
