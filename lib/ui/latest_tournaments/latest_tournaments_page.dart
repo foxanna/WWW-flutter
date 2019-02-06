f43652e75deea936cd6282681f0c0ce79710ca92
Anna Domashych <doan@ciklum.com>
2019-02-06 10:23:02 +0200

    Split LatestTournamentsPageBody into separate widgets

--- lib/ui/latest_tournaments/latest_tournaments_page.dart
+++ lib/ui/latest_tournaments/latest_tournaments_page.dart
@@ -1,16 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
-import 'package:tuple/tuple.dart';
 import 'package:what_when_where/redux/app/state.dart';
 import 'package:what_when_where/redux/latest/actions.dart';
-import 'package:what_when_where/redux/latest/state.dart';
-import 'package:what_when_where/resources/dimensions.dart';
-import 'package:what_when_where/ui/common/error_message.dart';
-import 'package:what_when_where/ui/common/progress_indicator.dart';
 import 'package:what_when_where/ui/latest_tournaments/latest_tournament_page_refresh_indicator.dart';
-import 'package:what_when_where/ui/latest_tournaments/latest_tournaments_grid.dart';
-import 'package:what_when_where/ui/latest_tournaments/latest_tournaments_page_appbar.dart';
-import 'package:what_when_where/utils/function_holder.dart';
+import 'package:what_when_where/ui/latest_tournaments/latest_tournaments_page_body.dart';
 
 class LatestTournamentsPage extends StatefulWidget {
   static const String routeName = 'latest_tournaments';
@@ -33,7 +26,7 @@ class _LatestTournamentsPageState extends State<LatestTournamentsPage> {
         backgroundColor: Theme.of(context).primaryColor,
         body: SafeArea(
           child: LatestTournamentsPageRefreshIndicator(
-            child: _LatestTournamentsPageBody(
+            child: LatestTournamentsPageBody(
               scrollController: _scrollController,
             ),
             onInit: _loadMore,
@@ -61,61 +54,4 @@ class _LatestTournamentsPageState extends State<LatestTournamentsPage> {
     final store = StoreProvider.of<AppState>(context);
     store.dispatch(LoadMoreLatestTournaments());
   }
-}
-
-class _LatestTournamentsPageBody extends StatelessWidget {
-  final ScrollController _scrollController;
-
-  const _LatestTournamentsPageBody({Key key, ScrollController scrollController})
-      : this._scrollController = scrollController,
-        super(key: key);
-
-  @override
-  Widget build(BuildContext context) =>
-      StoreConnector<AppState, Tuple2<LatestTournamentsState, FunctionHolder>>(
-        distinct: true,
-        converter: (store) => Tuple2(store.state.latestTournamentsState,
-            FunctionHolder(() => store.dispatch(LoadMoreLatestTournaments()))),
-        builder: (context, data) {
-          final state = data.item1;
-          final retryFunctionHolder = data.item2;
-
-          final children = <Widget>[LatestTournamentsAppBar()];
-
-          if (state.data.isNotEmpty) {
-            children.add(SliverPadding(
-              sliver: LatestTournamentsGrid(tournaments: state.data),
-              padding: Dimensions.defaultPadding,
-            ));
-            if (state.isLoadingMore) {
-              children
-                  .add(const SliverToBoxAdapter(child: WWWProgressIndicator()));
-            }
-          } else {
-            if (state.isLoading) {
-              children.add(const SliverToBoxAdapter(
-                  child: Padding(
-                padding:
-                    EdgeInsets.only(top: LatestTournamentsAppBar.appBarHeight),
-                child: WWWProgressIndicator(),
-              )));
-            }
-            if (state.hasError) {
-              children.add(SliverToBoxAdapter(
-                  child: Padding(
-                padding: const EdgeInsets.only(
-                    top: LatestTournamentsAppBar.appBarHeight),
-                child: ErrorMessage(
-                  retryFunction: () => retryFunctionHolder.function(),
-                  color: Theme.of(context).primaryIconTheme.color,
-                ),
-              )));
-            }
-          }
-          return CustomScrollView(
-            controller: _scrollController,
-            slivers: children,
-          );
-        },
-      );
 }
---
+++ lib/ui/latest_tournaments/latest_tournaments_page_body.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:what_when_where/db_chgk_info/models/tournament.dart';
+import 'package:what_when_where/redux/app/state.dart';
+import 'package:what_when_where/redux/latest/actions.dart';
+import 'package:what_when_where/redux/latest/state.dart';
+import 'package:what_when_where/resources/dimensions.dart';
+import 'package:what_when_where/ui/common/error_message.dart';
+import 'package:what_when_where/ui/common/progress_indicator.dart';
+import 'package:what_when_where/ui/latest_tournaments/latest_tournaments_grid.dart';
+import 'package:what_when_where/ui/latest_tournaments/latest_tournaments_page_appbar.dart';
+
+class LatestTournamentsPageBody extends StatelessWidget {
+  final ScrollController _scrollController;
+
+  const LatestTournamentsPageBody({Key key, ScrollController scrollController})
+      : this._scrollController = scrollController,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) =>
+      StoreConnector<AppState, LatestTournamentsState>(
+        distinct: true,
+        converter: (store) => store.state.latestTournamentsState,
+        builder: (context, state) {
+          if (state.data.isNotEmpty) {
+            return _LatestTournamentsPageContent(
+              scrollController: _scrollController,
+              isLoadingMore: state.isLoadingMore,
+              data: state.data,
+            );
+          } else {
+            if (state.isLoading) {
+              return _LatestTournamentsPageLoading(
+                scrollController: _scrollController,
+              );
+            }
+            if (state.hasError) {
+              return _LatestTournamentsPageError(
+                scrollController: _scrollController,
+              );
+            }
+          }
+
+          return Container();
+        },
+      );
+}
+
+class _LatestTournamentsPageLoading extends StatelessWidget {
+  final ScrollController _scrollController;
+
+  const _LatestTournamentsPageLoading({
+    Key key,
+    ScrollController scrollController,
+  })  : this._scrollController = scrollController,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) => CustomScrollView(
+        controller: _scrollController,
+        slivers: const [
+          LatestTournamentsAppBar(),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.only(
+                top: LatestTournamentsAppBar.appBarHeight / 2,
+              ),
+              child: WWWProgressIndicator(),
+            ),
+          )
+        ],
+      );
+}
+
+class _LatestTournamentsPageError extends StatelessWidget {
+  final ScrollController _scrollController;
+
+  const _LatestTournamentsPageError({
+    Key key,
+    ScrollController scrollController,
+  })  : this._scrollController = scrollController,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) => CustomScrollView(
+        controller: _scrollController,
+        slivers: [
+          const LatestTournamentsAppBar(),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  top: LatestTournamentsAppBar.appBarHeight / 2),
+              child: ErrorMessage(
+                retryFunction: () => _retry(context),
+                color: Theme.of(context).primaryIconTheme.color,
+              ),
+            ),
+          )
+        ],
+      );
+
+  void _retry(BuildContext context) {
+    final store = StoreProvider.of<AppState>(context);
+    store.dispatch(LoadMoreLatestTournaments());
+  }
+}
+
+class _LatestTournamentsPageContent extends StatelessWidget {
+  final ScrollController _scrollController;
+  final bool isLoadingMore;
+  final List<Tournament> data;
+
+  const _LatestTournamentsPageContent({
+    Key key,
+    ScrollController scrollController,
+    this.isLoadingMore,
+    this.data,
+  })  : this._scrollController = scrollController,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) => CustomScrollView(
+        controller: _scrollController,
+        slivers: [
+          const LatestTournamentsAppBar(),
+          SliverPadding(
+            sliver: LatestTournamentsGrid(tournaments: data),
+            padding: Dimensions.defaultPadding,
+          ),
+          SliverToBoxAdapter(
+              child: isLoadingMore ? const WWWProgressIndicator() : Container())
+        ],
+      );
+}
