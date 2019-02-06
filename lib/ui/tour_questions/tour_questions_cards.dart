9cea9bfbed42b8e7619a49037667d8fe75830ec0
Anna Domashych <doan@ciklum.com>
2019-02-06 11:17:15 +0200

    Extract TourQuestionsCards class

---
+++ lib/ui/tour_questions/tour_questions_cards.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:tuple/tuple.dart';
+import 'package:what_when_where/redux/app/state.dart';
+import 'package:what_when_where/redux/questions/actions.dart';
+import 'package:what_when_where/redux/timer/actions.dart';
+import 'package:what_when_where/ui/question/question_card.dart';
+import 'package:what_when_where/utils/function_holder.dart';
+
+class TourQuestionsCards extends StatelessWidget {
+  static const _viewportFraction = 0.85;
+
+  const TourQuestionsCards({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) =>
+      StoreConnector<AppState, Tuple3<int, int, FunctionHolder>>(
+        distinct: true,
+        converter: (store) => Tuple3(
+                store.state.questionsState.questions.length,
+                store.state.questionsState.currentQuestionIndex,
+                FunctionHolder((int index) {
+              store.dispatch(ResetTimer());
+              store.dispatch(SelectQuestion(index));
+            })),
+        builder: (context, data) {
+          final count = data.item1;
+          final startIndex = data.item2;
+          final onPageChanged = data.item3;
+
+          return PageView.builder(
+            controller: PageController(
+                initialPage: startIndex, viewportFraction: _viewportFraction),
+            itemBuilder: (context, index) => Padding(
+                padding: const EdgeInsets.only(bottom: kToolbarHeight),
+                child: QuestionCard(index: index)),
+            itemCount: count,
+            onPageChanged: (index) => onPageChanged.function(index),
+          );
+        },
+        onDispose: (store) {
+          store.dispatch(ResetTimer());
+          store.dispatch(VoidQuestions());
+        },
+      );
+}
--- lib/ui/tour_questions/tour_questions_page.dart
+++ lib/ui/tour_questions/tour_questions_page.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_redux/flutter_redux.dart';
-import 'package:tuple/tuple.dart';
-import 'package:what_when_where/redux/app/state.dart';
-import 'package:what_when_where/redux/questions/actions.dart';
-import 'package:what_when_where/redux/timer/actions.dart';
-import 'package:what_when_where/ui/question/question_card.dart';
 import 'package:what_when_where/ui/tour_questions/more_icon_button.dart';
 import 'package:what_when_where/ui/tour_questions/timer_button.dart';
 import 'package:what_when_where/ui/tour_questions/timer_text.dart';
-import 'package:what_when_where/utils/function_holder.dart';
+import 'package:what_when_where/ui/tour_questions/tour_questions_cards.dart';
 
 class TourQuestionsPage extends StatefulWidget {
   static const String routeName = 'questions';
@@ -28,7 +22,7 @@ class _TourQuestionsPageState extends State<TourQuestionsPage> {
         floatingActionButton: TimerButton(),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
         bottomNavigationBar: _TourQuestionsPageBottomAppBar(),
-        body: _TourQuestionsPageView(),
+        body: const TourQuestionsCards(),
       );
 }
 
@@ -46,40 +40,4 @@ class _TourQuestionsPageBottomAppBar extends StatelessWidget {
           ],
         ),
       ));
-}
-
-class _TourQuestionsPageView extends StatelessWidget {
-  static const _viewportFraction = 0.85;
-
-  @override
-  Widget build(BuildContext context) =>
-      StoreConnector<AppState, Tuple3<int, int, FunctionHolder>>(
-        distinct: true,
-        converter: (store) => Tuple3(
-                store.state.questionsState.questions.length,
-                store.state.questionsState.currentQuestionIndex,
-                FunctionHolder((int index) {
-              store.dispatch(ResetTimer());
-              store.dispatch(SelectQuestion(index));
-            })),
-        builder: (context, data) {
-          final count = data.item1;
-          final startIndex = data.item2;
-          final onPageChanged = data.item3;
-
-          return PageView.builder(
-            controller: PageController(
-                initialPage: startIndex, viewportFraction: _viewportFraction),
-            itemBuilder: (context, index) => Padding(
-                padding: const EdgeInsets.only(bottom: kToolbarHeight),
-                child: QuestionCard(index: index)),
-            itemCount: count,
-            onPageChanged: (index) => onPageChanged.function(index),
-          );
-        },
-        onDispose: (store) {
-          store.dispatch(ResetTimer());
-          store.dispatch(VoidQuestions());
-        },
-      );
 }
