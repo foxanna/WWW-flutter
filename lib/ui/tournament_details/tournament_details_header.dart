8efb06195d0e1694d6ed0adc394262f8ecf1f122
Anna Domashych <doan@ciklum.com>
2019-02-06 20:21:48 +0200

    Provide default bottom padding

--- lib/ui/tournament_details/tournament_details_header.dart
+++ lib/ui/tournament_details/tournament_details_header.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:what_when_where/redux/app/state.dart';
+import 'package:what_when_where/resources/dimensions.dart';
 import 'package:what_when_where/ui/tournament_details/more_icon_button.dart';
 
 class TournamentDetailsPageHeader extends StatelessWidget {
@@ -11,7 +12,10 @@ class TournamentDetailsPageHeader extends StatelessWidget {
 
   final EdgeInsets padding;
 
-  const TournamentDetailsPageHeader({Key key, this.padding}) : super(key: key);
+  const TournamentDetailsPageHeader({
+    Key key,
+    this.padding = EdgeInsets.zero,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) => Material(
@@ -19,38 +23,43 @@ class TournamentDetailsPageHeader extends StatelessWidget {
         child: IconTheme.merge(
           data: Theme.of(context).primaryIconTheme,
           child: Padding(
-            padding: padding,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                ConstrainedBox(
-                  constraints: _iconConstraints,
-                  child: const BackButton(),
-                ),
-                Expanded(
-                  child: StoreConnector<AppState, String>(
-                      distinct: true,
-                      converter: (store) =>
-                          store.state.tournamentState?.tournament?.title,
-                      builder: (context, data) => Padding(
-                            padding: EdgeInsets.only(
-                                top: (kToolbarHeight -
-                                        Theme.of(context)
-                                            .primaryTextTheme
-                                            .title
-                                            .fontSize) /
-                                    2),
-                            child: Text(
-                              data,
-                              style: Theme.of(context).primaryTextTheme.title,
-                            ),
-                          )),
-                ),
-                ConstrainedBox(
+            padding: const EdgeInsets.only(
+              bottom: Dimensions.defaultSidePadding * 2,
+            ),
+            child: Padding(
+              padding: padding,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  ConstrainedBox(
                     constraints: _iconConstraints,
-                    child: const MoreIconButton()),
-              ],
+                    child: const BackButton(),
+                  ),
+                  Expanded(
+                    child: StoreConnector<AppState, String>(
+                        distinct: true,
+                        converter: (store) =>
+                            store.state.tournamentState?.tournament?.title,
+                        builder: (context, data) => Padding(
+                              padding: EdgeInsets.only(
+                                  top: (kToolbarHeight -
+                                          Theme.of(context)
+                                              .primaryTextTheme
+                                              .title
+                                              .fontSize) /
+                                      2),
+                              child: Text(
+                                data,
+                                style: Theme.of(context).primaryTextTheme.title,
+                              ),
+                            )),
+                  ),
+                  ConstrainedBox(
+                      constraints: _iconConstraints,
+                      child: const MoreIconButton()),
+                ],
+              ),
             ),
           ),
         ),
