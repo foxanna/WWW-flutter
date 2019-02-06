dfe6fba1c7a0c098276fda022512f535b408f9dd
Anna Domashych <doan@ciklum.com>
2019-02-06 18:52:16 +0200

    Display error text depending on Exception type

--- lib/ui/common/error_message.dart
+++ lib/ui/common/error_message.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:what_when_where/resources/dimensions.dart';
 import 'package:what_when_where/resources/strings.dart';
+import 'package:what_when_where/utils/network_exception.dart';
 
 class ErrorMessage extends StatelessWidget {
   final Function _retryFunction;
   final Color color;
+  final Exception exception;
 
-  const ErrorMessage({Key key, Function retryFunction, this.color})
-      : this._retryFunction = retryFunction,
+  const ErrorMessage({
+    Key key,
+    Function retryFunction,
+    this.color,
+    this.exception,
+  })  : this._retryFunction = retryFunction,
         super(key: key);
 
   @override
@@ -18,7 +24,9 @@ class ErrorMessage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Text(
-                Strings.genericError,
+                (exception is NetworkException)
+                    ? Strings.noInternetError
+                    : Strings.genericError,
                 style: color != null
                     ? Theme.of(context).textTheme.subhead.copyWith(color: color)
                     : Theme.of(context).textTheme.subhead,
