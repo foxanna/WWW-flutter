3b410edffefa78184c3b5cbe2c5392604abc5cc4
Anna Domashych <doan@ciklum.com>
2019-02-06 18:49:04 +0200

    Create NetworkException

---
+++ lib/utils/network_exception.dart
@@ -0,0 +1,11 @@
+import 'package:meta/meta.dart';
+
+@immutable
+class NetworkException implements Exception {
+  final String message;
+
+  const NetworkException({this.message});
+
+  @override
+  String toString() => '$NetworkException ${message != null ? message : ""}';
+}
