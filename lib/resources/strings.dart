b80a50563498f0751f03f9a7ff5f13d300da3559
Anna Domashych <doan@ciklum.com>
2019-02-06 18:51:39 +0200

    More String resources

--- lib/resources/strings.dart
+++ lib/resources/strings.dart
@@ -4,6 +4,7 @@ class Strings {
   static const String tours = 'туров';
   static const String questions = 'вопросов';
   static const String genericError = 'Что-то пошло не так :(';
+  static const String noInternetError = 'Кажется, пропал интернет :(';
   static const String retry = 'повторить';
   static const String close = 'закрыть';
   static const String aboutTournament = 'о турнире';
