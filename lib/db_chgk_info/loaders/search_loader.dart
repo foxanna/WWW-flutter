7e366f29d9ef68d1815524305b8084a3d2855123
Anna Domashych <doan@ciklum.com>
2019-02-08 15:13:49 +0200

    Implement SearchLoader.searchTournaments

---
+++ lib/db_chgk_info/loaders/search_loader.dart
@@ -0,0 +1,50 @@
+import 'package:html/dom.dart';
+import 'package:html/parser.dart';
+import 'package:what_when_where/db_chgk_info/http_client.dart';
+import 'package:what_when_where/db_chgk_info/models/question.dart';
+import 'package:what_when_where/db_chgk_info/models/tournament.dart';
+import 'package:what_when_where/db_chgk_info/search/search_parameters.dart';
+
+class SearchLoader {
+  static final _instance = SearchLoader._internal();
+
+  factory SearchLoader() => _instance;
+
+  SearchLoader._internal();
+
+  Future<Iterable<Question>> searchQuestions(SearchParameters parameters,
+      {int page = 0}) async {
+    return null;
+  }
+
+  Future<Iterable<Tournament>> searchTournaments(SearchParameters parameters,
+      {int page = 0}) async {
+    final queryParameters = {'page': page.toString()};
+    final query = parameters.toQuery();
+    final html = await HttpClient().getRaw(
+        Uri(path: '/search/tours/$query', queryParameters: queryParameters));
+
+    final tournaments = _parseHtml(html).toList();
+    return tournaments;
+  }
+
+  Iterable<Tournament> _parseHtml(String html) {
+    final table =
+        parse(html).getElementsByClassName('search-results questions-results');
+    final nodes = table.first.nodes.where((x) => x.hasChildNodes());
+    return _getTournaments(nodes);
+  }
+
+  static Iterable<Tournament> _getTournaments(Iterable<Node> nodes) sync* {
+    final iterator = nodes.iterator;
+
+    while (iterator.moveNext()) {
+      final Node tournamentNode = iterator.current.nodes[1];
+      if (!iterator.moveNext()) {
+        return;
+      }
+      final Node dateNode = iterator.current.nodes[1].nodes[2];
+      yield Tournament.fromSearchHtml(tournamentNode, dateNode);
+    }
+  }
+}
