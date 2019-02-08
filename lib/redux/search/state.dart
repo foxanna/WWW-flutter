1497be8b9a203204d9a16f523cd5df09bbead94e
Anna Domashych <doan@ciklum.com>
2019-02-09 00:03:01 +0200

    Add QuestionsSearchState

--- lib/redux/search/state.dart
+++ lib/redux/search/state.dart
@@ -8,25 +8,37 @@ import 'package:what_when_where/db_chgk_info/models/tournament.dart';
 @immutable
 class SearchState {
   final TournamentsSearchState tournamentsSearchState;
+  final QuestionsSearchState questionsSearchState;
 
-  const SearchState({this.tournamentsSearchState});
+  const SearchState({
+    this.tournamentsSearchState,
+    this.questionsSearchState,
+  });
 
   SearchState.initial()
-      : this(tournamentsSearchState: TournamentsSearchState.initial());
+      : this(
+          tournamentsSearchState: TournamentsSearchState.initial(),
+          questionsSearchState: const QuestionsSearchState.initial(),
+        );
 
-  SearchState copyWith({TournamentsSearchState tournamentsSearchState}) =>
+  SearchState copyWith({
+    TournamentsSearchState tournamentsSearchState,
+    QuestionsSearchState questionsSearchState,
+  }) =>
       SearchState(
         tournamentsSearchState:
             tournamentsSearchState ?? this.tournamentsSearchState,
+        questionsSearchState: questionsSearchState ?? this.questionsSearchState,
       );
 
   @override
-  int get hashCode => tournamentsSearchState.hashCode;
+  int get hashCode => hash2(tournamentsSearchState, questionsSearchState);
 
   @override
   bool operator ==(dynamic other) =>
       other is SearchState &&
-      other.tournamentsSearchState == this.tournamentsSearchState;
+      other.tournamentsSearchState == this.tournamentsSearchState &&
+      other.questionsSearchState == this.questionsSearchState;
 }
 
 @immutable
@@ -67,4 +79,17 @@ class TournamentsSearchState {
       other.exception?.runtimeType == this.exception?.runtimeType &&
       const DeepCollectionEquality()
           .equals(other.tournaments, this.tournaments);
+}
+
+@immutable
+class QuestionsSearchState {
+  const QuestionsSearchState._();
+
+  const QuestionsSearchState.initial() : this._();
+
+  @override
+  int get hashCode => 1;
+
+  @override
+  bool operator ==(dynamic other) => other is QuestionsSearchState;
 }
