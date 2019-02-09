b47d40017a6cdff29854bcdedb8dd6c3c55d594f
Anna Domashych <doan@ciklum.com>
2019-02-09 19:22:37 +0200

    Refactor SearchMiddleware

--- lib/redux/search/middleware.dart
+++ lib/redux/search/middleware.dart
@@ -1,84 +1,90 @@
 import 'package:redux/redux.dart';
 import 'package:what_when_where/db_chgk_info/loaders/search_loader.dart';
+import 'package:what_when_where/db_chgk_info/models/tournament.dart';
 import 'package:what_when_where/db_chgk_info/search/search_parameters.dart';
 import 'package:what_when_where/redux/app/state.dart';
 import 'package:what_when_where/redux/search/actions.dart';
+import 'package:what_when_where/redux/search/state.dart';
 
 class SearchMiddleware {
   static final List<Middleware<AppState>> middleware = [
-    TypedMiddleware<AppState, VoidTournamentsSearch>(_resetTournaments),
+    TypedMiddleware<AppState, VoidTournamentsSearchResults>(_resetResults),
     TypedMiddleware<AppState, SearchTournaments>(_searchTournaments),
-    TypedMiddleware<AppState, SearchMoreTournaments>(_searchMoreTournaments),
+    TypedMiddleware<AppState, TournamentsSearchQueryChanged>(_queryChanged),
+    TypedMiddleware<AppState, TournamentsSearchSortingChanged>(_sortingChanged),
   ];
 
-  static final _loader = SearchLoader();
-
-  static int _page = 0;
-  static SearchTournaments _currentTournamentsSearch;
-
-  static void _resetTournaments(Store<AppState> store,
-      VoidTournamentsSearch action, NextDispatcher next) {
+  static void _resetResults(Store<AppState> store,
+      VoidTournamentsSearchResults action, NextDispatcher next) {
     next(action);
 
-    _reset(store);
+    store.dispatch(const TournamentsSearchPageChanged(0));
   }
 
   static Future _searchTournaments(Store<AppState> store,
       SearchTournaments action, NextDispatcher next) async {
     next(action);
 
-    if (_currentTournamentsSearch != action) {
-      _reset(store);
-    } else {
-      if (store.state.searchState.tournamentsSearchState.isLoading) {
-        return;
-      }
+    if (!store.state.searchState.searchResults.isLoading) {
+      await _search(store, action);
     }
-
-    await _search(store, action);
-  }
-
-  static Future _searchMoreTournaments(Store<AppState> store,
-      SearchMoreTournaments action, NextDispatcher next) async {
-    next(action);
-
-    if (_currentTournamentsSearch == null ||
-        store.state.searchState.tournamentsSearchState.isLoading) {
-      return;
-    }
-
-    await _search(store, _currentTournamentsSearch);
   }
 
   static Future _search(Store<AppState> store, SearchTournaments action) async {
-    _currentTournamentsSearch = action;
+    final parameters = store.state.searchState.searchParameters;
 
     try {
       store.dispatch(const TournamentsSearchIsLoading());
 
-      final data = await _loader.searchTournaments(
-          SearchParameters(
-            query: action.query,
-            sorting: action.sorting,
-          ),
-          page: _page);
+      final data = await _fetch(parameters);
 
-      if (_currentTournamentsSearch == action) {
-        _page++;
-
+      if (parameters == store.state.searchState.searchParameters) {
         store.dispatch(TournamentsSearchLoaded(data));
+        store.dispatch(TournamentsSearchPageChanged(parameters.nextPage + 1));
       }
     } catch (e) {
-      if (_currentTournamentsSearch == action) {
+      if (parameters == store.state.searchState.searchParameters) {
         store.dispatch(TournamentsSearchFailedToLoad(e));
       }
     }
   }
 
-  static void _reset(Store<AppState> store) {
-    store.dispatch(const ClearTournamentsSearch());
+  static void _sortingChanged(Store<AppState> store,
+      TournamentsSearchSortingChanged action, NextDispatcher next) {
+    final sortingHasChanged =
+        action.sorting != store.state.searchState.searchParameters.sorting;
 
-    _currentTournamentsSearch = null;
-    _page = 0;
+    next(action);
+
+    if (sortingHasChanged) {
+      final repeatSearch = store.state.searchState.searchResults.hasData ||
+          store.state.searchState.searchResults.isLoading;
+      store.dispatch(const VoidTournamentsSearchResults());
+
+      if (repeatSearch) {
+        store.dispatch(const SearchTournaments());
+      }
+    }
   }
+
+  static void _queryChanged(Store<AppState> store,
+      TournamentsSearchQueryChanged action, NextDispatcher next) {
+    final queryHasChanged =
+        action.query != store.state.searchState.searchParameters.query;
+
+    next(action);
+
+    if (queryHasChanged) {
+      store.dispatch(const VoidTournamentsSearchResults());
+    }
+  }
+
+  static Future<Iterable<Tournament>> _fetch(
+          SearchTournamentsParametersState parameters) =>
+      SearchLoader().searchTournaments(
+          SearchParameters(
+            query: parameters.query,
+            sorting: parameters.sorting,
+          ),
+          page: parameters.nextPage);
 }
