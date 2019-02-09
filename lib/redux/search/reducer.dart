908b9462d7049a4963a7e5e50760f2d67960292e
Anna Domashych <doan@ciklum.com>
2019-02-09 19:23:07 +0200

    Refactor SearchReducer

--- lib/redux/search/reducer.dart
+++ lib/redux/search/reducer.dart
@@ -5,36 +5,44 @@ import 'package:what_when_where/redux/search/state.dart';
 
 class SearchReducer {
   static final Reducer<SearchState> _reducer = combineReducers<SearchState>([
-    TypedReducer<SearchState, ClearTournamentsSearch>(_clearTournamentsSearch),
+    // user actions
+    TypedReducer<SearchState, TournamentsSearchQueryChanged>(
+        _tournamentsSearchQueryChanged),
+    TypedReducer<SearchState, TournamentsSearchSortingChanged>(
+        _tournamentsSearchSortingChanged),
+    TypedReducer<SearchState, VoidTournamentsSearchParameters>(
+        _voidTournamentsSearchParameters),
+    TypedReducer<SearchState, VoidTournamentsSearchResults>(
+        _voidTournamentsSearchResults),
+    // system actions
     TypedReducer<SearchState, TournamentsSearchLoaded>(
         _tournamentsSearchLoaded),
     TypedReducer<SearchState, TournamentsSearchIsLoading>(
         _tournamentsSearchIsLoading),
     TypedReducer<SearchState, TournamentsSearchFailedToLoad>(
         _tournamentsSearchFailedToLoad),
+    TypedReducer<SearchState, TournamentsSearchPageChanged>(
+        _tournamentsSearchPageChanged),
   ]);
 
   static SearchState reduce(SearchState state, dynamic action) =>
       _reducer(state, action);
 
-  static SearchState _clearTournamentsSearch(
-          SearchState state, ClearTournamentsSearch action) =>
-      state.copyWith(tournamentsSearchState: TournamentsSearchState.initial());
-
   static SearchState _tournamentsSearchIsLoading(
           SearchState state, TournamentsSearchIsLoading action) =>
       state.copyWith(
-          tournamentsSearchState: state.tournamentsSearchState.copyWith(
-        isLoading: true,
-        exception: null,
-      ));
+        searchResults: state.searchResults.copyWith(
+          isLoading: true,
+          exception: null,
+        ),
+      );
 
   static SearchState _tournamentsSearchLoaded(
           SearchState state, TournamentsSearchLoaded action) =>
       state.copyWith(
-          tournamentsSearchState: state.tournamentsSearchState.copyWith(
-        tournaments: <Tournament>[]
-          ..addAll(state.tournamentsSearchState.tournaments)
+          searchResults: state.searchResults.copyWith(
+        data: <Tournament>[]
+          ..addAll(state.searchResults.data)
           ..addAll(action.data),
         isLoading: false,
         exception: null,
@@ -43,8 +51,45 @@ class SearchReducer {
   static SearchState _tournamentsSearchFailedToLoad(
           SearchState state, TournamentsSearchFailedToLoad action) =>
       state.copyWith(
-          tournamentsSearchState: state.tournamentsSearchState.copyWith(
-        isLoading: false,
-        exception: action.exception,
-      ));
+        searchResults: state.searchResults.copyWith(
+          isLoading: false,
+          exception: action.exception,
+        ),
+      );
+
+  static SearchState _tournamentsSearchQueryChanged(
+          SearchState state, TournamentsSearchQueryChanged action) =>
+      state.copyWith(
+        searchParameters: state.searchParameters.copyWith(
+          query: action.query,
+        ),
+      );
+
+  static SearchState _tournamentsSearchSortingChanged(
+          SearchState state, TournamentsSearchSortingChanged action) =>
+      state.copyWith(
+        searchParameters: state.searchParameters.copyWith(
+          sorting: action.sorting,
+        ),
+      );
+
+  static SearchState _voidTournamentsSearchParameters(
+          SearchState state, VoidTournamentsSearchParameters action) =>
+      state.copyWith(
+        searchParameters: const SearchTournamentsParametersState.initial(),
+      );
+
+  static SearchState _voidTournamentsSearchResults(
+          SearchState state, VoidTournamentsSearchResults action) =>
+      state.copyWith(
+        searchResults: SearchTournamentsResultsState.initial(),
+      );
+
+  static SearchState _tournamentsSearchPageChanged(
+          SearchState state, TournamentsSearchPageChanged action) =>
+      state.copyWith(
+        searchParameters: state.searchParameters.copyWith(
+          nextPage: action.nextPage,
+        ),
+      );
 }
