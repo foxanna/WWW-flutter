f4965f81050cd0f7fcc674a02ff3ef83e96429e7
Anna Domashych <doan@ciklum.com>
2019-02-09 19:31:37 +0200

    Compose SearchPage

--- lib/ui/search/search_page.dart
+++ lib/ui/search/search_page.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:what_when_where/resources/strings.dart';
+import 'package:what_when_where/ui/search/search_tournaments_page.dart';
+import 'package:what_when_where/ui/search/search_tournaments_page_app_bar.dart';
 
 class SearchPage extends StatelessWidget {
   static const String routeName = 'search';
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: const Text(Strings.search),
-        ),
+        appBar: SearchTournamentsPageAppBar(),
+        body: SearchTournamentsPage(),
       );
 }
