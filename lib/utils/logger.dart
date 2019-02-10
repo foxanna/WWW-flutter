f4ff52c3d9e45a971269955f633f859d0f80d9e9
Anna Domashych <doan@ciklum.com>
2019-02-10 08:20:54 +0200

    Print logs only in non-release mode

--- lib/utils/logger.dart
+++ lib/utils/logger.dart
@@ -1,8 +1,16 @@
 import 'dart:math';
 
+import 'package:what_when_where/utils/build_mode.dart';
+
 const _maxLogSize = 512;
 
+final _shouldLog = buildMode != BuildMode.release;
+
 void log(Object object) {
+  if (!_shouldLog) {
+    return;
+  }
+
   final data = _split(object.toString(), _maxLogSize);
   data.forEach(print);
 }
