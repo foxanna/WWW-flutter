00f7efbc739be92d77bf2b27d817021f6023f97d
Anna Domashych <doan@ciklum.com>
2019-02-11 07:57:11 +0200

    Pass same ScrollController to SearchTournamentsPageAppBar and SearchTournamentsPage

--- lib/ui/search/search_page.dart
+++ lib/ui/search/search_page.dart
@@ -2,12 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:what_when_where/ui/search/search_tournaments_page.dart';
 import 'package:what_when_where/ui/search/search_tournaments_page_app_bar.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
   static const String routeName = 'search';
 
   @override
+  SearchPageState createState() => SearchPageState();
+}
+
+class SearchPageState extends State<SearchPage> {
+  ScrollController _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _scrollController = ScrollController();
+  }
+
+  @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: SearchTournamentsPageAppBar(),
-        body: SearchTournamentsPage(),
+        appBar:
+            SearchTournamentsPageAppBar(scrollController: _scrollController),
+        body: SearchTournamentsPage(scrollController: _scrollController),
       );
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+
+    super.dispose();
+  }
 }
