413aa5b72ba8e0eac092792f6504d88fe1d5e671
Anna Domashych <doan@ciklum.com>
2019-02-14 07:47:34 +0200

    Improve Analytics logging

--- lib/services/analytics.dart
+++ lib/services/analytics.dart
@@ -2,6 +2,7 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
+import 'package:what_when_where/utils/logger.dart';
 
 class AnalyticsService {
   static final _instance = AnalyticsService._();
@@ -19,9 +20,9 @@ class AnalyticsService {
               settings.name == '/' && home != null ? home : settings.name);
 
   void logEvent({@required String name, Map<String, dynamic> parameters}) =>
-      _analytics
-          .logEvent(name: name, parameters: parameters)
-          .then((v) => debugPrint('$AnalyticsService log event: $name'))
-          .catchError((Object error) =>
-              debugPrint('$AnalyticsService log event error: $error'));
+      _analytics.logEvent(name: name, parameters: parameters).then((_) {
+        log('$AnalyticsService logging event "$name", '
+            'parameters "${(parameters?.isEmpty ?? true) ? 'none' : parameters.entries.map<String>((e) => "${e.key}: ${e.value}").join(', ')}"');
+      }).catchError((Object error) =>
+          log('$AnalyticsService error logging event $name : $error'));
 }
