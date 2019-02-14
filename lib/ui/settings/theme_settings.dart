b28c9869e04d540b1de5017cf2526b51779a147b
Anna Domashych <doan@ciklum.com>
2019-02-14 08:01:29 +0200

    Create ThemeSetting widget

---
+++ lib/ui/settings/theme_settings.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:tuple/tuple.dart';
+import 'package:what_when_where/redux/app/state.dart';
+import 'package:what_when_where/redux/settings/actions.dart';
+import 'package:what_when_where/resources/dimensions.dart';
+import 'package:what_when_where/resources/strings.dart';
+import 'package:what_when_where/resources/themes.dart';
+import 'package:what_when_where/utils/function_holder.dart';
+
+class ThemeSetting extends StatelessWidget {
+  const ThemeSetting({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Row(
+        crossAxisAlignment: CrossAxisAlignment.baseline,
+        textBaseline: TextBaseline.alphabetic,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Text(
+            Strings.appTheme,
+            style: Theme.of(context).textTheme.subhead,
+          ),
+          const SizedBox(width: Dimensions.defaultSpacing * 3),
+          const _ThemeDropdownButton(),
+        ],
+      );
+}
+
+class _ThemeDropdownButton extends StatelessWidget {
+  static const _translations = <AppTheme, String>{
+    AppTheme.light: Strings.light,
+    AppTheme.dark: Strings.dark,
+  };
+
+  const _ThemeDropdownButton({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) =>
+      StoreConnector<AppState, Tuple2<AppTheme, FunctionHolder>>(
+        distinct: true,
+        converter: (store) => Tuple2(
+            store.state.settingsState.appTheme,
+            FunctionHolder(
+                (AppTheme appTheme) => store.dispatch(ChangeTheme(appTheme)))),
+        builder: (context, data) {
+          final appTheme = data.item1;
+          final changeThemeFunction = data.item2;
+
+          return DropdownButton<AppTheme>(
+            value: appTheme,
+            items: AppTheme.values.map(
+              (theme) {
+                final themeData = Themes.get(theme);
+
+                return DropdownMenuItem(
+                  value: theme,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: themeData.canvasColor,
+                      borderRadius: BorderRadius.circular(4.0),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        _translations[theme],
+                        style: themeData.textTheme.body1,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ).toList(),
+            onChanged: (newTheme) => changeThemeFunction.function(newTheme),
+          );
+        },
+      );
+}
