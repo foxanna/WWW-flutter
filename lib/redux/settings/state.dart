05bbae5e329d65acd4b23febaf75c0fac060fc9a
Anna Domashych <doan@ciklum.com>
2019-02-15 07:15:18 +0200

    Add text scale to settings state

--- lib/redux/settings/state.dart
+++ lib/redux/settings/state.dart
@@ -1,26 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
+import 'package:quiver/core.dart';
+import 'package:what_when_where/resources/fonts.dart';
 import 'package:what_when_where/resources/themes.dart';
 
 @immutable
 class SettingsState {
   final AppTheme appTheme;
+  final TextScale textScale;
 
-  const SettingsState._({this.appTheme});
+  const SettingsState._({
+    this.appTheme,
+    this.textScale,
+  });
 
-  const SettingsState.initial() : this._(appTheme: AppTheme.light);
+  const SettingsState.initial()
+      : this._(
+          appTheme: AppTheme.light,
+          textScale: TextScale.normal,
+        );
 
   SettingsState copyWith({
     AppTheme appTheme,
+    TextScale textScale,
   }) =>
       SettingsState._(
         appTheme: appTheme ?? this.appTheme,
+        textScale: textScale ?? this.textScale,
       );
 
   @override
-  int get hashCode => appTheme.hashCode;
+  int get hashCode => hash2(appTheme, textScale);
 
   @override
   bool operator ==(dynamic other) =>
-      other is SettingsState && other.appTheme == appTheme;
+      other is SettingsState &&
+      other.appTheme == appTheme &&
+      other.textScale == textScale;
 }
