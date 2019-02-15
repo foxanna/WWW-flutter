ac81a537f4eaf1c57cc8b318dd2075f748dcc2f3
Anna Domashych <doan@ciklum.com>
2019-02-15 07:41:32 +0200

    Dispatch SettingsRead action on ReadSettings action

--- lib/redux/settings/middleware.dart
+++ lib/redux/settings/middleware.dart
@@ -1,6 +1,7 @@
 import 'package:redux/redux.dart';
 import 'package:what_when_where/redux/app/state.dart';
 import 'package:what_when_where/redux/settings/actions.dart';
+import 'package:what_when_where/resources/fonts.dart';
 import 'package:what_when_where/resources/themes.dart';
 import 'package:what_when_where/services/preferences.dart';
 
@@ -11,6 +12,20 @@ class SettingsMiddleware {
   ];
 
   static const _themeKey = 'theme';
+  static const _textScaleKey = 'scale';
+
+  static Future _onReadSettings(
+      Store<AppState> store, ReadSettings action, NextDispatcher next) async {
+    next(action);
+
+    final appThemeIndex = await Preferences().getInt(_themeKey, 0);
+    final appTheme = AppTheme.values[appThemeIndex];
+
+    final textScaleIndex = await Preferences().getInt(_textScaleKey, 0);
+    final textScale = TextScale.values[textScaleIndex];
+
+    store.dispatch(SettingsRead(appTheme: appTheme, textScale: textScale));
+  }
 
   static Future _onThemeChanged(
       Store<AppState> store, ChangeTheme action, NextDispatcher next) async {
@@ -24,13 +39,6 @@ class SettingsMiddleware {
     }
   }
 
-  static Future _onReadSettings(
-      Store<AppState> store, ReadSettings action, NextDispatcher next) async {
-    next(action);
 
-    final appThemeIndex = await Preferences().getInt(_themeKey, 0);
-    final appTheme = AppTheme.values[appThemeIndex];
 
-    store.dispatch(ChangeTheme(appTheme));
-  }
 }
