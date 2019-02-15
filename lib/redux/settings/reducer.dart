f5aa1a4b0aa6a1649fb34e95aa993ce1a6d490b3
Anna Domashych <doan@ciklum.com>
2019-02-15 07:17:01 +0200

    Handle ChangeTextScale action

--- lib/redux/settings/reducer.dart
+++ lib/redux/settings/reducer.dart
@@ -6,6 +6,7 @@ class SettingsReducer {
   static final Reducer<SettingsState> _reducer =
       combineReducers<SettingsState>([
     TypedReducer<SettingsState, ChangeTheme>(_themeChanged),
+    TypedReducer<SettingsState, ChangeTextScale>(_textScaleChanged),
   ]);
 
   static SettingsState reduce(SettingsState state, dynamic action) =>
@@ -13,4 +14,8 @@ class SettingsReducer {
 
   static SettingsState _themeChanged(SettingsState state, ChangeTheme action) =>
       state.copyWith(appTheme: action.appTheme);
+
+  static SettingsState _textScaleChanged(
+          SettingsState state, ChangeTextScale action) =>
+      state.copyWith(textScale: action.textScale);
 }
