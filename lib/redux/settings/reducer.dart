2165d8f4425be3a0f40eb98620ac199a54267eae
Anna Domashych <doan@ciklum.com>
2019-02-15 07:20:00 +0200

    Handle SettingsRead action

--- lib/redux/settings/reducer.dart
+++ lib/redux/settings/reducer.dart
@@ -5,12 +5,20 @@ import 'package:what_when_where/redux/settings/state.dart';
 class SettingsReducer {
   static final Reducer<SettingsState> _reducer =
       combineReducers<SettingsState>([
+    TypedReducer<SettingsState, SettingsRead>(_settingsChanged),
     TypedReducer<SettingsState, ChangeTheme>(_themeChanged),
     TypedReducer<SettingsState, ChangeTextScale>(_textScaleChanged),
   ]);
 
   static SettingsState reduce(SettingsState state, dynamic action) =>
       _reducer(state, action);
+
+  static SettingsState _settingsChanged(
+          SettingsState state, SettingsRead action) =>
+      state.copyWith(
+        appTheme: action.appTheme,
+        textScale: action.textScale,
+      );
 
   static SettingsState _themeChanged(SettingsState state, ChangeTheme action) =>
       state.copyWith(appTheme: action.appTheme);
