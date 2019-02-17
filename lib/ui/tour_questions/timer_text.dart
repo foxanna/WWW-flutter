95f676865becf22625065720dd9459628b353aba
Anna Domashych <doan@ciklum.com>
2019-02-17 06:55:22 +0200

    Create _TimerValue to display secondsLeft

--- lib/ui/tour_questions/timer_text.dart
+++ lib/ui/tour_questions/timer_text.dart
@@ -2,6 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:what_when_where/redux/app/state.dart';
 import 'package:what_when_where/resources/dimensions.dart';
+import 'package:what_when_where/utils/duration_formatter.dart';
 
 
 
+class _TimerValue extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => StoreConnector<AppState, int>(
+        distinct: true,
+        converter: (store) => store.state.timerState.secondsLeft,
+        builder: (context, seconds) => Text(
+              DurationFormatter.formatSeconds(seconds),
+              style: Theme.of(context).primaryTextTheme.title,
+            ),
+      );
+}
