97ab30e96aef83bd495883d0093ec5f7ffc99012
Anna Domashych <doan@ciklum.com>
2019-02-17 06:56:30 +0200

    Create _TimerOptionsDropdownButton to display and change timer options

--- lib/ui/tour_questions/timer_text.dart
+++ lib/ui/tour_questions/timer_text.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:what_when_where/common/timer_type.dart';
 import 'package:what_when_where/redux/app/state.dart';
+import 'package:what_when_where/redux/timer/actions.dart';
 import 'package:what_when_where/resources/dimensions.dart';
 import 'package:what_when_where/utils/duration_formatter.dart';
 
 
+class _TimerOptionsDropdownButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => DropdownButtonHideUnderline(
+        child: DropdownButton<TimerType>(
+          value: TimerType.normal,
+          items: TimerType.values
+              .map(
+                (type) => DropdownMenuItem(
+                      value: type,
+                      child: Text(
+                        DurationFormatter.formatSeconds(
+                            Timers.getSeconds(type)),
+                      ),
+                    ),
+              )
+              .toList(),
+          onChanged: (newType) => _onTypeChanged(context, newType),
+        ),
+      );
+
+  void _onTypeChanged(BuildContext context, TimerType timerType) =>
+      StoreProvider.of<AppState>(context).dispatch(ChangeTimerType(timerType));
+}
 
 class _TimerValue extends StatelessWidget {
   @override
