270c974c622c14af659e08cc064e36deeca30da2
Anna Domashych <doan@ciklum.com>
2019-02-18 13:25:03 +0200

    Create ExpandingPanel widget

---
+++ lib/ui/common/expanding_panel.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class ExpandingPanel extends StatefulWidget {
+  final Widget header;
+  final Widget body;
+  final Duration animationDuration;
+
+  const ExpandingPanel({
+    Key key,
+    this.header,
+    this.body,
+    this.animationDuration = kThemeAnimationDuration,
+  }) : super(key: key);
+
+  @override
+  _ExpandingPanelState createState() => _ExpandingPanelState();
+}
+
+class _ExpandingPanelState extends State<ExpandingPanel> {
+  bool _isExpanded = false;
+
+  @override
+  Widget build(BuildContext context) => Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              children: <Widget>[
+                widget.header,
+                AnimatedCrossFade(
+                  firstChild: Container(height: 0.0),
+                  secondChild: widget.body,
+                  firstCurve:
+                      const Interval(0.0, 0.6, curve: Curves.fastOutSlowIn),
+                  secondCurve:
+                      const Interval(0.4, 1.0, curve: Curves.fastOutSlowIn),
+                  sizeCurve: Curves.fastOutSlowIn,
+                  crossFadeState: _isExpanded
+                      ? CrossFadeState.showSecond
+                      : CrossFadeState.showFirst,
+                  duration: widget.animationDuration,
+                ),
+              ],
+            ),
+          ),
+          IconButton(
+            icon: _isExpanded
+                ? const Icon(Icons.arrow_drop_up)
+                : const Icon(Icons.arrow_drop_down),
+            onPressed: () => setState(() => _isExpanded = !_isExpanded),
+          ),
+        ],
+      );
+}
