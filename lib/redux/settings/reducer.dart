c542f14e2e98f4e866bf6bf34eedea482d4efdfe
Anna Domashych <doan@ciklum.com>
2019-02-18 14:58:34 +0200

    Update state when timer settings change

--- lib/redux/settings/reducer.dart
+++ lib/redux/settings/reducer.dart
@@ -8,6 +8,10 @@ class SettingsReducer {
     TypedReducer<SettingsState, SettingsRead>(_settingsChanged),
     TypedReducer<SettingsState, ChangeTheme>(_themeChanged),
     TypedReducer<SettingsState, ChangeTextScale>(_textScaleChanged),
+    TypedReducer<SettingsState, ChangeNotifyShortTimerExpiration>(
+        _notifyShortTimerExpirationChanged),
+    TypedReducer<SettingsState, ChangeNotifyLongTimerExpiration>(
+        _notifyLongTimerExpirationChanged),
   ]);
 
   static SettingsState reduce(SettingsState state, dynamic action) =>
@@ -18,6 +22,8 @@ class SettingsReducer {
       state.copyWith(
         appTheme: action.appTheme,
         textScale: action.textScale,
+        notifyShortTimerExpiration: action.notifyShortTimerExpiration,
+        notifyLongTimerExpiration: action.notifyLongTimerExpiration,
       );
 
   static SettingsState _themeChanged(SettingsState state, ChangeTheme action) =>
@@ -26,4 +32,12 @@ class SettingsReducer {
   static SettingsState _textScaleChanged(
           SettingsState state, ChangeTextScale action) =>
       state.copyWith(textScale: action.textScale);
+
+  static SettingsState _notifyShortTimerExpirationChanged(
+          SettingsState state, ChangeNotifyShortTimerExpiration action) =>
+      state.copyWith(notifyShortTimerExpiration: action.newValue);
+
+  static SettingsState _notifyLongTimerExpirationChanged(
+          SettingsState state, ChangeNotifyLongTimerExpiration action) =>
+      state.copyWith(notifyLongTimerExpiration: action.newValue);
 }
