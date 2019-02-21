b7565271a10008d0b6905d97044dd895d9146cfd
Anna Domashych <doan@ciklum.com>
2019-02-21 13:55:59 +0200

    Create CustomIcons provider class

---
+++ lib/resources/custom_icons.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+
+class CustomIcons {
+  CustomIcons._();
+
+  static const _kFontFam = 'CustomIcons';
+
+  static const IconData random_card = IconData(0xe805, fontFamily: _kFontFam);
+}
