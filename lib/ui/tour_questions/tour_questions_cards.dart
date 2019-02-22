531697ca0cb7bccd4db708ea6e72864f798a7f80
Anna Domashych <doan@ciklum.com>
2019-02-22 10:23:59 +0200

    Display loading or error when question cards are being loaded

--- lib/ui/tour_questions/tour_questions_cards.dart
+++ lib/ui/tour_questions/tour_questions_cards.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
-import 'package:tuple/tuple.dart';
 import 'package:what_when_where/redux/app/state.dart';
 import 'package:what_when_where/redux/questions/actions.dart';
+import 'package:what_when_where/redux/questions/state.dart';
 import 'package:what_when_where/redux/timer/actions.dart';
+import 'package:what_when_where/ui/common/progress_indicator.dart';
 import 'package:what_when_where/ui/question/question_card.dart';
+import 'package:what_when_where/ui/tour_questions/error_message.dart';
 
 class TourQuestionsCards extends StatelessWidget {
   static const _viewportFraction = 0.85;
@@ -13,22 +15,30 @@ class TourQuestionsCards extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) =>
-      StoreConnector<AppState, Tuple2<int, int>>(
+      StoreConnector<AppState, QuestionsState>(
         distinct: true,
-        converter: (store) => Tuple2(
-            store.state.questionsState.questions.length,
-            store.state.questionsState.currentQuestionIndex),
-        builder: (context, data) {
-          final count = data.item1;
-          final startIndex = data.item2;
+        converter: (store) => store.state.questionsState,
+        builder: (context, state) {
+          final count = state.questions.length;
+          final startIndex = state.currentQuestionIndex;
+          final isLoading = state.isLoading;
+          final hasError = state.hasError;
+          final hasAdditionalCard = isLoading || hasError;
 
           return PageView.builder(
             controller: PageController(
-                initialPage: startIndex, viewportFraction: _viewportFraction),
+              initialPage: startIndex,
+              viewportFraction: _viewportFraction,
+            ),
             itemBuilder: (context, index) => Padding(
-                padding: const EdgeInsets.only(bottom: kToolbarHeight),
-                child: QuestionCard(index: index)),
-            itemCount: count,
+                  padding: const EdgeInsets.only(bottom: kToolbarHeight),
+                  child: hasAdditionalCard && index == count
+                      ? isLoading
+                          ? const WWWProgressIndicator()
+                          : const QuestionsErrorMessage()
+                      : QuestionCard(index: index),
+                ),
+            itemCount: hasAdditionalCard ? count + 1 : count,
             onPageChanged: (index) => _onPageChanged(context, index),
           );
         },
