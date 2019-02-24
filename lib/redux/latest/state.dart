663dc9612cccbe07e191b34bad1354648aab753c
Anna Domashych <doan@ciklum.com>
2019-02-24 15:36:26 +0200

    Add nextPage field to LatestTournamentsState

--- lib/redux/latest/state.dart
+++ lib/redux/latest/state.dart
@@ -11,44 +11,54 @@ class LatestTournamentsState {
   final bool isRefreshing;
   final UnmodifiableListView<Tournament> data;
   final Exception exception;
+  final int nextPage;
 
   bool get isLoading => isLoadingMore || isRefreshing;
   bool get hasError => exception != null;
   bool get hasData => data.isNotEmpty;
 
   LatestTournamentsState._({
+    Iterable<Tournament> data,
     this.isLoadingMore = false,
     this.isRefreshing = false,
     this.exception,
-    Iterable<Tournament> data,
+    this.nextPage = 0,
   }) : this.data = UnmodifiableListView<Tournament>(data ?? <Tournament>[]);
 
   LatestTournamentsState.initial() : this._();
 
+  @override
+  int get hashCode => hashObjects(<dynamic>[
+        data,
+        isLoadingMore,
+        isRefreshing,
+        exception.runtimeType,
+        nextPage
+      ]);
+
   LatestTournamentsState copyWith({
+    Optional<Iterable<Tournament>> data,
     Optional<bool> isLoadingMore,
     Optional<bool> isRefreshing,
     Optional<Exception> exception,
-    Optional<Iterable<Tournament>> data,
+    Optional<int> nextPage,
   }) =>
       LatestTournamentsState._(
+        data: data != null ? data.orNull : this.data,
         isLoadingMore:
             isLoadingMore != null ? isLoadingMore.orNull : this.isLoadingMore,
         isRefreshing:
             isRefreshing != null ? isRefreshing.orNull : this.isRefreshing,
         exception: exception != null ? exception.orNull : this.exception,
-        data: data != null ? data.orNull : this.data,
+        nextPage: nextPage != null ? nextPage.orNull : this.nextPage,
       );
-
-  @override
-  int get hashCode =>
-      hash4(isLoadingMore, isRefreshing, exception.runtimeType, data);
 
   @override
   bool operator ==(dynamic other) =>
       other is LatestTournamentsState &&
+      const DeepCollectionEquality().equals(other.data, data) &&
       other.isLoadingMore == isLoadingMore &&
       other.isRefreshing == isRefreshing &&
       other.exception?.runtimeType == exception?.runtimeType &&
-      const DeepCollectionEquality().equals(other.data, data);
+      other.nextPage == nextPage;
 }
