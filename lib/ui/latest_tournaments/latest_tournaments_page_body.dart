243c892fb5202c1c61fc4ea72ebe6ce195093db1
Anna Domashych <doan@ciklum.com>
2019-02-24 11:33:46 +0200

    Refactor _LatestTournamentsPageContent and display retry button when loading more data failed

--- lib/ui/latest_tournaments/latest_tournaments_page_body.dart
+++ lib/ui/latest_tournaments/latest_tournaments_page_body.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
-import 'package:what_when_where/db_chgk_info/models/tournament.dart';
 import 'package:what_when_where/redux/app/state.dart';
 import 'package:what_when_where/redux/latest/state.dart';
 import 'package:what_when_where/resources/dimensions.dart';
@@ -25,8 +24,6 @@ class LatestTournamentsPageBody extends StatelessWidget {
           if (state.data.isNotEmpty) {
             return _LatestTournamentsPageContent(
               scrollController: _scrollController,
-              isLoadingMore: state.isLoadingMore,
-              data: state.data,
             );
           } else {
             if (state.isLoading) {
@@ -102,28 +99,41 @@ class _LatestTournamentsPageError extends StatelessWidget {
 
 class _LatestTournamentsPageContent extends StatelessWidget {
   final ScrollController _scrollController;
-  final bool isLoadingMore;
-  final List<Tournament> data;
 
   const _LatestTournamentsPageContent({
     Key key,
     ScrollController scrollController,
-    this.isLoadingMore,
-    this.data,
   })  : this._scrollController = scrollController,
         super(key: key);
 
   @override
-  Widget build(BuildContext context) => CustomScrollView(
-        controller: _scrollController,
-        slivers: [
-          const LatestTournamentsAppBar(),
-          SliverPadding(
-            sliver: TournamentsGrid(tournaments: data),
-            padding: Dimensions.defaultPadding,
-          ),
-          SliverToBoxAdapter(
-              child: isLoadingMore ? const WWWProgressIndicator() : Container())
-        ],
+  Widget build(BuildContext context) =>
+      StoreConnector<AppState, LatestTournamentsState>(
+        distinct: true,
+        converter: (store) => store.state.latestTournamentsState,
+        builder: (context, state) => CustomScrollView(
+              controller: _scrollController,
+              slivers: [
+                const LatestTournamentsAppBar(),
+                SliverPadding(
+                  sliver: TournamentsGrid(tournaments: state.data),
+                  padding: Dimensions.defaultPadding,
+                ),
+                SliverToBoxAdapter(
+                  child: state.isLoadingMore
+                      ? WWWProgressIndicator(
+                          padding: Dimensions.defaultPadding * 3,
+                        )
+                      : Container(),
+                ),
+                SliverToBoxAdapter(
+                  child: state.hasError
+                      ? const LatestTournamentsErrorMessage(
+                          dense: true,
+                        )
+                      : Container(),
+                )
+              ],
+            ),
       );
 }
