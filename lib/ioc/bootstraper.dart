290ed38d2642152fb27eca45243ff875939d1cee
Anna Domashych <doan@ciklum.com>
2019-02-26 15:43:20 +0200

    Register SharingService as implementation of ISharingService

--- lib/ioc/bootstraper.dart
+++ lib/ioc/bootstraper.dart
@@ -1,6 +1,7 @@
 import 'package:dioc/dioc.dart';
 import 'package:what_when_where/ioc/container.dart';
 import 'package:what_when_where/services/analytics.dart';
+import 'package:what_when_where/services/sharing.dart';
 
 class Bootstrapper {
   static final Bootstrapper _instance = Bootstrapper._();
@@ -13,6 +14,8 @@ class Bootstrapper {
 
   void init() {
     _container.register<IAnalyticsService>((c) => AnalyticsService.ioc(),
+        defaultMode: InjectMode.singleton);
+    _container.register<ISharingService>((c) => SharingService.ioc(),
         defaultMode: InjectMode.singleton);
   }
 }
