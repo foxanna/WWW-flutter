0185b377f9ab976c3f879699e2c2b4e2f17e240d
Anna Domashych <doan@ciklum.com>
2019-02-26 15:52:47 +0200

    Register IBrowsingService in IoC

--- lib/ioc/bootstraper.dart
+++ lib/ioc/bootstraper.dart
@@ -1,6 +1,7 @@
 import 'package:dioc/dioc.dart';
 import 'package:what_when_where/ioc/container.dart';
 import 'package:what_when_where/services/analytics.dart';
+import 'package:what_when_where/services/browsing.dart';
 import 'package:what_when_where/services/sharing.dart';
 
 class Bootstrapper {
@@ -16,6 +17,8 @@ class Bootstrapper {
     _container.register<IAnalyticsService>((c) => AnalyticsService.ioc(),
         defaultMode: InjectMode.singleton);
     _container.register<ISharingService>((c) => SharingService.ioc(),
+        defaultMode: InjectMode.singleton);
+    _container.register<IBrowsingService>((c) => BrowsingService.ioc(),
         defaultMode: InjectMode.singleton);
   }
 }
