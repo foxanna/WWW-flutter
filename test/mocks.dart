2a9a25104b9364ac0cab01fff9bbf486c3bf74f9
Anna Domashych <doan@ciklum.com>
2019-02-26 20:04:04 +0200

    Create NavigationServiceMock

--- test/mocks.dart
+++ test/mocks.dart
@@ -1,6 +1,7 @@
 import 'package:mockito/mockito.dart';
 import 'package:what_when_where/services/analytics.dart';
 import 'package:what_when_where/services/browsing.dart';
+import 'package:what_when_where/services/navigation.dart';
 import 'package:what_when_where/services/sharing.dart';
 
 class AnalyticsServiceMock extends Mock implements IAnalyticsService {}
@@ -8,3 +9,5 @@ class AnalyticsServiceMock extends Mock implements IAnalyticsService {}
 class SharingServiceMock extends Mock implements ISharingService {}
 
 class BrowsingServiceMock extends Mock implements IBrowsingService {}
+
+class NavigationServiceMock extends Mock implements INavigationService {}
