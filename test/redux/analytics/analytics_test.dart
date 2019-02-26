3465b25337f560db37c171745f0c795dc3076512
Anna Domashych <doan@ciklum.com>
2019-02-26 15:47:09 +0200

    Test analytics when content is shared

--- test/redux/analytics/analytics_test.dart
+++ test/redux/analytics/analytics_test.dart
@@ -3,12 +3,17 @@ import 'package:mockito/mockito.dart';
 import 'package:redux/redux.dart';
 import 'package:test_api/test_api.dart';
 import 'package:what_when_where/common/timer_type.dart';
+import 'package:what_when_where/db_chgk_info/models/question.dart';
+import 'package:what_when_where/db_chgk_info/models/tour.dart';
+import 'package:what_when_where/db_chgk_info/models/tournament.dart';
 import 'package:what_when_where/ioc/container.dart';
 import 'package:what_when_where/redux/app/state.dart';
 import 'package:what_when_where/redux/app/store.dart';
 import 'package:what_when_where/redux/questions/actions.dart';
+import 'package:what_when_where/redux/sharing/actions.dart';
 import 'package:what_when_where/redux/timer/actions.dart';
 import 'package:what_when_where/services/analytics.dart';
+import 'package:what_when_where/services/sharing.dart';
 
 import '../../mocks.dart';
 
@@ -16,6 +21,8 @@ void main() {
   IAnalyticsService analyticsServiceMock;
   Store<AppState> store;
 
+  WWWIoC.container.register<ISharingService>((c) => SharingServiceMock(),
+      defaultMode: InjectMode.singleton);
   WWWIoC.container.register<IAnalyticsService>((c) => analyticsServiceMock,
       defaultMode: InjectMode.create);
 
@@ -54,6 +61,22 @@ void main() {
     test(
       '$HideAnswer',
       () => analyticsTest(const HideAnswer(0), 'hide_answer'),
+    );
+
+    test(
+      '$ShareQuestion',
+      () => analyticsTest(ShareQuestion(const Question()), 'share_question'),
+    );
+
+    test(
+      '$ShareTour',
+      () => analyticsTest(ShareTour(const Tour()), 'share_tour'),
+    );
+
+    test(
+      '$ShareTournament',
+      () => analyticsTest(
+          ShareTournament(const Tournament()), 'share_tournament'),
     );
   });
 
