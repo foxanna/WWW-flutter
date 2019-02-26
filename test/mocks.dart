16df8e9537595cdb9e8311ba875c4873dc8a1913
Anna Domashych <doan@ciklum.com>
2019-02-26 15:44:07 +0200

    Create SharingServiceMock

--- test/mocks.dart
+++ test/mocks.dart
@@ -1,4 +1,7 @@
 import 'package:mockito/mockito.dart';
 import 'package:what_when_where/services/analytics.dart';
+import 'package:what_when_where/services/sharing.dart';
 
 class AnalyticsServiceMock extends Mock implements IAnalyticsService {}
+
+class SharingServiceMock extends Mock implements ISharingService {}
