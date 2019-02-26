d1bc885b2f9bc6af977aeceac76e689eac5159c1
Anna Domashych <doan@ciklum.com>
2019-02-26 15:53:21 +0200

    Create BrowsingServiceMock

--- test/mocks.dart
+++ test/mocks.dart
@@ -1,7 +1,10 @@
 import 'package:mockito/mockito.dart';
 import 'package:what_when_where/services/analytics.dart';
+import 'package:what_when_where/services/browsing.dart';
 import 'package:what_when_where/services/sharing.dart';
 
 class AnalyticsServiceMock extends Mock implements IAnalyticsService {}
 
 class SharingServiceMock extends Mock implements ISharingService {}
+
+class BrowsingServiceMock extends Mock implements IBrowsingService {}
