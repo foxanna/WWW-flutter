a77a95792b8aa414c0883d70d185a61cca6b1e82
Anna Domashych <doan@ciklum.com>
2019-02-27 12:05:29 +0200

    Register UrlLauncher in IoC

--- lib/ioc/bootstraper.dart
+++ lib/ioc/bootstraper.dart
@@ -4,6 +4,7 @@ import 'package:what_when_where/services/analytics.dart';
 import 'package:what_when_where/services/browsing.dart';
 import 'package:what_when_where/services/navigation.dart';
 import 'package:what_when_where/services/sharing.dart';
+import 'package:what_when_where/services/url_launcher.dart';
 
 class Bootstrapper {
   static final Bootstrapper _instance = Bootstrapper._();
@@ -22,6 +23,8 @@ class Bootstrapper {
     _container.register<IBrowsingService>((c) => BrowsingService.ioc(),
         defaultMode: InjectMode.singleton);
     _container.register<INavigationService>((c) => NavigationService.ioc(),
+        defaultMode: InjectMode.singleton);
+    _container.register<IUrlLauncher>((c) => UrlLauncher.ioc(),
         defaultMode: InjectMode.singleton);
   }
 }
