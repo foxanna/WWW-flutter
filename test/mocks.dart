7487e639a9cf3caeba295bd3080441e40833c5e1
Anna Domashych <doan@ciklum.com>
2019-02-27 12:06:15 +0200

    Create UrlLauncherMock

--- test/mocks.dart
+++ test/mocks.dart
@@ -3,6 +3,7 @@ import 'package:what_when_where/services/analytics.dart';
 import 'package:what_when_where/services/browsing.dart';
 import 'package:what_when_where/services/navigation.dart';
 import 'package:what_when_where/services/sharing.dart';
+import 'package:what_when_where/services/url_launcher.dart';
 
 class AnalyticsServiceMock extends Mock implements IAnalyticsService {}
 
@@ -11,3 +12,5 @@ class SharingServiceMock extends Mock implements ISharingService {}
 class BrowsingServiceMock extends Mock implements IBrowsingService {}
 
 class NavigationServiceMock extends Mock implements INavigationService {}
+
+class UrlLauncherMock extends Mock implements IUrlLauncher {}
