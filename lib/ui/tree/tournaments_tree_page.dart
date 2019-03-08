eb9b231f812309cf39a4c89cb1f62b11d3b08076
Anna Domashych <doan@ciklum.com>
2019-03-08 15:57:36 +0200

    Create TournamentsTreePage stub

---
+++ lib/ui/tree/tournaments_tree_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TournamentsTreePage extends StatelessWidget {
+  static const String routeName = 'tree';
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: Container(),
+      );
+}
