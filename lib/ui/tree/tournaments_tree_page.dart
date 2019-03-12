784f880917fbbcd443ea5ae0ed81dcba7a545d47
Anna Domashych <doan@ciklum.com>
2019-03-12 20:27:26 +0200

    Declare TournamentsTreePage parameters

--- lib/ui/tree/tournaments_tree_page.dart
+++ lib/ui/tree/tournaments_tree_page.dart
@@ -3,6 +3,15 @@ import 'package:flutter/material.dart';
 class TournamentsTreePage extends StatelessWidget {
   static const String routeName = 'tree';
 
+  final String rootId;
+  final String title;
+
+  const TournamentsTreePage({
+    Key key,
+    @required this.rootId,
+    this.title,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) => Scaffold(
         body: Container(),
