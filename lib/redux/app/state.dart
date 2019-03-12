6011e1482293701b198694fffacacb0e676318a8
Anna Domashych <doan@ciklum.com>
2019-03-12 20:23:02 +0200

    Add TournamentsTreeState to AppState

--- lib/redux/app/state.dart
+++ lib/redux/app/state.dart
@@ -7,6 +7,7 @@ import 'package:what_when_where/redux/settings/state.dart';
 import 'package:what_when_where/redux/timer/state.dart';
 import 'package:what_when_where/redux/tornament/state.dart';
 import 'package:what_when_where/redux/tours/state.dart';
+import 'package:what_when_where/redux/tree/state.dart';
 
 @immutable
 class AppState {
@@ -18,6 +19,7 @@ class AppState {
   final SearchState searchState;
   final SettingsState settingsState;
   final RandomQuestionsState randomQuestionsState;
+  final TournamentsTreeState tournamentsTreeState;
 
   const AppState({
     this.timerState,
@@ -28,6 +30,7 @@ class AppState {
     this.searchState,
     this.settingsState,
     this.randomQuestionsState,
+    this.tournamentsTreeState,
   });
 
   AppState.initial()
@@ -40,5 +43,6 @@ class AppState {
           searchState: SearchState.initial(),
           settingsState: const SettingsState.initial(),
           randomQuestionsState: const RandomQuestionsState.initial(),
+          tournamentsTreeState: TournamentsTreeState.initial(),
         );
 }
