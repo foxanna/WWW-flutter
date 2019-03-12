29887fdef7d4c18c3016c8a94a028e7b8bac9691
Anna Domashych <doan@ciklum.com>
2019-03-12 20:28:33 +0200

    Use tree icon

--- lib/ui/latest_tournaments/tree_icon_button.dart
+++ lib/ui/latest_tournaments/tree_icon_button.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:what_when_where/redux/app/state.dart';
 import 'package:what_when_where/redux/navigation/actions.dart';
+import 'package:what_when_where/resources/custom_icons.dart';
 import 'package:what_when_where/resources/strings.dart';
 
 class LatestTournamentsTreeIconButton extends StatelessWidget {
@@ -9,12 +10,12 @@ class LatestTournamentsTreeIconButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => IconButton(
-        icon: const Icon(Icons.format_strikethrough),
+        icon: const Icon(CustomIcons.tree),
         tooltip: Strings.tournamentsTree,
         onPressed: () => _openTournamentsTree(context),
       );
 
   void _openTournamentsTree(BuildContext context) =>
       StoreProvider.of<AppState>(context)
-          .dispatch(OpenTournamentsTreePage(context));
+          .dispatch(OpenTournamentsTreePage(context: context));
 }
