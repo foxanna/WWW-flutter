d1c2ab75bd7c6f68ceedcb5ac9b34b17d2a14235
Anna Domashych <anna.domashych@gmail.com>
2019-03-30 16:50:14 +0200

    Display question number from question in tour questions and question index in random questions

--- lib/ui/question/question_number.dart
+++ lib/ui/question/question_number.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:what_when_where/redux/app/state.dart';
 import 'package:what_when_where/resources/strings.dart';
 
 class QuestionNumber extends StatelessWidget {
@@ -10,11 +12,18 @@ class QuestionNumber extends StatelessWidget {
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Text(
-        '${Strings.question} ${index + 1}',
-        style: Theme.of(context)
-            .textTheme
-            .headline
-            .copyWith(color: Theme.of(context).accentColor),
+  Widget build(BuildContext context) => StoreConnector<AppState, String>(
+        distinct: true,
+        converter: (store) =>
+            store.state.randomQuestionsState.randomQuestionsAreDisplayed
+                ? (index + 1).toString()
+                : store.state.questionsState.questions[index].question.number,
+        builder: (context, questionIndex) => Text(
+              '${Strings.question} $questionIndex',
+              style: Theme.of(context)
+                  .textTheme
+                  .headline
+                  .copyWith(color: Theme.of(context).accentColor),
+            ),
       );
 }
