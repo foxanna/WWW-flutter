d24397efb0aad122c0ee876a828e13f97d64be91
Anna Domashych <anna.domashych@gmail.com>
2019-04-22 12:04:47 +0300

    Create CrashService

---
+++ lib/services/crashes.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
+
+abstract class ICrashService {
+  void init();
+}
+
+class CrashService extends ICrashService {
+  factory CrashService.ioc() => CrashService._();
+
+  CrashService._();
+
+  @override
+  void init() {
+    Crashlytics.instance.enableInDevMode = true;
+
+    FlutterError.onError = (FlutterErrorDetails details) {
+      Crashlytics.instance.onError(details);
+    };
+  }
+}
