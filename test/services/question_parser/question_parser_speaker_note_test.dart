f3e076282b79ad1bea0f80de4066e4c645384ecd
Anna Domashych <anna.domashych@gmail.com>
2019-05-27 01:37:00 +0300

    Reorganize SpeakerNote tests

--- test/services/question_parser/question_parser_speaker_note_test.dart
+++ test/services/question_parser/question_parser_speaker_note_test.dart
@@ -8,96 +8,180 @@ void main() {
   final supportedSpeakerNotes = [
     'Ведущему',
     'Чтецу',
-//      'Примечание ведущему',
+    'Примечание ведущему',
   ];
 
-  supportedSpeakerNotes.forEach(
-    (x) => test(
-          'Speaker note "$x"',
-          () => questionParserTest(
-                  text: '[$x: раз, два, три]',
-                  expectedSections: <dynamic>[
-                    SpeakerNoteSection('[$x: раз, два, три]'),
-                  ]),
-        ),
-  );
+  group('Speaker note', () {
+    final _test = (String x) => questionParserTest(
+            text: '[$x: раз, два, три]',
+            expectedSections: <dynamic>[
+              SpeakerNoteSection('[$x: раз, два, три]'),
+            ]);
 
-  supportedSpeakerNotes.forEach(
-    (x) => test(
-          'Speaker note "$x" with dot',
-          () => questionParserTest(
-                  text: '[$x: раз, два, три.]',
-                  expectedSections: <dynamic>[
-                    SpeakerNoteSection('[$x: раз, два, три.]'),
-                  ]),
-        ),
-  );
+    test(
+      supportedSpeakerNotes[0],
+      () => _test(supportedSpeakerNotes[0]),
+    );
 
-  supportedSpeakerNotes.forEach(
-    (x) => test(
-          'Speaker note "$x" in the beginning',
-          () => questionParserTest(
-                  text: '[$x: раз, два, три] Текст вопроса.',
-                  expectedSections: <dynamic>[
-                    SpeakerNoteSection('[$x: раз, два, три]'),
-                    TextSection('Текст вопроса.'),
-                  ]),
-        ),
-  );
+    test(
+      supportedSpeakerNotes[1],
+      () => _test(supportedSpeakerNotes[1]),
+    );
 
-  supportedSpeakerNotes.forEach(
-    (x) => test(
-          'Speaker note "$x" in the middle',
-          () => questionParserTest(
-                  text: 'Текст вопроса. [$x: раз, два, три] Текст.',
-                  expectedSections: <dynamic>[
-                    TextSection('Текст вопроса.'),
-                    SpeakerNoteSection('[$x: раз, два, три]'),
-                    TextSection('Текст.'),
-                  ]),
-        ),
-  );
+    test(
+      supportedSpeakerNotes[2],
+      () => _test(supportedSpeakerNotes[2]),
+    );
+  });
 
-  supportedSpeakerNotes.forEach(
-    (x) => test(
-          'Speaker note "$x" in the end',
-          () => questionParserTest(
-                  text: 'Текст вопроса. [$x: раз, два, три]',
-                  expectedSections: <dynamic>[
-                    TextSection('Текст вопроса.'),
-                    SpeakerNoteSection('[$x: раз, два, три]'),
-                  ]),
-        ),
-  );
+  group('Speaker note with dot', () {
+    final _test = (String x) => questionParserTest(
+            text: '[$x: раз, два, три.]',
+            expectedSections: <dynamic>[
+              SpeakerNoteSection('[$x: раз, два, три.]'),
+            ]);
 
-  supportedSpeakerNotes.forEach(
-    (x) => test(
-          'Speaker note "$x" several entries',
-          () => questionParserTest(
-                  text: 'Текст вопроса раз. [$x: раз, два, три] '
-                      'Текст вопроса два. [$x: четыре, пять, шесть] '
-                      'Текст вопроса три. [$x: семь, восемь, девять] ',
-                  expectedSections: <dynamic>[
-                    TextSection('Текст вопроса раз.'),
-                    SpeakerNoteSection('[$x: раз, два, три]'),
-                    TextSection('Текст вопроса два.'),
-                    SpeakerNoteSection('[$x: четыре, пять, шесть]'),
-                    TextSection('Текст вопроса три.'),
-                    SpeakerNoteSection('[$x: семь, восемь, девять]'),
-                  ]),
-        ),
-  );
+    test(
+      supportedSpeakerNotes[0],
+      () => _test(supportedSpeakerNotes[0]),
+    );
 
-  supportedSpeakerNotes.forEach(
-    (x) => test(
-          'Speaker note "$x" without spaces around []',
-          () => questionParserTest(
-                  text: 'Текст[$x: раз, два три]Текст вопроса.',
-                  expectedSections: <dynamic>[
-                    TextSection('Текст'),
-                    SpeakerNoteSection('[$x: раз, два три]'),
-                    TextSection('Текст вопроса.'),
-                  ]),
-        ),
-  );
+    test(
+      supportedSpeakerNotes[1],
+      () => _test(supportedSpeakerNotes[1]),
+    );
+
+    test(
+      supportedSpeakerNotes[2],
+      () => _test(supportedSpeakerNotes[2]),
+    );
+  });
+
+  group('Speaker note in the beginning', () {
+    final _test = (String x) => questionParserTest(
+            text: '[$x: раз, два, три] Текст вопроса.',
+            expectedSections: <dynamic>[
+              SpeakerNoteSection('[$x: раз, два, три]'),
+              TextSection('Текст вопроса.'),
+            ]);
+
+    test(
+      supportedSpeakerNotes[0],
+      () => _test(supportedSpeakerNotes[0]),
+    );
+
+    test(
+      supportedSpeakerNotes[1],
+      () => _test(supportedSpeakerNotes[1]),
+    );
+
+    test(
+      supportedSpeakerNotes[2],
+      () => _test(supportedSpeakerNotes[2]),
+    );
+  });
+
+  group('Speaker note in the middle', () {
+    final _test = (String x) => questionParserTest(
+            text: 'Текст вопроса. [$x: раз, два, три] Текст.',
+            expectedSections: <dynamic>[
+              TextSection('Текст вопроса.'),
+              SpeakerNoteSection('[$x: раз, два, три]'),
+              TextSection('Текст.'),
+            ]);
+
+    test(
+      supportedSpeakerNotes[0],
+      () => _test(supportedSpeakerNotes[0]),
+    );
+
+    test(
+      supportedSpeakerNotes[1],
+      () => _test(supportedSpeakerNotes[1]),
+    );
+
+    test(
+      supportedSpeakerNotes[2],
+      () => _test(supportedSpeakerNotes[2]),
+    );
+  });
+
+  group('Speaker note in the end', () {
+    final _test = (String x) => questionParserTest(
+            text: 'Текст вопроса. [$x: раз, два, три]',
+            expectedSections: <dynamic>[
+              TextSection('Текст вопроса.'),
+              SpeakerNoteSection('[$x: раз, два, три]'),
+            ]);
+
+    test(
+      supportedSpeakerNotes[0],
+      () => _test(supportedSpeakerNotes[0]),
+    );
+
+    test(
+      supportedSpeakerNotes[1],
+      () => _test(supportedSpeakerNotes[1]),
+    );
+
+    test(
+      supportedSpeakerNotes[2],
+      () => _test(supportedSpeakerNotes[2]),
+    );
+  });
+
+  group('Speaker note several entries', () {
+    final _test = (String x) => questionParserTest(
+            text: 'Текст вопроса раз. [$x: раз, два, три] '
+                'Текст вопроса два. [$x: четыре, пять, шесть] '
+                'Текст вопроса три. [$x: семь, восемь, девять] ',
+            expectedSections: <dynamic>[
+              TextSection('Текст вопроса раз.'),
+              SpeakerNoteSection('[$x: раз, два, три]'),
+              TextSection('Текст вопроса два.'),
+              SpeakerNoteSection('[$x: четыре, пять, шесть]'),
+              TextSection('Текст вопроса три.'),
+              SpeakerNoteSection('[$x: семь, восемь, девять]'),
+            ]);
+
+    test(
+      supportedSpeakerNotes[0],
+      () => _test(supportedSpeakerNotes[0]),
+    );
+
+    test(
+      supportedSpeakerNotes[1],
+      () => _test(supportedSpeakerNotes[1]),
+    );
+
+    test(
+      supportedSpeakerNotes[2],
+      () => _test(supportedSpeakerNotes[2]),
+    );
+  });
+
+  group('Speaker note without spaces around', () {
+    final _test = (String x) => questionParserTest(
+            text: 'Текст[$x: раз, два три]Текст вопроса.',
+            expectedSections: <dynamic>[
+              TextSection('Текст'),
+              SpeakerNoteSection('[$x: раз, два три]'),
+              TextSection('Текст вопроса.'),
+            ]);
+
+    test(
+      supportedSpeakerNotes[0],
+      () => _test(supportedSpeakerNotes[0]),
+    );
+
+    test(
+      supportedSpeakerNotes[1],
+      () => _test(supportedSpeakerNotes[1]),
+    );
+
+    test(
+      supportedSpeakerNotes[2],
+      () => _test(supportedSpeakerNotes[2]),
+    );
+  });
 }
