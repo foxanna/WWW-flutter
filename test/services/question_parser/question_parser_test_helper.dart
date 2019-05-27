c766e05e762ca7eb3fa2fdd88048f2f70564f4a5
Anna Domashych <anna.domashych@gmail.com>
2019-05-28 00:50:54 +0300

    Refactor

--- test/services/question_parser/question_parser_test_helper.dart
+++ test/services/question_parser/question_parser_test_helper.dart
@@ -1,10 +1,11 @@
 import 'package:collection/collection.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:what_when_where/services/question_parser/question_parser.dart';
 
 final Function questionParserTest = ({
-  String text,
-  List<dynamic> expectedSections,
+  @required String text,
+  @required List<dynamic> expectedSections,
 }) {
   final sections = QuestionParser.split(text).toList();
   final equals =
