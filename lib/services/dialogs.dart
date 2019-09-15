0fd5aa3eefa7452d37dcb7223cd0dd9289df6d29
Anna Domashych <anna.domashych@gmail.com>
2019-09-15 15:29:13 +0300

    Create a DialogService

---
+++ lib/services/dialogs.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+abstract class IDialogService {
+  Future<T> show<T>({@required WidgetBuilder builder});
+}
+
+abstract class IDialogHelper {
+  void registerPresenter(
+      Future<T> Function<T>(WidgetBuilder builder) presenter);
+}
+
+class DialogService implements IDialogService, IDialogHelper {
+  factory DialogService.ioc() => DialogService._();
+
+  DialogService._();
+
+  Future<T> Function<T>(WidgetBuilder builder) _presenter;
+
+  @override
+  Future<T> show<T>({@required WidgetBuilder builder}) async {
+    assert(_presenter != null);
+    assert(builder != null);
+
+    final T result = await _presenter(builder);
+    return result;
+  }
+
+  @override
+  void registerPresenter(
+          Future<T> Function<T>(WidgetBuilder builder) presenter) =>
+      _presenter = presenter;
+}
