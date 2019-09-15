02cffc5919d24fc96fc5d9e1ceb541fc79f2307e
Anna Domashych <anna.domashych@gmail.com>
2019-09-15 15:30:55 +0300

    Create a DialogPresenter responsible for displaying dialogs

---
+++ lib/ui/common/dialog_presenter.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:what_when_where/ioc/container.dart';
+import 'package:what_when_where/services/dialogs.dart';
+
+class DialogPresenter extends StatefulWidget {
+  final Widget child;
+
+  DialogPresenter({Key key, @required this.child}) : super(key: key);
+
+  @override
+  _DialogPresenterState createState() => _DialogPresenterState();
+}
+
+class _DialogPresenterState extends State<DialogPresenter> {
+  @override
+  void initState() {
+    super.initState();
+    WWWIoC.container<IDialogHelper>().registerPresenter(_showDialog);
+  }
+
+  @override
+  Widget build(BuildContext context) => widget.child;
+
+  Future<T> _showDialog<T>(WidgetBuilder builder) =>
+      showDialog<T>(context: context, builder: builder);
+}
