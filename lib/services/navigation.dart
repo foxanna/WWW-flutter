0ebe86e3fb498f0ba6d5736ead855902f25365cf
Anna Domashych <anna.domashych@gmail.com>
2019-09-15 15:37:10 +0300

    Remove IDialog from NavigationService

--- lib/services/navigation.dart
+++ lib/services/navigation.dart
@@ -1,17 +1,11 @@
 import 'package:flutter/material.dart';
 
-abstract class IDialog {
-  void show();
-}
-
 abstract class INavigationService {
   void navigateToPage({
     BuildContext context,
     String routeName,
     WidgetBuilder builder,
   });
-
-  void showDialog(IDialog dialog);
 }
 
 class NavigationService extends INavigationService {
@@ -26,10 +20,10 @@ class NavigationService extends INavigationService {
     @required WidgetBuilder builder,
   }) =>
       Navigator.push<Object>(
-          context,
-          MaterialPageRoute(
-              settings: RouteSettings(name: routeName), builder: builder));
-
-  @override
-  void showDialog(IDialog dialog) => dialog.show();
+        context,
+        MaterialPageRoute(
+          settings: RouteSettings(name: routeName),
+          builder: builder,
+        ),
+      );
 }
