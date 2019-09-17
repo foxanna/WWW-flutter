a68c8a33dc5aa3a1a83bcb048bdb17ca1c5d214e
Anna Domashych <anna.domashych@gmail.com>
2019-09-17 11:37:10 +0300

    Refactor AppMiddleware to correspond to the new instance-base approach

--- lib/redux/app/middleware.dart
+++ lib/redux/app/middleware.dart
@@ -1,4 +1,5 @@
 import 'package:redux/redux.dart';
+import 'package:what_when_where/ioc/container.dart';
 import 'package:what_when_where/redux/analytics/middleware.dart';
 import 'package:what_when_where/redux/app/state.dart';
 import 'package:what_when_where/redux/browsing/middleware.dart';
@@ -17,24 +18,67 @@ import 'package:what_when_where/redux/timer/middleware.dart';
 import 'package:what_when_where/redux/tornament/middleware.dart';
 import 'package:what_when_where/redux/tours/middleware.dart';
 import 'package:what_when_where/redux/tree/middleware.dart';
+import 'package:what_when_where/services/analytics.dart';
+import 'package:what_when_where/services/browsing.dart';
+import 'package:what_when_where/services/crashes.dart';
+import 'package:what_when_where/services/dialogs.dart';
+import 'package:what_when_where/services/navigation.dart';
+import 'package:what_when_where/services/preferences.dart';
+import 'package:what_when_where/services/rating.dart';
+import 'package:what_when_where/services/sharing.dart';
+import 'package:what_when_where/services/sound.dart';
+import 'package:what_when_where/services/url_launcher.dart';
+import 'package:what_when_where/services/vibrating.dart';
 
 class AppMiddleware {
-  static final middleware = <Middleware<AppState>>[]
+  final IContainer _container;
+
+  List<Middleware<AppState>> _middleware;
+  Iterable<Middleware<AppState>> get middleware => _middleware;
+
+  AppMiddleware({IContainer container}) : _container = container {
+    _middleware = _createMiddleware();
+  }
+
+  List<Middleware<AppState>> _createMiddleware() => []
     ..addAll(LogsMiddleware.middleware)
-    ..addAll(InitMiddleware.middleware)
-    ..addAll(AnalyticsMiddleware.middleware)
-    ..addAll(TimerMiddleware.middleware)
-    ..addAll(ShareMiddleware.middleware)
-    ..addAll(BrowseMiddleware.middleware)
-    ..addAll(NavigationMiddleware.middleware)
-    ..addAll(DialogMiddleware.middleware)
-    ..addAll(ToursMiddleware.middleware)
-    ..addAll(TournamentMiddleware.middleware)
-    ..addAll(LatestTournamentsMiddleware.middleware)
-    ..addAll(MiscMiddleware.middleware)
-    ..addAll(SearchMiddleware.middleware)
-    ..addAll(SettingsMiddleware.middleware)
-    ..addAll(RandomQuestionsMiddleware.middleware)
-    ..addAll(TournamentsTreeMiddleware.middleware)
-    ..addAll(RatingMiddleware.middleware);
+    ..addAll(InitMiddleware(
+      crashService: _container<ICrashService>(),
+      soundService: _container<ISoundService>(),
+    ).middleware)
+    ..addAll(AnalyticsMiddleware(
+      analyticsService: _container<IAnalyticsService>(),
+    ).middleware)
+    ..addAll(TimerMiddleware(
+      soundService: _container<ISoundService>(),
+      vibratingService: _container<IVibratingService>(),
+    ).middleware)
+    ..addAll(ShareMiddleware(
+      sharingService: _container<ISharingService>(),
+    ).middleware)
+    ..addAll(BrowseMiddleware(
+      browsingService: _container<IBrowsingService>(),
+    ).middleware)
+    ..addAll(NavigationMiddleware(
+      navigationService: _container<INavigationService>(),
+    ).middleware)
+    ..addAll(DialogMiddleware(
+      dialogService: _container<IDialogService>(),
+    ).middleware)
+    ..addAll(ToursMiddleware().middleware)
+    ..addAll(TournamentMiddleware().middleware)
+    ..addAll(LatestTournamentsMiddleware().middleware)
+    ..addAll(MiscMiddleware(
+      urlLauncher: _container<IUrlLauncher>(),
+    ).middleware)
+    ..addAll(SearchMiddleware().middleware)
+    ..addAll(SettingsMiddleware(
+      preferences: _container<IPreferences>(),
+    ).middleware)
+    ..addAll(RandomQuestionsMiddleware().middleware)
+    ..addAll(TournamentsTreeMiddleware().middleware)
+    ..addAll(RatingMiddleware(
+      preferences: _container<IPreferences>(),
+      ratingService: _container<IRatingService>(),
+    ).middleware);
 }
