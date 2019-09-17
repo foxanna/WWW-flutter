c7cacc08ffedf484f80e099dce21f5dbf6590995
Anna Domashych <anna.domashych@gmail.com>
2019-09-17 11:13:04 +0300

    Refactor IoC container, hide implementation behind IContainer interface

--- lib/ioc/container.dart
+++ lib/ioc/container.dart
@@ -1,5 +1,33 @@
 import 'package:dioc/dioc.dart';
 
-class WWWIoC {
-  static final Container container = Container();
+typedef ICreator<T> = T Function(IContainer container);
+
+abstract class IContainer {
+  void registerSingleton<T>(ICreator<T> creator);
+  void registerMultiInstance<T>(ICreator<T> creator);
+
+  T call<T>();
+}
+
+class WWWContainer implements IContainer {
+  static final WWWContainer _instance = WWWContainer._();
+
+  factory WWWContainer() => _instance;
+
+  final Container _container = Container();
+
+  WWWContainer._() {
+    registerSingleton<IContainer>((c) => this);
+  }
+
+  @override
+  T call<T>() => _container<T>();
+
+  @override
+  void registerMultiInstance<T>(ICreator<T> creator) =>
+      _container.register((c) => creator(this), defaultMode: InjectMode.create);
+
+  @override
+  void registerSingleton<T>(ICreator<T> creator) => _container
+      .register((c) => creator(this), defaultMode: InjectMode.singleton);
 }
