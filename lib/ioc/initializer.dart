92eda62b7d0dec688804d8398cbd655459089da1
Anna Domashych <anna.domashych@gmail.com>
2019-09-17 12:44:02 +0300

    Register loaders in IoC

--- lib/ioc/initializer.dart
+++ lib/ioc/initializer.dart
@@ -1,3 +1,10 @@
+import 'package:what_when_where/db_chgk_info/http_client.dart';
+import 'package:what_when_where/db_chgk_info/loaders/latest_tournaments_loader.dart';
+import 'package:what_when_where/db_chgk_info/loaders/random_questions_loader.dart';
+import 'package:what_when_where/db_chgk_info/loaders/search_loader.dart';
+import 'package:what_when_where/db_chgk_info/loaders/tour_details_loader.dart';
+import 'package:what_when_where/db_chgk_info/loaders/tournament_details_loader.dart';
+import 'package:what_when_where/db_chgk_info/loaders/tournaments_tree_loader.dart';
 import 'package:what_when_where/global/navigatorKey.dart';
 import 'package:what_when_where/ioc/container.dart';
 import 'package:what_when_where/services/analytics.dart';
@@ -19,6 +26,7 @@ class IoCInitializer {
 
   void init() {
     _ServicesInitializer(container: _container).init();
+    _LoadersInitializer(container: _container).init();
   }
 }
 
@@ -46,5 +54,38 @@ class _ServicesInitializer {
     _container.registerSingleton<ISoundService>((c) => SoundService.ioc());
     _container.registerSingleton<ICrashService>((c) => CrashService.ioc());
     _container.registerSingleton<IRatingService>((c) => RatingService.ioc());
+  }
+}
+
+class _LoadersInitializer {
+  final IContainer _container;
+
+  _LoadersInitializer({IContainer container}) : _container = container;
+
+  void init() {
+    _container.registerSingleton<IHttpClient>((c) => HttpClient.ioc());
+    _container.registerSingleton<ILatestTournamentsLoader>(
+        (c) => LatestTournamentsLoader.ioc(
+              httpClient: c<IHttpClient>(),
+            ));
+    _container.registerSingleton<IRandomQuestionsLoader>(
+        (c) => RandomQuestionsLoader.ioc(
+              httpClient: c<IHttpClient>(),
+            ));
+    _container.registerSingleton<ISearchLoader>((c) => SearchLoader.ioc(
+          httpClient: c<IHttpClient>(),
+        ));
+    _container
+        .registerSingleton<ITourDetailsLoader>((c) => TourDetailsLoader.ioc(
+              httpClient: c<IHttpClient>(),
+            ));
+    _container.registerSingleton<ITournamentDetailsLoader>(
+        (c) => TournamentDetailsLoader.ioc(
+              httpClient: c<IHttpClient>(),
+            ));
+    _container.registerSingleton<ITournamentsTreeLoader>(
+        (c) => TournamentsTreeLoader.ioc(
+              httpClient: c<IHttpClient>(),
+            ));
   }
 }
