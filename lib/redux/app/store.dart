b417fca945e0c9960717b2f7c3b1d77e7afdc8c0
Anna Domashych <anna.domashych@gmail.com>
2019-09-17 11:41:10 +0300

    Use new AppMiddleware

--- lib/redux/app/store.dart
+++ lib/redux/app/store.dart
@@ -1,10 +1,11 @@
 import 'package:redux/redux.dart';
+import 'package:what_when_where/ioc/container.dart';
 import 'package:what_when_where/redux/app/middleware.dart';
 import 'package:what_when_where/redux/app/reducer.dart';
 import 'package:what_when_where/redux/app/state.dart';
 
-Store<AppState> createStore() => Store<AppState>(
+Store<AppState> createStore(IContainer container) => Store<AppState>(
       AppReducer.reducer,
       initialState: AppState.initial(),
-      middleware: AppMiddleware.middleware,
+      middleware: AppMiddleware(container: container).middleware,
     );
