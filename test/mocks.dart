5782d56aa0f3546451b731ac867c68b0c9451c63
Anna Domashych <anna.domashych@gmail.com>
2019-09-18 11:51:48 +0300

    Declare mocks for all injected services and loaders

--- test/mocks.dart
+++ test/mocks.dart
@@ -1,11 +1,21 @@
 import 'package:mockito/mockito.dart';
+import 'package:what_when_where/db_chgk_info/loaders/latest_tournaments_loader.dart';
+import 'package:what_when_where/db_chgk_info/loaders/random_questions_loader.dart';
+import 'package:what_when_where/db_chgk_info/loaders/search_loader.dart';
+import 'package:what_when_where/db_chgk_info/loaders/tour_details_loader.dart';
+import 'package:what_when_where/db_chgk_info/loaders/tournament_details_loader.dart';
+import 'package:what_when_where/db_chgk_info/loaders/tournaments_tree_loader.dart';
 import 'package:what_when_where/services/analytics.dart';
 import 'package:what_when_where/services/browsing.dart';
+import 'package:what_when_where/services/crashes.dart';
 import 'package:what_when_where/services/dialogs.dart';
 import 'package:what_when_where/services/navigation.dart';
 import 'package:what_when_where/services/preferences.dart';
+import 'package:what_when_where/services/rating.dart';
 import 'package:what_when_where/services/sharing.dart';
+import 'package:what_when_where/services/sound.dart';
 import 'package:what_when_where/services/url_launcher.dart';
+import 'package:what_when_where/services/vibrating.dart';
 
 class AnalyticsServiceMock extends Mock implements IAnalyticsService {}
 
@@ -17,6 +27,32 @@ class NavigationServiceMock extends Mock implements INavigationService {}
 
 class DialogServiceMock extends Mock implements IDialogService {}
 
+class DialogHelperMock extends Mock implements IDialogHelper {}
+
 class UrlLauncherMock extends Mock implements IUrlLauncher {}
 
 class PreferencesMock extends Mock implements IPreferences {}
+
+class VibratingServiceMock extends Mock implements IVibratingService {}
+
+class SoundServiceMock extends Mock implements ISoundService {}
+
+class CrashServiceMock extends Mock implements ICrashService {}
+
+class RatingServiceMock extends Mock implements IRatingService {}
+
+class LatestTournamentsLoaderMock extends Mock
+    implements ILatestTournamentsLoader {}
+
+class RandomQuestionsLoaderMock extends Mock implements IRandomQuestionsLoader {
+}
+
+class SearchLoaderMock extends Mock implements ISearchLoader {}
+
+class TourDetailsLoaderMock extends Mock implements ITourDetailsLoader {}
+
+class TournamentDetailsLoaderMock extends Mock
+    implements ITournamentDetailsLoader {}
+
+class TournamentsTreeLoaderMock extends Mock implements ITournamentsTreeLoader {
+}
