16421ab7a618a230d95d50925428a3ca522baca3
Anna Domashych <anna.domashych@gmail.com>
2020-03-31 19:32:08 +0300

    More styling

--- lib/resources/style_configuration.dart
+++ lib/resources/style_configuration.dart
@@ -30,21 +30,85 @@ class StyleConfiguration {
 
 class TournamentDetailsStyleConfiguration {
   const TournamentDetailsStyleConfiguration({
+    this.questionTextStyle,
+    this.tourCardSize,
+    this.tourTitleTextStyle,
+    this.tourContentPadding,
+    this.tourColorGenerator,
+    this.elevation,
+    this.cornerRadius,
+    this.shape,
+    this.tournamentTitlePadding,
     this.tournamentTitleTextStyle,
+    this.actionBarBackgroundColor,
+    this.actionBarIconTheme,
+    this.scaffoldBackground,
   });
 
   final TextStyle tournamentTitleTextStyle;
-}
+  final TextStyle tourTitleTextStyle;
+  final TextStyle questionTextStyle;
+  final Color actionBarBackgroundColor;
+  final IconThemeData actionBarIconTheme;
+  final EdgeInsets tournamentTitlePadding;
+  final EdgeInsets tourContentPadding;
+  final Color scaffoldBackground;
+  final ShapeBorder shape;
+  final Radius cornerRadius;
+  final double elevation;
+  final Color Function(int index) tourColorGenerator;
+  final Size tourCardSize;
 
   factory TournamentDetailsStyleConfiguration.create(
       {@required BuildContext context}) {
     final theme = Theme.of(context);
+    const radiusValue = 32.0;
+    const radius = Radius.circular(radiusValue);
+    const toursColorsCount = 5;
+    final firstTourColor = HSLColor.fromColor(theme.accentColor);
+    final tourColorTween = ColorTween(
+        begin: firstTourColor.toColor(),
+        end: firstTourColor
+            .withLightness(
+                firstTourColor.lightness - (toursColorsCount - 1) * 0.04)
+            .toColor());
 
-  return TournamentDetailsStyleConfiguration(
-    tournamentTitleTextStyle: theme.textTheme.headline4.copyWith(
-      color: theme.primaryColor,
-    ),
-  );
+    return TournamentDetailsStyleConfiguration(
+      tournamentTitleTextStyle: theme.textTheme.headline4.copyWith(
+        color: theme.primaryColor,
+      ),
+      tourTitleTextStyle: theme.accentTextTheme.headline5,
+      actionBarBackgroundColor: theme.cardColor,
+      actionBarIconTheme: theme.iconTheme,
+      scaffoldBackground: theme.primaryColor,
+      tournamentTitlePadding: EdgeInsets.only(
+        bottom: radiusValue,
+        left: kMinInteractiveDimension,
+        right: kMinInteractiveDimension,
+      ),
+      tourContentPadding: EdgeInsets.only(
+        bottom: radiusValue,
+        top: radiusValue * 2 + theme.cardTheme.elevation,
+        left: kMinInteractiveDimension,
+        right: kMinInteractiveDimension,
+      ),
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(bottomLeft: radius),
+      ),
+      cornerRadius: radius,
+      elevation: theme.cardTheme.elevation,
+      tourColorGenerator: (int index) {
+        const patternLength = (toursColorsCount - 1) * 2;
+        final multiplier = (index ~/ (toursColorsCount - 1)).isEven
+            ? index % patternLength
+            : (patternLength - index % patternLength);
+        final color = tourColorTween.lerp(multiplier / (toursColorsCount - 1));
+        return color;
+      },
+      tourCardSize: const Size(150, 200),
+      questionTextStyle: theme.textTheme.subtitle1,
+    );
+  }
 }
 
 class TournamentsGridStyleConfiguration {
