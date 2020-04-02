d219a7935e39065da8d4e23d4794bc1c189dc697
Anna Domashych <anna.domashych@gmail.com>
2020-04-02 14:40:05 +0300

    Take scaleFactor into account

--- lib/utils/measure_text.dart
+++ lib/utils/measure_text.dart
@@ -4,6 +4,7 @@ Size measureText(
         BuildContext context, String text, TextStyle style, double width) =>
     (TextPainter(
             text: TextSpan(text: text, style: style),
+            textScaleFactor: MediaQuery.of(context).textScaleFactor,
             textDirection: Directionality.of(context))
           ..layout(minWidth: 0, maxWidth: width))
         .size;
