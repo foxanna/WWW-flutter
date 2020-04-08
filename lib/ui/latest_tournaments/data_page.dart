0ed5c8c80980c2bc6409a003a7e77633605126c6
Anna Domashych <anna.domashych@gmail.com>
2020-04-08 16:29:34 +0300

    Move progressbar and error to footer

--- lib/ui/latest_tournaments/data_page.dart
+++ lib/ui/latest_tournaments/data_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:what_when_where/redux/app/state.dart';
 import 'package:what_when_where/redux/latest/state.dart';
-import 'package:what_when_where/resources/dimensions.dart';
 import 'package:what_when_where/ui/common/progress_indicator.dart';
 import 'package:what_when_where/ui/common/tournaments_grid.dart';
 import 'package:what_when_where/ui/latest_tournaments/error_message.dart';
@@ -15,24 +14,22 @@ class LatestTournamentsDataPage extends StatelessWidget {
       StoreConnector<AppState, LatestTournamentsState>(
         distinct: true,
         converter: (store) => store.state.latestTournamentsState,
-        builder: (context, state) => CustomScrollView(
-          slivers: [
-            TournamentsGrid(tournaments: state.data),
-            SliverToBoxAdapter(
-              child: state.isLoadingMore
-                  ? WWWProgressIndicator(
-                      padding: Dimensions.defaultPadding * 3,
-                    )
-                  : Container(),
-            ),
-            SliverToBoxAdapter(
-              child: state.hasError
-                  ? const LatestTournamentsErrorMessage(
-                      dense: true,
-                    )
-                  : Container(),
-            )
-          ],
+        builder: (context, state) => TournamentsGrid(
+          tournaments: state.data,
+          footerBuilder: (context) {
+            if (state.isLoadingMore) {
+              return const WWWProgressIndicator();
+            }
+
+            if (state.hasError) {
+              return LatestTournamentsErrorMessage(
+                exception: state.exception,
+                dense: true,
+              );
+            }
+
+            return Container();
+          },
         ),
       );
 }
