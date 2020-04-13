086c4143f19276282f693ef7035440686af8c559
Anna Domashych <anna.domashych@gmail.com>
2020-04-13 11:00:34 +0300

    Refactor InfoDialog to use configuration from Theme and StyleConfiguration

--- lib/ui/common/info_dialog.dart
+++ lib/ui/common/info_dialog.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:what_when_where/resources/dimensions.dart';
 import 'package:what_when_where/resources/strings.dart';
+import 'package:what_when_where/resources/style_configuration.dart';
 import 'package:what_when_where/ui/common/gradient_decoration.dart';
 
 class InfoDialog extends StatelessWidget {
@@ -10,31 +11,28 @@ class InfoDialog extends StatelessWidget {
   const InfoDialog({Key key, this.title, this.content}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => AlertDialog(
-        contentPadding: const EdgeInsets.symmetric(vertical: 12, horizontal: 0),
-        title: Text(
-          title,
-          style: Theme.of(context)
-              .textTheme
-              .headline6
-              .copyWith(color: Theme.of(context).accentColor),
+  Widget build(BuildContext context) {
+    final styleConfiguration =
+        StyleConfiguration.of(context).alertDialogStyleConfiguration;
+
+    return AlertDialog(
+      contentPadding: styleConfiguration.contentPadding,
+      title: Text(title),
+      content: Container(
+        foregroundDecoration:
+            GradientDecoration(color: Theme.of(context).dialogBackgroundColor),
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          padding: Dimensions.dialogContentPadding,
+          child: Text(content),
         ),
-        content: Container(
-          foregroundDecoration: GradientDecoration(
-              color: Theme.of(context).dialogBackgroundColor),
-          child: SingleChildScrollView(
-            padding: Dimensions.dialogContentPadding,
-            child: Text(
-              content,
-              style: Theme.of(context).textTheme.caption,
-            ),
-          ),
+      ),
+      actions: [
+        FlatButton(
+          onPressed: () => Navigator.of(context).pop(),
+          child: const Text(Strings.close),
         ),
-        actions: [
-          FlatButton(
-            onPressed: () => Navigator.of(context).pop(),
-            child: const Text(Strings.close),
-          ),
-        ],
-      );
+      ],
+    );
+  }
 }
