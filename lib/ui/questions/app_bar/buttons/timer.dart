78b10f2ceb7fce4efedc08582726426a0daeda0c
Anna Domashych <anna.domashych@gmail.com>
2020-04-14 14:47:41 +0300

    Refactor

--- lib/ui/questions/app_bar/buttons/timer.dart
+++ lib/ui/questions/app_bar/buttons/timer.dart
@@ -1,33 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
-import 'package:tuple/tuple.dart';
 import 'package:what_when_where/redux/app/state.dart';
 import 'package:what_when_where/redux/timer/actions.dart';
 import 'package:what_when_where/resources/strings.dart';
-import 'package:what_when_where/utils/function_holder.dart';
 
 class QuestionsBottomAppBarTimerButton extends StatelessWidget {
   @override
-  Widget build(BuildContext context) =>
-      StoreConnector<AppState, Tuple2<FunctionHolder, bool>>(
+  Widget build(BuildContext context) => StoreConnector<AppState, bool>(
         distinct: true,
-        converter: (store) {
-          final isRunning = store.state.timerState.isRunning;
-          return Tuple2<FunctionHolder, bool>(
-              FunctionHolder(() => store.dispatch(
-                    isRunning ? const StopTimer() : const StartTimer(),
-                  )),
-              isRunning);
-        },
-        builder: (context, data) {
-          final holder = data.item1;
-          final isRunning = data.item2;
-          return FloatingActionButton(
-              child: Icon(
-                isRunning ? Icons.timer_off : Icons.timer,
-              ),
-              tooltip: isRunning ? Strings.pauseTimer : Strings.startTimer,
-              onPressed: () => holder.function());
-        },
+        converter: (store) => store.state.timerState.isRunning,
+        builder: (context, isRunning) => FloatingActionButton(
+          child: Icon(
+            isRunning ? Icons.timer_off : Icons.timer,
+          ),
+          tooltip: isRunning ? Strings.pauseTimer : Strings.startTimer,
+          onPressed: () {
+            StoreProvider.of<AppState>(context)
+                .dispatch(isRunning ? const StopTimer() : const StartTimer());
+          },
+        ),
       );
 }
