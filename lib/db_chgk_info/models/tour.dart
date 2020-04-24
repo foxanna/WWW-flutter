35e31d8f5f8017d7e309d4266105487795f20a14
Anna Domashych <anna.domashych@gmail.com>
2020-04-24 13:04:22 +0300

    Adjust tour model to contain tour info model

--- lib/db_chgk_info/models/tour.dart
+++ lib/db_chgk_info/models/tour.dart
@@ -3,6 +3,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:what_when_where/constants.dart';
 import 'package:what_when_where/db_chgk_info/models/dto_models/tour_dto.dart';
 import 'package:what_when_where/db_chgk_info/models/question.dart';
+import 'package:what_when_where/db_chgk_info/models/tour_info.dart';
 import 'package:what_when_where/utils/texts.dart';
 
 part 'tour.freezed.dart';
@@ -11,29 +12,30 @@ part 'tour.freezed.dart';
 abstract class Tour with _$Tour {
   const factory Tour({
     String id,
-//    String parentId,
-    String title,
-//    String number,
-    String questionsCount,
-//    String tournamentTitle,
-    String description,
-    String url,
-    String editors,
-    String createdAt,
-    String playedAt,
+    TourInfo info,
     List<Question> questions,
   }) = _Tour;
 
-  factory Tour.fromDto(TourDto dto) => Tour(
-        id: dto.id,
-        title: TextUtils.normalizeToSingleLine(dto.title),
+  factory Tour.fromDto(TourDto dto) {
+    final info = TourInfo(
+      id: dto.id,
+      title: TextUtils.normalizeToSingleLine(dto.title),
 //      number: dto.number,
-        questionsCount: dto.questionsCount,
-        description: TextUtils.normalizeToSingleLine(dto.description),
-        url: '${Constants.databaseUrl}/tour/${dto.id}',
-        editors: TextUtils.normalizeToSingleLine(dto.editors),
-        createdAt: TextUtils.normalizeToSingleLine(dto.createdAt),
-        playedAt: TextUtils.normalizeToSingleLine(dto.playedAt),
-        questions: dto.questions.map((dto) => Question.fromDto(dto)).toList(),
-      );
+      questionsCount: dto.questionsCount,
+      description: TextUtils.normalizeToSingleLine(dto.description),
+      url: '${Constants.databaseUrl}/tour/${dto.id}',
+      editors: TextUtils.normalizeToSingleLine(dto.editors),
+      createdAt: TextUtils.normalizeToSingleLine(dto.createdAt),
+      playedAt: TextUtils.normalizeToSingleLine(dto.playedAt),
+    );
+
+    return Tour(
+      id: dto.id,
+      info: info,
+      questions: dto.questions
+          .map((dto) => Question.fromDto(dto))
+          .map((question) => question.copyWith(tourInfo: info))
+          .toList(),
+    );
+  }
 }
--- lib/db_chgk_info/models/tour.freezed.dart
+++ lib/db_chgk_info/models/tour.freezed.dart
@@ -12,25 +12,10 @@ T _$identity<T>(T value) => value;
 class _$TourTearOff {
   const _$TourTearOff();
 
-  _Tour call(
-      {String id,
-      String title,
-      String questionsCount,
-      String description,
-      String url,
-      String editors,
-      String createdAt,
-      String playedAt,
-      List<Question> questions}) {
+  _Tour call({String id, TourInfo info, List<Question> questions}) {
     return _Tour(
       id: id,
-      title: title,
-      questionsCount: questionsCount,
-      description: description,
-      url: url,
-      editors: editors,
-      createdAt: createdAt,
-      playedAt: playedAt,
+      info: info,
       questions: questions,
     );
   }
@@ -41,13 +26,7 @@ const $Tour = _$TourTearOff();
 
 mixin _$Tour {
   String get id;
-  String get title;
-  String get questionsCount;
-  String get description;
-  String get url;
-  String get editors;
-  String get createdAt;
-  String get playedAt;
+  TourInfo get info;
   List<Question> get questions;
 
   $TourCopyWith<Tour> get copyWith;
@@ -56,16 +35,9 @@ mixin _$Tour {
 abstract class $TourCopyWith<$Res> {
   factory $TourCopyWith(Tour value, $Res Function(Tour) then) =
       _$TourCopyWithImpl<$Res>;
-  $Res call(
-      {String id,
-      String title,
-      String questionsCount,
-      String description,
-      String url,
-      String editors,
-      String createdAt,
-      String playedAt,
-      List<Question> questions});
+  $Res call({String id, TourInfo info, List<Question> questions});
+
+  $TourInfoCopyWith<$Res> get info;
 }
 
 class _$TourCopyWithImpl<$Res> implements $TourCopyWith<$Res> {
@@ -78,30 +50,25 @@ class _$TourCopyWithImpl<$Res> implements $TourCopyWith<$Res> {
   @override
   $Res call({
     Object id = freezed,
-    Object title = freezed,
-    Object questionsCount = freezed,
-    Object description = freezed,
-    Object url = freezed,
-    Object editors = freezed,
-    Object createdAt = freezed,
-    Object playedAt = freezed,
+    Object info = freezed,
     Object questions = freezed,
   }) {
     return _then(_value.copyWith(
       id: id == freezed ? _value.id : id as String,
-      title: title == freezed ? _value.title : title as String,
-      questionsCount: questionsCount == freezed
-          ? _value.questionsCount
-          : questionsCount as String,
-      description:
-          description == freezed ? _value.description : description as String,
-      url: url == freezed ? _value.url : url as String,
-      editors: editors == freezed ? _value.editors : editors as String,
-      createdAt: createdAt == freezed ? _value.createdAt : createdAt as String,
-      playedAt: playedAt == freezed ? _value.playedAt : playedAt as String,
+      info: info == freezed ? _value.info : info as TourInfo,
       questions:
           questions == freezed ? _value.questions : questions as List<Question>,
     ));
+  }
+
+  @override
+  $TourInfoCopyWith<$Res> get info {
+    if (_value.info == null) {
+      return null;
+    }
+    return $TourInfoCopyWith<$Res>(_value.info, (value) {
+      return _then(_value.copyWith(info: value));
+    });
   }
 }
 
@@ -109,16 +76,10 @@ abstract class _$TourCopyWith<$Res> implements $TourCopyWith<$Res> {
   factory _$TourCopyWith(_Tour value, $Res Function(_Tour) then) =
       __$TourCopyWithImpl<$Res>;
   @override
-  $Res call(
-      {String id,
-      String title,
-      String questionsCount,
-      String description,
-      String url,
-      String editors,
-      String createdAt,
-      String playedAt,
-      List<Question> questions});
+  $Res call({String id, TourInfo info, List<Question> questions});
+
+  @override
+  $TourInfoCopyWith<$Res> get info;
 }
 
 class __$TourCopyWithImpl<$Res> extends _$TourCopyWithImpl<$Res>
@@ -132,27 +93,12 @@ class __$TourCopyWithImpl<$Res> extends _$TourCopyWithImpl<$Res>
   @override
   $Res call({
     Object id = freezed,
-    Object title = freezed,
-    Object questionsCount = freezed,
-    Object description = freezed,
-    Object url = freezed,
-    Object editors = freezed,
-    Object createdAt = freezed,
-    Object playedAt = freezed,
+    Object info = freezed,
     Object questions = freezed,
   }) {
     return _then(_Tour(
       id: id == freezed ? _value.id : id as String,
-      title: title == freezed ? _value.title : title as String,
-      questionsCount: questionsCount == freezed
-          ? _value.questionsCount
-          : questionsCount as String,
-      description:
-          description == freezed ? _value.description : description as String,
-      url: url == freezed ? _value.url : url as String,
-      editors: editors == freezed ? _value.editors : editors as String,
-      createdAt: createdAt == freezed ? _value.createdAt : createdAt as String,
-      playedAt: playedAt == freezed ? _value.playedAt : playedAt as String,
+      info: info == freezed ? _value.info : info as TourInfo,
       questions:
           questions == freezed ? _value.questions : questions as List<Question>,
     ));
@@ -160,39 +106,18 @@ class __$TourCopyWithImpl<$Res> extends _$TourCopyWithImpl<$Res>
 }
 
 class _$_Tour with DiagnosticableTreeMixin implements _Tour {
-  const _$_Tour(
-      {this.id,
-      this.title,
-      this.questionsCount,
-      this.description,
-      this.url,
-      this.editors,
-      this.createdAt,
-      this.playedAt,
-      this.questions});
+  const _$_Tour({this.id, this.info, this.questions});
 
   @override
   final String id;
   @override
-  final String title;
-  @override
-  final String questionsCount;
-  @override
-  final String description;
-  @override
-  final String url;
-  @override
-  final String editors;
-  @override
-  final String createdAt;
-  @override
-  final String playedAt;
+  final TourInfo info;
   @override
   final List<Question> questions;
 
   @override
   String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
-    return 'Tour(id: $id, title: $title, questionsCount: $questionsCount, description: $description, url: $url, editors: $editors, createdAt: $createdAt, playedAt: $playedAt, questions: $questions)';
+    return 'Tour(id: $id, info: $info, questions: $questions)';
   }
 
   @override
@@ -201,13 +126,7 @@ class _$_Tour with DiagnosticableTreeMixin implements _Tour {
     properties
       ..add(DiagnosticsProperty('type', 'Tour'))
       ..add(DiagnosticsProperty('id', id))
-      ..add(DiagnosticsProperty('title', title))
-      ..add(DiagnosticsProperty('questionsCount', questionsCount))
-      ..add(DiagnosticsProperty('description', description))
-      ..add(DiagnosticsProperty('url', url))
-      ..add(DiagnosticsProperty('editors', editors))
-      ..add(DiagnosticsProperty('createdAt', createdAt))
-      ..add(DiagnosticsProperty('playedAt', playedAt))
+      ..add(DiagnosticsProperty('info', info))
       ..add(DiagnosticsProperty('questions', questions));
   }
 
@@ -217,25 +136,8 @@ class _$_Tour with DiagnosticableTreeMixin implements _Tour {
         (other is _Tour &&
             (identical(other.id, id) ||
                 const DeepCollectionEquality().equals(other.id, id)) &&
-            (identical(other.title, title) ||
-                const DeepCollectionEquality().equals(other.title, title)) &&
-            (identical(other.questionsCount, questionsCount) ||
-                const DeepCollectionEquality()
-                    .equals(other.questionsCount, questionsCount)) &&
-            (identical(other.description, description) ||
-                const DeepCollectionEquality()
-                    .equals(other.description, description)) &&
-            (identical(other.url, url) ||
-                const DeepCollectionEquality().equals(other.url, url)) &&
-            (identical(other.editors, editors) ||
-                const DeepCollectionEquality()
-                    .equals(other.editors, editors)) &&
-            (identical(other.createdAt, createdAt) ||
-                const DeepCollectionEquality()
-                    .equals(other.createdAt, createdAt)) &&
-            (identical(other.playedAt, playedAt) ||
-                const DeepCollectionEquality()
-                    .equals(other.playedAt, playedAt)) &&
+            (identical(other.info, info) ||
+                const DeepCollectionEquality().equals(other.info, info)) &&
             (identical(other.questions, questions) ||
                 const DeepCollectionEquality()
                     .equals(other.questions, questions)));
@@ -245,13 +147,7 @@ class _$_Tour with DiagnosticableTreeMixin implements _Tour {
   int get hashCode =>
       runtimeType.hashCode ^
       const DeepCollectionEquality().hash(id) ^
-      const DeepCollectionEquality().hash(title) ^
-      const DeepCollectionEquality().hash(questionsCount) ^
-      const DeepCollectionEquality().hash(description) ^
-      const DeepCollectionEquality().hash(url) ^
-      const DeepCollectionEquality().hash(editors) ^
-      const DeepCollectionEquality().hash(createdAt) ^
-      const DeepCollectionEquality().hash(playedAt) ^
+      const DeepCollectionEquality().hash(info) ^
       const DeepCollectionEquality().hash(questions);
 
   @override
@@ -260,33 +156,13 @@ class _$_Tour with DiagnosticableTreeMixin implements _Tour {
 }
 
 abstract class _Tour implements Tour {
-  const factory _Tour(
-      {String id,
-      String title,
-      String questionsCount,
-      String description,
-      String url,
-      String editors,
-      String createdAt,
-      String playedAt,
-      List<Question> questions}) = _$_Tour;
+  const factory _Tour({String id, TourInfo info, List<Question> questions}) =
+      _$_Tour;
 
   @override
   String get id;
   @override
-  String get title;
-  @override
-  String get questionsCount;
-  @override
-  String get description;
-  @override
-  String get url;
-  @override
-  String get editors;
-  @override
-  String get createdAt;
-  @override
-  String get playedAt;
+  TourInfo get info;
   @override
   List<Question> get questions;
   @override
