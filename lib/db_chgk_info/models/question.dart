604d67adbd209eae29f05f18fb733ec238521cef
Anna Domashych <anna.domashych@gmail.com>
2020-04-24 14:38:14 +0300

    Fill in tourInfo tournamentInfo with either parameter of values from dto

--- lib/db_chgk_info/models/question.dart
+++ lib/db_chgk_info/models/question.dart
@@ -3,6 +3,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:what_when_where/constants.dart';
 import 'package:what_when_where/db_chgk_info/models/dto_models/question_dto.dart';
 import 'package:what_when_where/db_chgk_info/models/tour_info.dart';
+import 'package:what_when_where/db_chgk_info/models/tournament_info.dart';
 import 'package:what_when_where/utils/texts.dart';
 
 part 'question.freezed.dart';
@@ -18,27 +19,35 @@ abstract class Question with _$Question {
     String passCriteria,
     String comments,
     String sources,
-//    String tourId,
-//    String tournamentId,
-//    String tourTitle,
-//    String tournamentTitle,
     String url,
     TourInfo tourInfo,
   }) = _Question;
 
-  factory Question.fromDto(QuestionDto dto) => Question(
-        questionId: dto.questionId,
-        question: dto.question,
-        number: dto.number,
-        answer: TextUtils.normalizeToMultiLine(dto.answer),
-        authors: TextUtils.normalizeToSingleLine(dto.authors),
-        passCriteria: TextUtils.normalizeToMultiLine(dto.passCriteria),
-        comments: TextUtils.normalizeToMultiLine(dto.comments),
-        sources: TextUtils.normalizeToMultiLine(dto.sources),
-//        tourId: dto.tourId,
-//        tournamentId: dto.tournamentId,
-//        tourTitle: TextUtils.normalizeToSingleLine(dto.tourTitle),
-//        tournamentTitle: TextUtils.normalizeToSingleLine(dto.tournamentTitle),
-        url: '${Constants.databaseUrl}/question/${dto.parentId}/${dto.number}',
-      );
+  factory Question.fromDto(QuestionDto dto,
+      {TourInfo tourInfo = const TourInfo()}) {
+    final tourInfoCopy = tourInfo
+        .copyWith(
+          id: tourInfo.id ?? dto.tourId,
+          title: tourInfo.title ?? dto.tourTitle,
+          tournamentInfo: tourInfo.tournamentInfo ?? const TournamentInfo(),
+        )
+        .copyWith
+        .tournamentInfo(
+          id: tourInfo.tournamentInfo?.id ?? dto.tournamentId,
+          title: tourInfo.tournamentInfo?.title ?? dto.tournamentTitle,
+        );
+
+    return Question(
+      questionId: dto.questionId,
+      question: dto.question,
+      number: dto.number,
+      answer: TextUtils.normalizeToMultiLine(dto.answer),
+      authors: TextUtils.normalizeToSingleLine(dto.authors),
+      passCriteria: TextUtils.normalizeToMultiLine(dto.passCriteria),
+      comments: TextUtils.normalizeToMultiLine(dto.comments),
+      sources: TextUtils.normalizeToMultiLine(dto.sources),
+      url: '${Constants.databaseUrl}/question/${dto.parentId}/${dto.number}',
+      tourInfo: tourInfoCopy,
+    );
+  }
 }
--- lib/db_chgk_info/models/tour.dart
+++ lib/db_chgk_info/models/tour.dart
@@ -4,6 +4,7 @@ import 'package:what_when_where/constants.dart';
 import 'package:what_when_where/db_chgk_info/models/dto_models/tour_dto.dart';
 import 'package:what_when_where/db_chgk_info/models/question.dart';
 import 'package:what_when_where/db_chgk_info/models/tour_info.dart';
+import 'package:what_when_where/db_chgk_info/models/tournament_info.dart';
 import 'package:what_when_where/utils/texts.dart';
 
 part 'tour.freezed.dart';
@@ -16,25 +17,28 @@ abstract class Tour with _$Tour {
     List<Question> questions,
   }) = _Tour;
 
-  factory Tour.fromDto(TourDto dto) {
+  factory Tour.fromDto(TourDto dto,
+      {TournamentInfo tournamentInfo = const TournamentInfo()}) {
     final info = TourInfo(
       id: dto.id,
       title: TextUtils.normalizeToSingleLine(dto.title),
-//      number: dto.number,
       questionsCount: dto.questionsCount,
       description: TextUtils.normalizeToSingleLine(dto.description),
       url: '${Constants.databaseUrl}/tour/${dto.id}',
       editors: TextUtils.normalizeToSingleLine(dto.editors),
       createdAt: TextUtils.normalizeToSingleLine(dto.createdAt),
       playedAt: TextUtils.normalizeToSingleLine(dto.playedAt),
+      tournamentInfo: tournamentInfo.copyWith(
+        id: tournamentInfo.id ?? dto.parentId,
+        title: tournamentInfo.title ?? dto.tournamentTitle,
+      ),
     );
 
     return Tour(
       id: dto.id,
       info: info,
       questions: dto.questions
-          .map((dto) => Question.fromDto(dto))
-          .map((question) => question.copyWith(tourInfo: info))
+          .map((dto) => Question.fromDto(dto, tourInfo: info))
           .toList(),
     );
   }
