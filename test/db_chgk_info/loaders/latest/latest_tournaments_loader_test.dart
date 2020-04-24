330d666e9692dfe5ff1bf8b70e865abb92ad53b9
Anna Domashych <anna.domashych@gmail.com>
2020-04-24 22:00:12 +0300

    Test latest tournaments request with DioMock

--- test/db_chgk_info/loaders/latest/latest_tournaments_loader_test.dart
+++ test/db_chgk_info/loaders/latest/latest_tournaments_loader_test.dart
@@ -1,5 +1,7 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:what_when_where/db_chgk_info/http_client.dart';
 import 'package:what_when_where/db_chgk_info/loaders/latest_tournaments_loader.dart';
 
 import '../../../mocks.dart';
@@ -7,11 +9,13 @@ import 'test_data_1.dart';
 
 void main() {
   test('Loads and parses latest tournaments', () async {
-    final httpClient = HttpClientMock();
-    when(httpClient.getRaw(Uri(path: '/last', queryParameters: {'page': '0'})))
-        .thenAnswer((_) => Future.value(latestTournamentsApiResponse1));
+    final dioMock = DioMock();
+    when(dioMock.get<String>('/last?page=0')).thenAnswer(
+        (_) => Future.value(Response(data: latestTournamentsApiResponse1)));
 
-    final loader = LatestTournamentsLoader.ioc(httpClient: httpClient);
+    final loader =
+        LatestTournamentsLoader.ioc(httpClient: HttpClient.ioc(dio: dioMock));
+
     final tournaments = await loader.get(page: 0);
 
     expect(tournaments, expectedLatestTournaments1);
