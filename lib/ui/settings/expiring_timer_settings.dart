1dc29c4dbfcf06246b916c48bb80de1b74889b8b
Anna Domashych <anna.domashych@gmail.com>
2020-04-25 16:46:52 +0300

    Delete FunctionHolder and usages

--- lib/ui/settings/expiring_timer_settings.dart
+++ lib/ui/settings/expiring_timer_settings.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
-import 'package:tuple/tuple.dart';
 import 'package:what_when_where/redux/app/state.dart';
 import 'package:what_when_where/redux/settings/actions.dart';
+import 'package:what_when_where/redux/settings/state.dart';
 import 'package:what_when_where/resources/dimensions.dart';
 import 'package:what_when_where/resources/strings.dart';
 import 'package:what_when_where/ui/common/expanding_panel.dart';
-import 'package:what_when_where/utils/function_holder.dart';
 
 class ExpiringTimerSettings extends StatelessWidget {
   const ExpiringTimerSettings({Key key}) : super(key: key);
@@ -17,7 +16,7 @@ class ExpiringTimerSettings extends StatelessWidget {
         body: Column(
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: const <Widget>[
+          children: const [
             _ExpiringLongTimerSettings(),
             _ExpiringShortTimerSettings(),
           ],
@@ -29,104 +28,87 @@ class _ExpiringTimerGeneralSettings extends StatelessWidget {
   const _ExpiringTimerGeneralSettings({Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => StoreConnector<AppState,
-          Tuple4<bool, FunctionHolder, bool, FunctionHolder>>(
-      distinct: true,
-      converter: (store) => Tuple4(
-          store.state.settingsState.notifyShortTimerExpiration,
-          FunctionHolder((bool newValue) => store
-              .dispatch(ChangeNotifyShortTimerExpiration(newValue: newValue))),
-          store.state.settingsState.notifyLongTimerExpiration,
-          FunctionHolder((bool newValue) => store
-              .dispatch(ChangeNotifyLongTimerExpiration(newValue: newValue)))),
-      builder: (context, data) {
-        final notifyShortTimerExpiration = data.item1;
-        final changeNotifyShortTimerExpirationFunction = data.item2;
-        final notifyLongTimerExpiration = data.item3;
-        final changeNotifyLongTimerExpirationFunction = data.item4;
-
-        return Row(children: [
-          Expanded(
-            child: Text(
-              Strings.expiringTimerSignal,
-              textAlign: TextAlign.end,
-              style: Theme.of(context).textTheme.subtitle1,
+  Widget build(BuildContext context) => StoreConnector<AppState, SettingsState>(
+        distinct: true,
+        converter: (store) => store.state.settingsState,
+        builder: (context, state) => Row(
+          children: [
+            Expanded(
+              child: Text(
+                Strings.expiringTimerSignal,
+                textAlign: TextAlign.end,
+                style: Theme.of(context).textTheme.subtitle1,
+              ),
             ),
-          ),
-          const SizedBox(width: Dimensions.defaultSpacing * 2),
-          Switch(
-            value: notifyShortTimerExpiration || notifyLongTimerExpiration,
-            onChanged: (newValue) {
-              changeNotifyLongTimerExpirationFunction.function(newValue);
-              changeNotifyShortTimerExpirationFunction.function(newValue);
-            },
-          )
-        ]);
-      });
+            const SizedBox(width: Dimensions.defaultSpacing * 2),
+            Switch(
+              value: state.notifyShortTimerExpiration ||
+                  state.notifyLongTimerExpiration,
+              onChanged: (newValue) {
+                final store = StoreProvider.of<AppState>(context);
+                store.dispatch(
+                    ChangeNotifyShortTimerExpiration(newValue: newValue));
+                store.dispatch(
+                    ChangeNotifyLongTimerExpiration(newValue: newValue));
+              },
+            ),
+          ],
+        ),
+      );
 }
 
 class _ExpiringShortTimerSettings extends StatelessWidget {
   const _ExpiringShortTimerSettings({Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) =>
-      StoreConnector<AppState, Tuple2<bool, FunctionHolder>>(
-          distinct: true,
-          converter: (store) => Tuple2(
-                store.state.settingsState.notifyShortTimerExpiration,
-                FunctionHolder((bool newValue) => store.dispatch(
-                    ChangeNotifyShortTimerExpiration(newValue: newValue))),
+  Widget build(BuildContext context) => StoreConnector<AppState, bool>(
+        distinct: true,
+        converter: (store) =>
+            store.state.settingsState.notifyShortTimerExpiration,
+        builder: (context, data) => Row(
+          children: [
+            const Expanded(
+              child: Text(
+                Strings.forShortTimer,
+                textAlign: TextAlign.end,
               ),
-          builder: (context, data) {
-            final notifyShortTimerExpiration = data.item1;
-            final changeNotifyShortTimerExpirationFunction = data.item2;
-
-            return Row(children: [
-              const Expanded(
-                child: Text(
-                  Strings.forShortTimer,
-                  textAlign: TextAlign.end,
-                ),
-              ),
-              const SizedBox(width: Dimensions.defaultSpacing * 2),
-              Switch(
-                value: notifyShortTimerExpiration,
-                onChanged: (newValue) =>
-                    changeNotifyShortTimerExpirationFunction.function(newValue),
-              )
-            ]);
-          });
+            ),
+            const SizedBox(width: Dimensions.defaultSpacing * 2),
+            Switch(
+              value: data,
+              onChanged: (newValue) => StoreProvider.of<AppState>(context)
+                  .dispatch(
+                      ChangeNotifyShortTimerExpiration(newValue: newValue)),
+            )
+          ],
+        ),
+      );
 }
 
 class _ExpiringLongTimerSettings extends StatelessWidget {
   const _ExpiringLongTimerSettings({Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) =>
-      StoreConnector<AppState, Tuple2<bool, FunctionHolder>>(
-          distinct: true,
-          converter: (store) => Tuple2(
-                store.state.settingsState.notifyLongTimerExpiration,
-                FunctionHolder((bool newValue) => store.dispatch(
-                    ChangeNotifyLongTimerExpiration(newValue: newValue))),
+  Widget build(BuildContext context) => StoreConnector<AppState, bool>(
+        distinct: true,
+        converter: (store) =>
+            store.state.settingsState.notifyLongTimerExpiration,
+        builder: (context, data) => Row(
+          children: [
+            const Expanded(
+              child: Text(
+                Strings.forLongTimer,
+                textAlign: TextAlign.end,
               ),
-          builder: (context, data) {
-            final notifyLongTimerExpiration = data.item1;
-            final changeNotifyLongTimerExpirationFunction = data.item2;
-
-            return Row(children: [
-              const Expanded(
-                child: Text(
-                  Strings.forLongTimer,
-                  textAlign: TextAlign.end,
-                ),
-              ),
-              const SizedBox(width: Dimensions.defaultSpacing * 2),
-              Switch(
-                value: notifyLongTimerExpiration,
-                onChanged: (newValue) =>
-                    changeNotifyLongTimerExpirationFunction.function(newValue),
-              )
-            ]);
-          });
+            ),
+            const SizedBox(width: Dimensions.defaultSpacing * 2),
+            Switch(
+              value: data,
+              onChanged: (newValue) => StoreProvider.of<AppState>(context)
+                  .dispatch(
+                      ChangeNotifyLongTimerExpiration(newValue: newValue)),
+            ),
+          ],
+        ),
+      );
 }
