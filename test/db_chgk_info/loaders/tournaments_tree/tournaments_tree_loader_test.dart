257aca71e33eb76698e8078066d65c8a0366bdd6
Anna Domashych <anna.domashych@gmail.com>
2020-04-25 16:29:10 +0300

    Test tournaments tree loader

---
+++ test/db_chgk_info/loaders/tournaments_tree/tournaments_tree_loader_test.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:what_when_where/db_chgk_info/http_client.dart';
+import 'package:what_when_where/db_chgk_info/loaders/latest_tournaments_loader.dart';
+import 'package:what_when_where/db_chgk_info/loaders/tournaments_tree_loader.dart';
+import 'package:what_when_where/db_chgk_info/models/tournaments_tree.dart';
+
+import '../../../mocks.dart';
+import 'test_data_1.dart';
+
+void main() {
+  test('Loads and parses tournaments tree', () async {
+    final dioMock = DioMock();
+    when(dioMock.get<String>('/tour/0/xml')).thenAnswer(
+        (_) => Future.value(Response(data: tournamentsTreeApiResponse1)));
+
+    final loader =
+        TournamentsTreeLoader.ioc(httpClient: HttpClient.ioc(dio: dioMock));
+
+    final tournamentsTree = await loader.get(id: '0');
+
+    expect(tournamentsTree, expectedTournamentsTree1);
+  });
+}
