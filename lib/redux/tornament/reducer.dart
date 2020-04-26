bfaae35f349be9dc0588db566fe8e0f5a007fabf
Anna Domashych <anna.domashych@gmail.com>
2020-04-26 14:42:58 +0300

    Adapt TournamentReducer to new TournamentState

--- lib/redux/tornament/reducer.dart
+++ lib/redux/tornament/reducer.dart
@@ -1,4 +1,3 @@
-import 'package:quiver/core.dart';
 import 'package:redux/redux.dart';
 import 'package:what_when_where/redux/tornament/actions.dart';
 import 'package:what_when_where/redux/tornament/state.dart';
@@ -6,45 +5,37 @@ import 'package:what_when_where/redux/tornament/state.dart';
 class TournamentReducer {
   static final Reducer<TournamentState> _reducer =
       combineReducers<TournamentState>([
-    TypedReducer<TournamentState, SetTournament>(_setTournament),
-    TypedReducer<TournamentState, ClearTournament>(_voidTournament),
-    TypedReducer<TournamentState, TournamentIsLoading>(
-        _updateTournamentIsLoading),
-    TypedReducer<TournamentState, TournamentLoaded>(_updateTournamentLoaded),
+    TypedReducer<TournamentState, ClearTournament>(_clearTournament),
+    TypedReducer<TournamentState, TournamentIsLoading>(_onTournamentLoading),
+    TypedReducer<TournamentState, TournamentLoaded>(_onTournamentLoaded),
     TypedReducer<TournamentState, TournamentFailedLoading>(
-        _updateTournamentFailedLoading),
+        _onTournamentFailedLoading),
   ]);
 
   static TournamentState reduce(TournamentState state, dynamic action) =>
       _reducer(state, action);
 
-  static TournamentState _setTournament(
-          TournamentState state, SetTournament action) =>
-      TournamentState.from(action.tournament);
-
-  static TournamentState _voidTournament(
+  static TournamentState _clearTournament(
           TournamentState state, ClearTournament action) =>
-      const TournamentState.initial();
+      null;
 
-  static TournamentState _updateTournamentIsLoading(
+  static TournamentState _onTournamentLoading(
           TournamentState state, TournamentIsLoading action) =>
-      state.copyWith(
-        isLoading: Optional.of(true),
-        exception: const Optional.absent(),
+      TournamentState.loading(
+        info: action.info,
       );
 
-  static TournamentState _updateTournamentLoaded(
+  static TournamentState _onTournamentLoaded(
           TournamentState state, TournamentLoaded action) =>
-      state.copyWith(
-        tournament: Optional.of(action.tournament),
-        isLoading: Optional.of(false),
-        exception: const Optional.absent(),
+      TournamentState.data(
+        info: action.tournament.info,
+        tournament: action.tournament,
       );
 
-  static TournamentState _updateTournamentFailedLoading(
+  static TournamentState _onTournamentFailedLoading(
           TournamentState state, TournamentFailedLoading action) =>
-      state.copyWith(
-        isLoading: Optional.of(false),
-        exception: Optional.of(action.exception),
+      TournamentState.error(
+        info: action.info,
+        exception: action.exception,
       );
 }
