7a1bb76c535d202435027ec90268f536542905d2
Anna Domashych <anna.domashych@gmail.com>
2020-05-03 12:57:53 +0300

    Refactor search state

--- lib/redux/search/state.dart
+++ lib/redux/search/state.dart
@@ -1,136 +1,68 @@
-import 'package:collection/collection.dart';
-import 'package:meta/meta.dart';
-import 'package:quiver/core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:what_when_where/db_chgk_info/models/tournament.dart';
-import 'package:what_when_where/db_chgk_info/search/search_parameters.dart';
+import 'package:what_when_where/db_chgk_info/search/sorting.dart';
 
-@immutable
-class SearchState {
-  final SearchTournamentsParametersState searchParameters;
-  final SearchTournamentsResultsState searchResults;
+part 'state.freezed.dart';
 
-  const SearchState._({this.searchParameters, this.searchResults});
+@freezed
+abstract class SearchState with _$SearchState {
+  const factory SearchState.initial({
+    @required SearchParameters parameters,
+  }) = InitialSearchState;
 
-  SearchState.initial()
-      : this._(
-          searchParameters: const SearchTournamentsParametersState.initial(),
-          searchResults: SearchTournamentsResultsState.initial(),
-        );
+  const factory SearchState.loadingFirstPage({
+    @required SearchParameters parameters,
+  }) = LoadingFirstPageSearchState;
 
-  SearchState copyWith({
-    Optional<SearchTournamentsParametersState> searchParameters,
-    Optional<SearchTournamentsResultsState> searchResults,
-  }) =>
-      SearchState._(
-        searchParameters: searchParameters != null
-            ? searchParameters.orNull
-            : this.searchParameters,
-        searchResults:
-            searchResults != null ? searchResults.orNull : this.searchResults,
-      );
+  const factory SearchState.loadingWithData({
+    @required SearchParameters parameters,
+    @required List<Tournament> data,
+  }) = LoadingWithDataSearchState;
 
-  @override
-  int get hashCode => hash2(searchParameters, searchResults);
+  const factory SearchState.errorFirstPage({
+    @required SearchParameters parameters,
+    @required Exception exception,
+  }) = ErrorFirstPageSearchState;
 
-  @override
-  bool operator ==(dynamic other) =>
-      other is SearchState &&
-      other.searchParameters == searchParameters &&
-      other.searchResults == searchResults;
+  const factory SearchState.errorWithData({
+    @required SearchParameters parameters,
+    @required List<Tournament> data,
+    @required Exception exception,
+  }) = ErrorWithDataSearchState;
+
+  const factory SearchState.data({
+    @required SearchParameters parameters,
+    @required List<Tournament> data,
+    @required bool canLoadMore,
+  }) = DataSearchState;
 }
 
-@immutable
-class SearchTournamentsParametersState {
-  final String query;
-  final Sorting sorting;
-
-  final int nextPage;
-
-  bool get hasQuery => query?.isNotEmpty ?? false;
-
-  const SearchTournamentsParametersState._({
-    this.query = '',
-    this.sorting = Sorting.relevance,
-    this.nextPage = 0,
-  });
-
-  const SearchTournamentsParametersState.initial() : this._();
-
-  SearchTournamentsParametersState copyWith({
-    Optional<String> query,
-    Optional<Sorting> sorting,
-    Optional<int> nextPage,
-  }) =>
-      SearchTournamentsParametersState._(
-        query: query != null ? query.orNull : this.query,
-        sorting: sorting != null ? sorting.orNull : this.sorting,
-        nextPage: nextPage != null ? nextPage.orNull : this.nextPage,
-      );
-
-  @override
-  int get hashCode => hash3(query, sorting, nextPage);
-
-  @override
-  bool operator ==(dynamic other) =>
-      other is SearchTournamentsParametersState &&
-      other.query == query &&
-      other.sorting == sorting &&
-      other.nextPage == nextPage;
+@freezed
+abstract class SearchParameters with _$SearchParameters {
+  const factory SearchParameters({
+    @Default('') String query,
+    @Default(Sorting.relevance) Sorting sorting,
+    @Default(0) int nextPage,
+  }) = _SearchParameters;
 }
 
-@immutable
-class SearchTournamentsResultsState {
-  final List<Tournament> data;
-  final bool isLoading;
-  final Exception exception;
-  final bool emptyResults;
-  final bool canLoadMore;
+extension SearchStateX on SearchState {
+  List<Tournament> get dataOrEmpty => this.dataOrNull ?? <Tournament>[];
 
-  bool get hasError => exception != null;
-  bool get hasData => data.isNotEmpty;
+  List<Tournament> get dataOrNull {
+    if (this is DataSearchState) {
+      return (this as DataSearchState).data;
+    }
 
-  SearchTournamentsResultsState._({
-    Iterable<Tournament> data,
-    this.isLoading = false,
-    this.exception,
-    this.emptyResults = false,
-    this.canLoadMore = true,
-  }) : this.data = List<Tournament>.unmodifiable(data ?? <Tournament>[]);
+    if (this is ErrorWithDataSearchState) {
+      return (this as ErrorWithDataSearchState).data;
+    }
 
-  SearchTournamentsResultsState.initial() : this._();
+    if (this is LoadingWithDataSearchState) {
+      return (this as LoadingWithDataSearchState).data;
+    }
 
-  SearchTournamentsResultsState copyWith({
-    Optional<Iterable<Tournament>> data,
-    Optional<bool> isLoading,
-    Optional<Exception> exception,
-    Optional<bool> emptyResults,
-    Optional<bool> canLoadMore,
-  }) =>
-      SearchTournamentsResultsState._(
-        data: data != null ? data.orNull : this.data,
-        isLoading: isLoading != null ? isLoading.orNull : this.isLoading,
-        exception: exception != null ? exception.orNull : this.exception,
-        emptyResults:
-            emptyResults != null ? emptyResults.orNull : this.emptyResults,
-        canLoadMore:
-            canLoadMore != null ? canLoadMore.orNull : this.canLoadMore,
-      );
-
-  @override
-  int get hashCode => hashObjects(<dynamic>[
-        isLoading,
-        exception.runtimeType,
-        data,
-        emptyResults,
-        canLoadMore,
-      ]);
-
-  @override
-  bool operator ==(dynamic other) =>
-      other is SearchTournamentsResultsState &&
-      other.isLoading == isLoading &&
-      other.exception?.runtimeType == exception?.runtimeType &&
-      other.emptyResults == emptyResults &&
-      other.canLoadMore == canLoadMore &&
-      const DeepCollectionEquality().equals(other.data, data);
+    return null;
+  }
 }
