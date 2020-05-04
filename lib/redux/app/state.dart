82103a2c0e28fb5c6b24e05d399f241994e15f9d
Anna Domashych <anna.domashych@gmail.com>
2020-05-04 13:33:29 +0300

    Remove separate random questions state from AppState

--- lib/redux/app/state.dart
+++ lib/redux/app/state.dart
@@ -1,7 +1,6 @@
 import 'package:meta/meta.dart';
 import 'package:what_when_where/redux/latest/state.dart';
 import 'package:what_when_where/redux/questions/state.dart';
-import 'package:what_when_where/redux/random/state.dart';
 import 'package:what_when_where/redux/search/state.dart';
 import 'package:what_when_where/redux/settings/state.dart';
 import 'package:what_when_where/redux/timer/state.dart';
@@ -18,7 +17,6 @@ class AppState {
   final LatestTournamentsState latestTournamentsState;
   final SearchState searchState;
   final SettingsState settingsState;
-  final RandomQuestionsState randomQuestionsState;
   final TournamentsTreeState tournamentsTreeState;
 
   const AppState({
@@ -29,7 +27,6 @@ class AppState {
     this.questionsState,
     this.searchState,
     this.settingsState,
-    this.randomQuestionsState,
     this.tournamentsTreeState,
   });
 
@@ -42,7 +39,6 @@ class AppState {
           questionsState: QuestionsState.initial(),
           searchState: null,
           settingsState: const SettingsState.initial(),
-          randomQuestionsState: const RandomQuestionsState.initial(),
           tournamentsTreeState: null,
         );
 }
