2d3ba8af65c9add75ac3bd1d3beda1c5cae1b938
Anna Domashych <anna.domashych@gmail.com>
2020-05-06 16:22:40 +0300

    Refactor WWWApp. Read settings from StoreConnector and make SplashScreen a home page

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,50 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:what_when_where/app/splash_screen.dart';
+import 'package:what_when_where/common/app_theme.dart';
 import 'package:what_when_where/constants.dart';
 import 'package:what_when_where/ioc/ioc.dart';
+import 'package:what_when_where/redux/app/state.dart';
+import 'package:what_when_where/redux/initialization/actions.dart';
+import 'package:what_when_where/redux/settings/state.dart';
+import 'package:what_when_where/common/text_scale.dart';
 import 'package:what_when_where/resources/themes.dart';
 import 'package:what_when_where/services/analytics.dart';
 import 'package:what_when_where/ui/common/dialog_presenter.dart';
-import 'package:what_when_where/ui/latest_tournaments/route_page.dart';
 import 'package:what_when_where/utils/style_configurator.dart';
 
 class WWWApp extends StatelessWidget {
-  final ThemeMode themeMode;
-  final double textScaleFactor;
-
-  const WWWApp({
-    Key key,
-    this.themeMode,
-    this.textScaleFactor,
-  }) : super(key: key);
+  const WWWApp({Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: Constants.appName,
-        theme: Themes.get(ThemeMode.light),
-        darkTheme: Themes.get(ThemeMode.dark),
-        themeMode: themeMode,
-        navigatorKey: ioc<GlobalKey<NavigatorState>>(),
-        navigatorObservers: [
-          ioc<IAnalyticsService>()
-              .observer(home: LatestTournamentsPage.routeName),
-        ],
-        home: const LatestTournamentsPage(),
-        builder: (context, navigator) {
-          final mediaQueryData = MediaQuery.of(context);
+  Widget build(BuildContext context) => StoreConnector<AppState, SettingsState>(
+        distinct: true,
+        onInit: (store) => store.dispatch(const InitializationAction.init()),
+        converter: (store) => store.state.settingsState,
+        builder: (context, settings) => MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: Constants.appName,
+          theme: Themes.createLightAppTheme(),
+          darkTheme: Themes.createDarkAppTheme(),
+          themeMode: settings?.appTheme?.toThemeMode(),
+          navigatorKey: ioc<GlobalKey<NavigatorState>>(),
+          navigatorObservers: [
+            ioc<IAnalyticsService>().observer(),
+          ],
+          home: const SplashScreen(),
+          builder: (context, navigator) {
+            final mediaQueryData = MediaQuery.of(context);
+            final textScaleFactor = settings?.textScale?.toDouble() ?? 1.0;
 
-          return MediaQuery(
-            data: mediaQueryData.copyWith(
-                textScaleFactor:
-                    mediaQueryData.textScaleFactor * textScaleFactor),
-            child: StyleConfigurator(
-              child: Navigator(
-                onGenerateRoute: (settings) => MaterialPageRoute<dynamic>(
-                  builder: (context) => DialogPresenter(child: navigator),
+            return MediaQuery(
+              data: mediaQueryData.copyWith(
+                  textScaleFactor:
+                      mediaQueryData.textScaleFactor * textScaleFactor),
+              child: StyleConfigurator(
+                child: Navigator(
+                  onGenerateRoute: (settings) => MaterialPageRoute<dynamic>(
+                    builder: (context) => DialogPresenter(child: navigator),
+                  ),
                 ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       );
 }
--- lib/app/app_container.dart
+++ lib/app/app_container.dart
@@ -2,11 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux/redux.dart';
 import 'package:what_when_where/app/app.dart';
-import 'package:what_when_where/common/app_theme.dart';
 import 'package:what_when_where/redux/app/state.dart';
-import 'package:what_when_where/redux/init/actions.dart';
-import 'package:what_when_where/redux/settings/state.dart';
-import 'package:what_when_where/resources/fonts.dart';
 
 class WWWAppContainer extends StatelessWidget {
   final Store<AppState> _store;
@@ -18,14 +14,6 @@ class WWWAppContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) => StoreProvider<AppState>(
         store: _store,
-        child: StoreConnector<AppState, SettingsState>(
-          distinct: true,
-          onInit: (store) => store.dispatch(const Init()),
-          converter: (store) => store.state.settingsState,
-          builder: (context, state) => WWWApp(
-            themeMode: state.appTheme.toThemeMode(),
-            textScaleFactor: Fonts.getTextScale(state.textScale),
-          ),
-        ),
+        child: const WWWApp(),
       );
 }
