53fd767eb766ff856151127ac71d52de6bb537e1
Anna Domashych <anna.domashych@gmail.com>
2020-05-06 16:16:12 +0300

    Declare InitializationAction.init action

---
+++ lib/redux/initialization/actions.freezed.dart
@@ -0,0 +1,85 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named
+
+part of 'actions.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+class _$InitializationActionTearOff {
+  const _$InitializationActionTearOff();
+
+  InitInitializationAction init() {
+    return const InitInitializationAction();
+  }
+}
+
+// ignore: unused_element
+const $InitializationAction = _$InitializationActionTearOff();
+
+mixin _$InitializationAction {}
+
+abstract class $InitializationActionCopyWith<$Res> {
+  factory $InitializationActionCopyWith(InitializationAction value,
+          $Res Function(InitializationAction) then) =
+      _$InitializationActionCopyWithImpl<$Res>;
+}
+
+class _$InitializationActionCopyWithImpl<$Res>
+    implements $InitializationActionCopyWith<$Res> {
+  _$InitializationActionCopyWithImpl(this._value, this._then);
+
+  final InitializationAction _value;
+  // ignore: unused_field
+  final $Res Function(InitializationAction) _then;
+}
+
+abstract class $InitInitializationActionCopyWith<$Res> {
+  factory $InitInitializationActionCopyWith(InitInitializationAction value,
+          $Res Function(InitInitializationAction) then) =
+      _$InitInitializationActionCopyWithImpl<$Res>;
+}
+
+class _$InitInitializationActionCopyWithImpl<$Res>
+    extends _$InitializationActionCopyWithImpl<$Res>
+    implements $InitInitializationActionCopyWith<$Res> {
+  _$InitInitializationActionCopyWithImpl(InitInitializationAction _value,
+      $Res Function(InitInitializationAction) _then)
+      : super(_value, (v) => _then(v as InitInitializationAction));
+
+  @override
+  InitInitializationAction get _value =>
+      super._value as InitInitializationAction;
+}
+
+class _$InitInitializationAction
+    with DiagnosticableTreeMixin
+    implements InitInitializationAction {
+  const _$InitInitializationAction();
+
+  @override
+  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
+    return 'InitializationAction.init()';
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties..add(DiagnosticsProperty('type', 'InitializationAction.init'));
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) || (other is InitInitializationAction);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+}
+
+abstract class InitInitializationAction implements InitializationAction {
+  const factory InitInitializationAction() = _$InitInitializationAction;
+}
