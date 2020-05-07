4adb0c582572fe7a1e8ceb48d0b12f604798761f
Anna Domashych <anna.domashych@gmail.com>
2020-05-07 21:34:10 +0300

    Remove unused code

--- lib/resources/strings.dart
+++
@@ -1,8 +0,0 @@
-class Strings {
-  Strings._();
-
-  static const String question = 'Вопрос';
-
-  static const String app = 'Приложение';
-  static const String sharedVia = 'Отправлено через приложение';
-}
