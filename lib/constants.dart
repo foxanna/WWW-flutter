d017ea230f536bd88a8a2e57ea72c3dfbbd33808
Anna Domashych <anna.domashych@gmail.com>
2020-05-07 21:34:18 +0300

    Refactor

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,7 +3,8 @@ class Constants {
 
   static const String appNameLong = 'Интеллектуальное казино Сова';
   static const String databaseUrl = 'https://db.chgk.info';
-  static const String marketLink =
+  static const String playMarketLink =
       'https://play.google.com/store/apps/details?id=chgk.droid';
+  static const String appStoreLink = '';
   static const String developersEmail = 'que.ou.quand@gmail.com';
 }
