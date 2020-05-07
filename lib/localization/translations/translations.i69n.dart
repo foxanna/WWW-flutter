5223be4a67150b381380cbaece901f3a3bda64c5
Anna Domashych <anna.domashych@gmail.com>
2020-05-07 14:24:55 +0300

    Generate initial translations

---
+++ lib/localization/translations/translations.i69n.dart
@@ -0,0 +1,55 @@
+// ignore_for_file: unused_element, unused_field, camel_case_types, annotate_overrides, prefer_single_quotes
+// GENERATED FILE, do not edit!
+import 'package:i69n/i69n.dart' as i69n;
+
+String get _languageCode => 'en';
+String get _localeName => 'en';
+
+String _plural(int count,
+        {String zero,
+        String one,
+        String two,
+        String few,
+        String many,
+        String other}) =>
+    i69n.plural(count, _languageCode,
+        zero: zero, one: one, two: two, few: few, many: many, other: other);
+String _ordinal(int count,
+        {String zero,
+        String one,
+        String two,
+        String few,
+        String many,
+        String other}) =>
+    i69n.ordinal(count, _languageCode,
+        zero: zero, one: one, two: two, few: few, many: many, other: other);
+String _cardinal(int count,
+        {String zero,
+        String one,
+        String two,
+        String few,
+        String many,
+        String other}) =>
+    i69n.cardinal(count, _languageCode,
+        zero: zero, one: one, two: two, few: few, many: many, other: other);
+
+class Translations implements i69n.I69nMessageBundle {
+  const Translations();
+  String get addedAt => "added at";
+  String get playedAt => "played at";
+  Object operator [](String key) {
+    var index = key.indexOf('.');
+    if (index > 0) {
+      return (this[key.substring(0, index)]
+          as i69n.I69nMessageBundle)[key.substring(index + 1)];
+    }
+    switch (key) {
+      case 'addedAt':
+        return addedAt;
+      case 'playedAt':
+        return playedAt;
+      default:
+        throw Exception('Message $key doesn\'t exist in $this');
+    }
+  }
+}
---
+++ lib/localization/translations/translations_ua.i69n.dart
@@ -0,0 +1,56 @@
+// ignore_for_file: unused_element, unused_field, camel_case_types, annotate_overrides, prefer_single_quotes
+// GENERATED FILE, do not edit!
+import 'package:i69n/i69n.dart' as i69n;
+import 'translations.i69n.dart';
+
+String get _languageCode => 'ua';
+String get _localeName => 'ua';
+
+String _plural(int count,
+        {String zero,
+        String one,
+        String two,
+        String few,
+        String many,
+        String other}) =>
+    i69n.plural(count, _languageCode,
+        zero: zero, one: one, two: two, few: few, many: many, other: other);
+String _ordinal(int count,
+        {String zero,
+        String one,
+        String two,
+        String few,
+        String many,
+        String other}) =>
+    i69n.ordinal(count, _languageCode,
+        zero: zero, one: one, two: two, few: few, many: many, other: other);
+String _cardinal(int count,
+        {String zero,
+        String one,
+        String two,
+        String few,
+        String many,
+        String other}) =>
+    i69n.cardinal(count, _languageCode,
+        zero: zero, one: one, two: two, few: few, many: many, other: other);
+
+class Translations_ua extends Translations {
+  const Translations_ua();
+  String get addedAt => "додано";
+  String get playedAt => "відіграно";
+  Object operator [](String key) {
+    var index = key.indexOf('.');
+    if (index > 0) {
+      return (this[key.substring(0, index)]
+          as i69n.I69nMessageBundle)[key.substring(index + 1)];
+    }
+    switch (key) {
+      case 'addedAt':
+        return addedAt;
+      case 'playedAt':
+        return playedAt;
+      default:
+        return super[key];
+    }
+  }
+}
