1bb6c852916c2dabbbd7d7917b69bf6ec5a06a6b
Anna Domashych <anna.domashych@gmail.com>
2020-05-08 12:57:25 +0300

    Refactor logger

--- lib/utils/logger.dart
+++ lib/utils/logger.dart
@@ -1,22 +1,7 @@
-import 'dart:math';
-
-import 'package:what_when_where/utils/build_mode.dart';
-
-const _maxLogSize = 512;
-
-final _shouldLog = buildMode != BuildMode.release;
+import 'package:flutter/foundation.dart';
 
 void log(Object object) {
-  if (!_shouldLog) {
-    return;
-  }
-
-  final data = _split(object.toString(), _maxLogSize);
-  data.forEach(print);
-}
-
-Iterable<String> _split(String string, int size) sync* {
-  for (var i = 0; i < string.length; i += size) {
-    yield string.substring(i, min(i + size, string.length));
+  if (!kReleaseMode) {
+    debugPrint(object?.toString());
   }
 }
