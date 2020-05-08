f67e30d301812c6e1c81c4b92b786fa2ad021732
Anna Domashych <anna.domashych@gmail.com>
2020-05-08 14:05:35 +0300

    Return logger chunking implementation

--- lib/utils/logger.dart
+++ lib/utils/logger.dart
@@ -1,7 +1,17 @@
+import 'dart:math';
 import 'package:flutter/foundation.dart';
 
+const _maxLogSize = 512;
+
 void log(Object object) {
-  if (!kReleaseMode) {
-    debugPrint(object?.toString());
+  if (!kReleaseMode && object != null) {
+    final data = _split(object.toString(), _maxLogSize);
+    data.forEach(debugPrint);
+  }
+}
+
+Iterable<String> _split(String string, int size) sync* {
+  for (var i = 0; i < string.length; i += size) {
+    yield string.substring(i, min(i + size, string.length));
   }
 }
