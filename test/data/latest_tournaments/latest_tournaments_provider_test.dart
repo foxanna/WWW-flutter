fb8e79169910ab9bcf36102137eda1e5e2ab3198
Anna Domashych <anna.domashych@gmail.com>
2020-05-30 18:19:22 +0300

    Test latest tournaments isNew status

--- test/data/latest_tournaments/latest_tournaments_provider_test.dart
+++ test/data/latest_tournaments/latest_tournaments_provider_test.dart
@@ -2,19 +2,26 @@ import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:what_when_where/data/latest_tournaments_provider.dart';
+import 'package:what_when_where/data/models/tournament.dart';
+import 'package:what_when_where/data/models/tournament_status.dart';
+import 'package:what_when_where/services/storage.dart';
 
 import '../../ioc/container.dart';
 import '../../mocks.dart';
 import 'test_data_1.dart';
+import 'test_data_2.dart';
 
 void main() {
   group('Loads and parses latest tournaments', () {
-    test('Tournaments list in xml', () async {
+    final execute = ({
+      String apiResponse,
+      Iterable<Tournament> expectedTournaments,
+    }) async {
       // arrange
       final testIoc = configureTestIocContainer(mockDio: true);
 
-      when(testIoc<DioMock>().get<String>('/last?page=0')).thenAnswer(
-          (_) => Future.value(Response(data: latestTournamentsApiResponse1)));
+      when(testIoc<DioMock>().get<String>('/last?page=0'))
+          .thenAnswer((_) => Future.value(Response(data: apiResponse)));
 
       final provider = testIoc<ILatestTournamentsProvider>();
 
@@ -22,7 +29,61 @@ void main() {
       final tournaments = await provider.get(page: 0);
 
       // assert
-      expect(tournaments, expectedLatestTournaments1);
-    });
+      expect(tournaments, expectedTournaments);
+    };
+
+    test(
+      'Tournaments list in real-life xml',
+      () => execute(
+        apiResponse: latestTournamentsApiResponse1,
+        expectedTournaments: expectedLatestTournaments1,
+      ),
+    );
+  });
+
+  group('Actualizes tournaments status', () {
+    final execute = ({
+      String apiResponse,
+      Iterable<Tournament> expectedTournaments,
+      bool tournamentsAreRead,
+    }) async {
+      // arrange
+      final testIoc = configureTestIocContainer(mockDio: true);
+
+      when(testIoc<DioMock>().get<String>('/last?page=0'))
+          .thenAnswer((_) => Future.value(Response(data: apiResponse)));
+      when(testIoc<ILocalStorageService>().containsKey('history', any))
+          .thenAnswer((_) => Future.value(tournamentsAreRead));
+
+      final provider = testIoc<ILatestTournamentsProvider>();
+
+      // act
+      final tournaments = await provider.get(page: 0);
+
+      // assert
+      expect(tournaments, expectedTournaments);
+    };
+
+    test(
+      'All tournaments are new',
+      () => execute(
+        apiResponse: latestTournamentsApiResponse2,
+        expectedTournaments: expectedLatestTournaments2.map((x) => x.copyWith(
+              status: const TournamentStatus(isNew: true),
+            )),
+        tournamentsAreRead: false,
+      ),
+    );
+
+    test(
+      'All tournaments were read',
+      () => execute(
+        apiResponse: latestTournamentsApiResponse2,
+        expectedTournaments: expectedLatestTournaments2.map((x) => x.copyWith(
+              status: const TournamentStatus(isNew: false),
+            )),
+        tournamentsAreRead: true,
+      ),
+    );
   });
 }
