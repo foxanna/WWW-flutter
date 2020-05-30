79fc3fb3216c17f0977eb915a4e6494aca9624bc
Anna Domashych <anna.domashych@gmail.com>
2020-05-30 18:41:57 +0300

    Test search tournaments provider actualizes isNew status

--- test/data/search/search_provider_test.dart
+++ test/data/search/search_provider_test.dart
@@ -3,13 +3,16 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:what_when_where/data/models/tournament.dart';
 import 'package:what_when_where/api/search/sorting.dart';
+import 'package:what_when_where/data/models/tournament_status.dart';
 import 'package:what_when_where/data/search_provider.dart';
+import 'package:what_when_where/services/storage.dart';
 
 import '../../ioc/container.dart';
 import '../../mocks.dart';
 import 'test_data_1.dart';
 import 'test_data_2.dart';
 import 'test_data_3.dart';
+import 'test_data_4.dart';
 
 void main() {
   group('Loads and parses tournaments search', () {
@@ -78,5 +81,50 @@ void main() {
         expectedResult: expectedSearchTournaments3,
       ),
     );
+  });
+
+  group('Actualizes tournaments status', () {
+    final execute = ({
+      String apiResponse,
+      Iterable<Tournament> expectedResult,
+      bool tournamentsAreRead,
+    }) async {
+      // arrange
+      final testIoc = configureTestIocContainer(mockDio: true);
+
+      when(testIoc<DioMock>().get<String>(any))
+          .thenAnswer((_) => Future.value(Response(data: apiResponse)));
+      when(testIoc<ILocalStorageService>().containsKey('history', any))
+          .thenAnswer((_) => Future.value(tournamentsAreRead));
+
+      final provider = testIoc<ISearchProvider>();
+
+      // act
+      final results =
+          await provider.get(query: '', sorting: Sorting.date, page: 0);
+
+      // assert
+      expect(results.toList(), expectedResult);
+    };
+
+    test(
+        'All tournaments are new',
+        () => execute(
+              apiResponse: searchTournamentsApiResponse4,
+              expectedResult: expectedSearchTournaments4.map((x) => x.copyWith(
+                    status: const TournamentStatus(isNew: true),
+                  )),
+              tournamentsAreRead: false,
+            ));
+
+    test(
+        'All tournaments were read',
+        () => execute(
+              apiResponse: searchTournamentsApiResponse4,
+              expectedResult: expectedSearchTournaments4.map((x) => x.copyWith(
+                    status: const TournamentStatus(isNew: false),
+                  )),
+              tournamentsAreRead: true,
+            ));
   });
 }
---
+++ test/data/search/test_data_4.dart
@@ -0,0 +1,50 @@
+import 'package:what_when_where/data/models/tournament.dart';
+import 'package:what_when_where/data/models/tournament_info.dart';
+import 'package:what_when_where/data/models/tournament_status.dart';
+
+const expectedSearchTournaments4 = [
+  Tournament(
+    id2: 'ichb04.8',
+    info: TournamentInfo(
+      id2: 'ichb04.8',
+      title: r'''8 тур, "Тормоза" и "Exception"''',
+      url: r'''https://db.chgk.info/tour/ichb04.8''',
+      toursCount: '0',
+      playedAt: '2003-01-01',
+    ),
+  ),
+  Tournament(
+    id2: 'mgp11516.7',
+    info: TournamentInfo(
+      id2: 'mgp11516.7',
+      title: r'''4.2. "Venceremos"''',
+      url: r'''https://db.chgk.info/tour/mgp11516.7''',
+      toursCount: '0',
+      playedAt: '2016-02-27',
+    ),
+  ),
+];
+
+const searchTournamentsApiResponse4 = '''
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<body>
+
+<dl class="search-results questions-results">
+  <dt class="title">
+    <a href="/tour/ichb04.8">8 тур, &quot;Тормоза&quot; и &quot;Exception&quot;</a>
+  </dt>
+  <dd>
+    <p class="search-snippet"><br/><strong>Дата:</strong> 2003-01-01</p>
+  </dd>
+  <dt class="title">
+    <a href="/tour/mgp11516.7">4.2. &quot;Venceremos&quot;</a>
+  </dt>
+  <dd>
+    <p class="search-snippet"><br/><strong>Дата:</strong> 2016-02-27</p>
+  </dd>
+</dl>
+
+</body>
+</html>
+''';
