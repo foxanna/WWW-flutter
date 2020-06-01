2aa6ecc6c15ac7ba6dc798aa722e8d9b97231232
Anna Domashych <anna.domashych@gmail.com>
2020-06-01 14:28:17 +0300

    Update icons

--- lib/resources/custom_icons.dart
+++ lib/resources/custom_icons.dart
@@ -4,7 +4,14 @@ class CustomIcons {
   CustomIcons._();
 
   static const _kFontFam = 'CustomIcons';
+  static const String _kFontPkg = null;
 
-  static const IconData random_card = IconData(0xe805, fontFamily: _kFontFam);
-  static const IconData tree = IconData(0xe800, fontFamily: _kFontFam);
+  static const IconData random_card =
+      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData bookmarks =
+      IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData tree =
+      IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData settings =
+      IconData(0xe806, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
