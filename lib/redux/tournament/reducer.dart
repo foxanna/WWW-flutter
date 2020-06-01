5384fd449a0539d3a52d03e05a1753f1f4933954
Anna Domashych <anna.domashych@gmail.com>
2020-06-01 17:23:36 +0300

    Update tournament state when its status changed

--- lib/redux/tournament/reducer.dart
+++ lib/redux/tournament/reducer.dart
@@ -1,4 +1,6 @@
+import 'package:dartx/dartx.dart';
 import 'package:redux/redux.dart';
+import 'package:what_when_where/data/models/tournament_info.dart';
 import 'package:what_when_where/redux/redux_action.dart';
 import 'package:what_when_where/redux/tournament/actions.dart';
 import 'package:what_when_where/redux/tournament/state.dart';
@@ -11,6 +13,8 @@ class TournamentReducer {
     TypedReducer<TournamentState, LoadingTournamentSystemAction>(_loading),
     TypedReducer<TournamentState, CompletedTournamentSystemAction>(_completed),
     TypedReducer<TournamentState, FailedTournamentSystemAction>(_failed),
+    TypedReducer<TournamentState, StatusChangedTournamentSystemAction>(
+        _statusChanged),
   ]);
 
   static TournamentState reduce(TournamentState state, ReduxAction action) =>
@@ -35,9 +39,7 @@ class TournamentReducer {
 
   static TournamentState _completed(
           TournamentState state, CompletedTournamentSystemAction action) =>
-      state != null &&
-              (action.tournament.info.id == state.info.id ||
-                  action.tournament.info.id2 == state.info.id2)
+      state != null && _isTheOne(state, action.tournament.info)
           ? TournamentState.data(
               info: action.tournament.info,
               status: state.status,
@@ -47,13 +49,21 @@ class TournamentReducer {
 
   static TournamentState _failed(
           TournamentState state, FailedTournamentSystemAction action) =>
-      state != null &&
-              (action.info.id == state.info.id ||
-                  action.info.id2 == state.info.id2)
+      state != null && _isTheOne(state, action.info)
           ? TournamentState.error(
               info: action.info,
               status: state.status,
               exception: action.exception,
             )
           : state;
+
+  static TournamentState _statusChanged(
+          TournamentState state, StatusChangedTournamentSystemAction action) =>
+      state != null && _isTheOne(state, action.info)
+          ? state.copyWith(status: action.status)
+          : state;
+
+  static bool _isTheOne(TournamentState state, TournamentInfo info) =>
+      (info.id.isNotNullOrEmpty && state.info.id == info.id) ||
+      (info.id2.isNotNullOrEmpty && state.info.id2 == info.id2);
 }
