05e9ccdaa0bda04d8a6a30b8328501a95a393b9c
Anna Domashych <anna.domashych@gmail.com>
2020-06-01 18:40:34 +0300

    Implement BookmarkedMarker

---
+++ lib/ui/common/bookmarked_marker.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class BookmarkedMarker extends StatelessWidget {
+  final double height;
+  final Color color;
+
+  const BookmarkedMarker({
+    Key key,
+    this.height,
+    this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => CustomPaint(
+        size: Size(24.0, height),
+        painter: BookmarkedMarkerPainter(color: color),
+      );
+}
+
+class BookmarkedMarkerPainter extends CustomPainter {
+  final Color color;
+
+  const BookmarkedMarkerPainter({this.color});
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = color
+      ..style = PaintingStyle.fill;
+
+    final path = Path();
+    path.lineTo(0, size.height);
+    path.lineTo(size.width / 2, size.height - size.width / 4);
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, 0.0);
+    path.close();
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
