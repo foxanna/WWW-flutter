feb2e42fd81f74b0bc117e635b786f2b06abdae2
Anna Domashych <anna.domashych@gmail.com>
2020-06-03 13:56:24 +0300

    Fix test

--- test/redux/tournament/tournament_events_analytics_test.dart
+++ test/redux/tournament/tournament_events_analytics_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:what_when_where/data/models/tournament_info.dart';
+import 'package:what_when_where/data/models/tournament_status.dart';
 import 'package:what_when_where/redux/tournament/actions.dart';
 
 import '../analytics_helper.dart';
@@ -9,7 +10,10 @@ void main() {
     test(
       '$OpenTournamentUserAction',
       () => executeAnalyticsTest(
-          const UserActionTournament.open(info: TournamentInfo()),
+          const UserActionTournament.open(
+            info: TournamentInfo(),
+            status: TournamentStatus(),
+          ),
           expectedName: 'tournament'),
     );
   });
