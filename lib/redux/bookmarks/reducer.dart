81d6d789251a984eb55ce6b70d66946c8295eba3
Anna Domashych <anna.domashych@gmail.com>
2020-06-04 18:19:02 +0300

    Listen for tournaments status change

--- lib/redux/bookmarks/reducer.dart
+++ lib/redux/bookmarks/reducer.dart
@@ -1,13 +1,18 @@
 import 'package:redux/redux.dart';
+import 'package:dartx/dartx.dart';
+import 'package:what_when_where/data/models/tournament.dart';
 import 'package:what_when_where/redux/bookmarks/actions.dart';
 import 'package:what_when_where/redux/bookmarks/state.dart';
 import 'package:what_when_where/redux/redux_action.dart';
+import 'package:what_when_where/redux/tournament/actions.dart';
 
 class BookmarksReducer {
   static final Reducer<BookmarksState> _reducer =
       combineReducers<BookmarksState>([
     TypedReducer<BookmarksState, InitBookmarksSystemAction>(_init),
     TypedReducer<BookmarksState, DeInitBookmarksSystemAction>(_deInit),
+    TypedReducer<BookmarksState, StatusChangedTournamentSystemAction>(
+        _statusChanged),
   ]);
 
   static BookmarksState reduce(BookmarksState state, ReduxAction action) =>
@@ -20,4 +25,33 @@ class BookmarksReducer {
   static BookmarksState _deInit(
           BookmarksState state, DeInitBookmarksSystemAction action) =>
       null;
+
+  static BookmarksState _statusChanged(
+      BookmarksState state, StatusChangedTournamentSystemAction action) {
+    if (state == null) {
+      return state;
+    }
+
+    final isTheOne = (Tournament tournament) =>
+        (action.info.id.isNotNullOrEmpty && tournament.id == action.info.id) ||
+        (action.info.id2.isNotNullOrEmpty && tournament.id2 == action.info.id2);
+
+    final index = state.dataOrEmpty.indexWhere((x) => isTheOne(x));
+
+    if (index < 0) {
+      return state;
+    }
+
+    final newData = List<Tournament>.from(state.dataOrEmpty);
+    newData[index] = newData[index].copyWith(status: action.status);
+
+    if (!newData[index].status.isBookmarked) {
+      newData.removeAt(index);
+    }
+
+    return state.maybeMap(
+      data: (value) => value.copyWith(data: newData),
+      orElse: () => state,
+    );
+  }
 }
