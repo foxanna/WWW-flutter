ec82221658d5e95220a424089696cf4b134ef3ad
Anna Domashych <anna.domashych@gmail.com>
2020-06-07 17:06:35 +0300

    Add 'up' FAB to tournaments tree page

--- lib/ui/tree/route_page.dart
+++ lib/ui/tree/route_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:what_when_where/data/models/tournaments_tree_info.dart';
 import 'package:what_when_where/resources/style_configuration.dart';
+import 'package:what_when_where/ui/common/scroll_controller_bound_fab.dart';
 import 'package:what_when_where/ui/tree/app_bar.dart';
 import 'package:what_when_where/ui/tree/page_content.dart';
 
-class TournamentsTreeRoutePage extends StatelessWidget {
+class TournamentsTreeRoutePage extends StatefulWidget {
   static const String routeName = 'tree';
 
   final TournamentsTreeInfo info;
@@ -15,16 +16,35 @@ class TournamentsTreeRoutePage extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  _TournamentsTreeRoutePageState createState() =>
+      _TournamentsTreeRoutePageState();
+}
+
+class _TournamentsTreeRoutePageState extends State<TournamentsTreeRoutePage> {
+  final _scrollController = ScrollController();
+
+  @override
   Widget build(BuildContext context) => Scaffold(
         backgroundColor: StyleConfiguration.of(context)
             .tournamentsTreeStyleConfiguration
             .scaffoldBackground,
         body: CustomScrollView(
+          controller: _scrollController,
           physics: const BouncingScrollPhysics(),
           slivers: [
-            TournamentsTreeAppBar(info: info),
-            TournamentsTreePageContent(info: info),
+            TournamentsTreeAppBar(info: widget.info),
+            TournamentsTreePageContent(info: widget.info),
           ],
         ),
+        floatingActionButton: ScrollControllerBoundFloatingActionButton(
+          scrollController: _scrollController,
+        ),
       );
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+
+    super.dispose();
+  }
 }
