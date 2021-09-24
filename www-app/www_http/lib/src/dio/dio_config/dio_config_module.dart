907e829c219a5a7237051a746d9b0b45b83517f1
Anna Domashych <anna.domashych@gmail.com>
2021-09-24 23:30:45 +0200

    More relaxed timeouts

--- www-app/www_http/lib/src/dio/dio_config/dio_config_module.dart
+++ www-app/www_http/lib/src/dio/dio_config/dio_config_module.dart
@@ -11,10 +11,10 @@ abstract class DioConfigInjectableModule {
   String get baseUrl => WWWConstants.databaseUrl;
 
   @Named(DioConfigParameters.connectTimeout)
-  int get connectTimeout => 15000;
+  int get connectTimeout => 30000;
 
   @Named(DioConfigParameters.receiveTimeout)
-  int get receiveTimeout => 3000;
+  int get receiveTimeout => 60000;
 
   @Named(DioConfigParameters.logHttpCommunication)
   bool get logHttpCommunication => true;
